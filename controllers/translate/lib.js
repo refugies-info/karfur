c41392ce60efef92750e9eb4d4712b7e6c2f2ef5
Soufiane <souflam007@yahoo.fr>
2019-05-29 11:25:05 +0200

    clearing config

--- controllers/translate/lib.js
+++ controllers/translate/lib.js
@@ -3,11 +3,24 @@ const projectId = 'traduction-1551702821050';
 
 const translate = new Translate({
   projectId: projectId,
-  keyFilename: __dirname + "../../../config/Traduction-1edb23e00f9a-serviceAccount.json"
+  credentials: {
+    "type": "service_account",
+    "project_id": "traduction-1551702821050",
+    "private_key_id": process.env.GCLOUD_PRIVATE_KEY_ID,
+    "private_key": process.env.GCLOUD_PKEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.GCLOUD_CLIENT_EMAIL,
+    "client_id": process.env.GCLOUD_CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": process.env.GCLOUD_CLIENT_X509
+  },
+  
+  // keyFilename: __dirname + "../../../config/Traduction-bd4ec8a5b32d-unharmed.json"
 });
 
 //A mettre en place d'abord:
-//export GOOGLE_APPLICATION_CREDENTIALS="/Users/tonyparker/Documents/github/karfur/config/Traduction-1edb23e00f9a-serviceAccount.json" 
+//export GOOGLE_APPLICATION_CREDENTIALS="/Users/tonyparker/Documents/github/karfur/config/Traduction-bd4ec8a5b32d-unharmed.json" 
 function get_translation(req, res) {
   if (!req.body || !req.body.q) {
     res.status(400).json({
