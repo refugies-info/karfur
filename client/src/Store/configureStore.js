9b6ed09b97a2325dc9d16a34bb84ae79c06bdd8c
Soufiane <souflam007@yahoo.fr>
2019-06-06 16:16:13 +0200

    redux fixed

--- client/src/Store/configureStore.js
+++ client/src/Store/configureStore.js
@@ -14,5 +14,5 @@ export default createStore(
   rootReducer, 
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) || compose
   ))
