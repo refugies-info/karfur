f49a1c0e85a519636c5d9dac8597eeac1a7d28a4
Tony4469 <souflam007@yahoo.fr>
2019-10-21 17:50:45 +0200

    livraison demarche v1.1 built

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e7bc92f7b6b7498525aa2d17d30e9c00.js"
+  "/precache-manifest.216e61506b8ace31698a5dc4c485c447.js"
 );
 
 workbox.clientsClaim();
