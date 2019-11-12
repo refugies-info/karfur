f5bbdd4bd89cc3c830963244a2f27e8c70b80298
Tony4469 <souflam007@yahoo.fr>
2019-11-12 11:31:00 +0100

    logo

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { logo_header } from '../../assets/figma';
+
 import './Logo.scss';
 
 const logo = (props) => (
   <NavLink to="/" className="logo mr-10">
-    <span>Réfugiés.info</span>
-    {!props.reduced && <sup className="beta-tag">beta</sup>}
+    <img src={logo_header} className="logo-img" />
   </NavLink>
 );
 
