914079117dd6dc90425b437b024ddf1fa41c3678
hugo-stephan <hugo.stephan28@gmail.com>
2019-11-13 15:53:38 +0100

    Test Fbutton Hugo

--- client/src/containers/HomePage/HomePage.js
+++ client/src/containers/HomePage/HomePage.js
@@ -71,19 +71,9 @@ class HomePage extends Component {
           </div>
           <div className="chevron-wrapper">
             <div className="slide-animation">
-              <AnchorLink offset='60' href="#plan" className="arrowhead-icon header-anchor">
-                <div className="slide-background">
-                  <EVAIcon className="bottom-slider" name="arrow-circle-down" size="hero"/>
-                </div>
-              </AnchorLink>
+               <EVAIcon className="bottom-slider" name="arrow-circle-down" size="hero"/>
             </div>
           </div>
-          <AnchorLink offset='60' href="#plan" className="arrowhead-icon header-anchor d-inline-flex justify-content-center align-items-center">
-            <div className="slide-animation">
-              <span className="slide-background"></span>
-              <EVAIcon className="slide-bottom" name="arrow-circle-down" size="hero"/>
-            </div>
-          </AnchorLink>
         </section>
 
         <section id="plan" className="triptique">
