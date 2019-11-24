8426dd21ffc8aaf4dd5936f30aefedf54d4c1eb2
Tony4469 <souflam007@yahoo.fr>
2019-11-24 22:33:15 +0100

    version finale 25 novembre, yallah

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe3c4cd9f049cd712bc38facd0d8d406.js"
+  "/precache-manifest.4843ea1d1126cc68050e80002143b43a.js"
 );
 
 workbox.clientsClaim();
