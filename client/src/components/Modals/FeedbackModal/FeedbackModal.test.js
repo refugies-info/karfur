ada00ef17a0b54e69ed8b95922d044bd417f23aa
Tony4469 <souflam007@yahoo.fr>
2019-11-27 14:43:38 +0100

    bug fix AdvancedSearch pin

--- client/src/components/Modals/FeedbackModal/FeedbackModal.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import FeedbackModal from './FeedbackModal';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<FeedbackModal />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
---
+++ messenger/whatsapp.js
@@ -0,0 +1,15 @@
+const {accountSid, authToken} = process.env;
+const client = require('twilio')(accountSid, authToken);
+
+function whatsapp_post(req, res) {
+  res.sendStatus(200);
+  client.messages
+    .create({
+        from: 'whatsapp:+14155238886',
+        body: 'Hello, there!',
+        to: 'whatsapp:+33617118026'
+      })
+    .then(message => console.log(message.sid));
+};
+
+exports.whatsapp_post = whatsapp_post;
