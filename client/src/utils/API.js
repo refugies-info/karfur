d27c9e88bd4756da8d23492d88ee4a23cf6dce6c
Tony4469 <souflam007@yahoo.fr>
2019-12-13 17:19:24 +0100

    tests: fix CI

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,12 +10,12 @@ export { socket };
 
 const headers = {
   'Content-Type': 'application/json',
-  'x-access-token' : localStorage.getItem("token") || undefined,
+  'x-access-token' : process.env.NODE_ENV === "test" ? process.env.REACT_APP_FAKE_TOKEN : localStorage.getItem("token") || undefined,
   'cookie-id' : Cookies.get("_ga"),
   'site-secret' : process.env.REACT_APP_SITE_SECRET
 }
 console.log(process.env.NODE_ENV, process.env.REACT_APP_TEST_ENV)
-const burl = process.env.NODE_ENV === "test" ? process.env.REACT_APP_TEST_ENV ? "https://agir-qa.herokuapp.com" : "http://localhost:3000" : ""; //Noté explicitement pour les tests, sinon il arrive pas à proxy entre :80 et :3000
+const burl = process.env.NODE_ENV === "test" ? process.env.REACT_APP_TEST_ENV === "qa" ? "https://agir-qa.herokuapp.com" : "http://localhost:3000" : ""; //Noté explicitement pour les tests, sinon il arrive pas à proxy entre :80 et :3000
 
 axios.withCredentials = true;
 
