44b87beb9ca65c0bc1c4114dd2c4b6a7597d6c33
Tony4469 <souflam007@yahoo.fr>
2019-12-20 16:20:22 +0100

    feature: admin dashboard

--- client/src/components/Backend/Admin/CustomTabPane.js
+++ client/src/components/Backend/Admin/CustomTabPane.js
@@ -5,10 +5,15 @@ import UsersTab from './UsersTab/UsersTab';
 import LanguesTab from './LanguesTab/LanguesTab';
 import ThemesTab from './ThemesTab/ThemesTab';
 import StructuresTab from './StructuresTab/StructuresTab';
+import AdminContenu from '../../../containers/Backend/AdminContenu/AdminContenu';
+import Dashboard from '../../../containers/Backend/Dashboard/Dashboard';
 
 const customTabPane = (props) => {
   return(
     <>
+      <TabPane tabId="0" className="no-padding">
+        <AdminContenu/>
+      </TabPane>
       <TabPane tabId="1">
         <UsersTab {...props}/>
       </TabPane>
@@ -20,6 +25,9 @@ const customTabPane = (props) => {
       </TabPane>
       <TabPane tabId="4">
         <StructuresTab {...props} />
+      </TabPane>
+      <TabPane tabId="5" className="no-padding">
+        <Dashboard />
       </TabPane>
     </>
   )
