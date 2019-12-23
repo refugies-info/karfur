90bee1795a8deaab68748f6e6147c186564dd0c9
Tony4469 <souflam007@yahoo.fr>
2019-12-23 17:26:50 +0100

    fix: bug validation trad dispo

--- client/src/containers/Dispositif/SideTrad/SideTrad.js
+++ client/src/containers/Dispositif/SideTrad/SideTrad.js
@@ -257,7 +257,7 @@ class SideTrad extends Component {
       const nbTraduits = this._countContents([traduction.translatedText]);
       const nbInit = (this._countContents(this.props.menu) + pointeurs.length - this.props.menu.length);
       traduction.avancement =  nbTraduits / nbInit;
-    }else{ traduction.avancement = 1 }
+    }
     traduction.title= (this.props.content.titreMarque || "") + (this.props.content.titreMarque && this.props.content.titreInformatif ? " - " : "") + (this.props.content.titreInformatif || "");
 
     if(this.props.isExpert){
