a1bdd08593a9140a4b809b3cba4dfa4b2ff212c6
Luca Mazzi <lamazing@Lucas-MacBook-Pro.local>
2020-04-27 12:51:07 +0100

    animations

--- client/src/containers/HomePage/CatList.js
+++ client/src/containers/HomePage/CatList.js
@@ -15,8 +15,7 @@ import { NavLink } from "react-router-dom";
 import variables from "scss/colors.scss";
 import icons from "../../assets/streamline";
 import Streamline from "../../assets/streamline";
-
-
+import { motion } from "framer-motion";
 
 //import "./SearchItem.scss";
 // import variables from 'scss/colors.scss';
@@ -26,6 +25,27 @@ const InnerButton = styled.div`
   justify-content: space-between;
   align-items: center;
 `;
+
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: 0.3,
+      when: "beforeChildren",
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const itemanim = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
 
 class CatList extends Component {
   constructor(props) {
@@ -74,50 +94,101 @@ class CatList extends Component {
     console.log(this.state, item);
 
     return (
-      <div className="options-wrapper">
+      <motion.ul
+        className="options-wrapper"
+        variants={container}
+        initial="hidden"
+        animate="visible"
+      >
         {item.children.map((subi, idx) => {
           return (
-            <FSearchBtn
+            <motion.li
               key={idx}
-              onClick={() => this.selectOption(subi)}
               className={"search-options color" + (subi.short ? "" : " full")}
-              color={(subi.short || "").replace(/ /g, "-")}
+              variants={itemanim}
             >
-              <InnerButton>
-              {subi.icon ?
-              <div style={{display: 'flex',marginRight: 10, justifyContent: 'center', alignItems: 'center'}}>
-              <Streamline name={subi.icon} stroke={'white'} width={22} height={22}/>
-              </div>
-              : null}
-              {t("Tags." + subi.name, subi.name)}
-              </InnerButton>
-             </FSearchBtn>
+              <FSearchBtn
+                key={idx}
+                onClick={() => this.selectOption(subi)}
+                className={"search-options color" + (subi.short ? "" : " full")}
+                color={(subi.short || "").replace(/ /g, "-")}
+              >
+                <InnerButton>
+                  {subi.icon ? (
+                    <div
+                      style={{
+                        display: "flex",
+                        marginRight: 10,
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Streamline
+                        name={subi.icon}
+                        stroke={"white"}
+                        width={22}
+                        height={22}
+                      />
+                    </div>
+                  ) : null}
+                  {t("Tags." + subi.name, subi.name)}
+                </InnerButton>
+              </FSearchBtn>
+            </motion.li>
           );
         })}
-        <a
-          target="_blank"
-          href="https://soliguide.fr/"
-          className={"search-options advanced-search-btn  bg-noir"}
+        <motion.li
+          key={item.children.length}
+          className={"search-options  advanced-search-a bg-noir"}
+          variants={itemanim}
         >
-          <InnerButton>
-              <div style={{display: 'flex',marginRight: 10, justifyContent: 'center', alignItems: 'center'}}>
-              <Streamline name={'message'} stroke={'white'} width={22} height={22}/>
+          <a
+            target="_blank"
+            href="https://soliguide.fr/"
+          >
+            <InnerButton>
+              <div
+                style={{
+                  display: "flex",
+                  marginRight: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Streamline
+                  name={"message"}
+                  stroke={"white"}
+                  width={22}
+                  height={22}
+                />
               </div>
-          manger, me doucher...
-              </InnerButton>
-        </a>
-        <button
-          onClick={() => {this.props.history.push('/advanced-search')}}
-          className={"search-options advanced-search-btn-menu  bg-blanc"}
-        >
-          <InnerButton>
-              <div style={{display: 'flex',marginRight: 10, justifyContent: 'center', alignItems: 'center'}}>
-                <Streamline name={'menu'} stroke={'black'} />
+              manger, me doucher...
+            </InnerButton>
+          </a>
+        </motion.li>
+        <motion.li key={item.children.length + 1} variants={itemanim}>
+          <button
+            onClick={() => {
+              this.props.history.push("/advanced-search");
+            }}
+            className={"search-options advanced-search-btn-menu  bg-blanc"}
+          >
+            <InnerButton>
+              <div
+                style={{
+                  display: "flex",
+                  marginRight: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Streamline name={"menu"} stroke={"black"} />
               </div>
               {t("Toolbar.Tout voir", "Tout voir")}
-              </InnerButton>
-        </button>
-      </div>
+            </InnerButton>
+          </button>
+        </motion.li>
+      </motion.ul>
     );
   }
 }
