a32fae4049ee707af3d57368c4cec4321f7cc2ae
agathekieny <agkieny@gmail.com>
2020-05-22 16:57:22 +0200

    :recycle: reformat

--- client/src/Store/actions/actionTypes.js
+++ client/src/Store/actions/actionTypes.js
@@ -1,12 +1,12 @@
-export const TOGGLE_LANGUE = 'TOGGLE_LANGUE';
-export const TOGGLE_LANG_MODAL = 'TOGGLE_LANG_MODAL';
+export const TOGGLE_LANGUE = "TOGGLE_LANGUE";
+export const TOGGLE_LANG_MODAL = "TOGGLE_LANG_MODAL";
 
-export const TOGGLE_TTS = 'TOGGLE_TTS';
-export const TOGGLE_SPINNER = 'TOGGLE_SPINNER';
+export const TOGGLE_TTS = "TOGGLE_TTS";
+export const TOGGLE_SPINNER = "TOGGLE_SPINNER";
 
-export const SET_LANGUES = 'SET_LANGUES';
-export const SET_DISPOSITIFS = 'SET_DISPOSITIFS';
-export const SET_STRUCTURES = 'SET_STRUCTURES';
+export const SET_LANGUES = "SET_LANGUES";
+export const SET_DISPOSITIFS = "SET_DISPOSITIFS";
+export const SET_STRUCTURES = "SET_STRUCTURES";
 
-export const SET_USER = 'SET_USER';
-export const UPDATE_USER = 'UPDATE_USER';
+export const SET_USER = "SET_USER";
+export const UPDATE_USER = "UPDATE_USER";
--- client/src/Store/actions/tts.js
+++ client/src/Store/actions/tts.js
@@ -1,4 +1,7 @@
-import * as actionTypes from './actionTypes';
+import * as actionTypes from "./actionTypes";
 
 export const toggleTTS = () => ({ type: actionTypes.TOGGLE_TTS });
-export const toggleSpinner = value => ({ type: actionTypes.TOGGLE_SPINNER, value: value });
+export const toggleSpinner = (value) => ({
+  type: actionTypes.TOGGLE_SPINNER,
+  value: value,
+});
