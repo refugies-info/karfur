796cb072ed6f7a7ebe3d99ff09d28ef10bb14cd4
agathekieny <agkieny@gmail.com>
2020-05-25 18:20:36 +0200

    :recycle: move tts actions in correct folder

--- client/src/services/actions/tts.js
+++
@@ -1,7 +0,0 @@
-import { TOGGLE_TTS, TOGGLE_SPINNER } from "../Tts/tts.actionTypes";
-
-export const toggleTTS = () => ({ type: TOGGLE_TTS });
-export const toggleSpinner = (value) => ({
-  type: TOGGLE_SPINNER,
-  value: value,
-});
