d761b45eecb14ea779663f4196a690be5cd1aeae
agathekieny <agkieny@gmail.com>
2020-05-25 19:01:46 +0200

    :recycle: reducer structure

--- client/src/services/Structures/structures.actions.ts
+++ client/src/services/Structures/structures.actions.ts
@@ -1,2 +1,2 @@
 // example file
-export {};
+export type StructureActions = any;
--- client/src/services/Structures/structures.reducer.ts
+++ client/src/services/Structures/structures.reducer.ts
@@ -1,2 +1,19 @@
-// example file
-export {};
+import { updateObject } from "../utility";
+import { createReducer } from "typesafe-actions";
+import { StructureActions } from "./structures.actions";
+import { Structure } from "../../@types/interface";
+
+export interface StructureState {
+  structures: Structure[];
+}
+const initialStructureState: StructureState = {
+  structures: [],
+};
+
+export const structureReducer = createReducer<StructureState, StructureActions>(
+  initialStructureState,
+  {
+    SET_STRUCTURES: (state, action) =>
+      updateObject(state, { structures: action.value }),
+  }
+);
