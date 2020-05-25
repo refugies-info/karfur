c4be6de69e99d986ded29128ee97038a80e146e0
agathekieny <agkieny@gmail.com>
2020-05-25 11:20:43 +0200

    :label: type user actions

--- client/src/services/User/user.actions.ts
+++ client/src/services/User/user.actions.ts
@@ -1,23 +1,24 @@
 import { SET_USER, UPDATE_USER } from "./user.actionTypes";
 import API from "../../utils/API";
+import { RequestReturn, User } from "../../@types/interface";
 
-export const setUserActionCreator = (value) => {
+export const setUserActionCreator = (value: User | {}) => {
   return {
     type: SET_USER,
     value: value,
   };
 };
 
-export const updateUserActionCreator = (value) => ({
+export const updateUserActionCreator = (value: User) => ({
   type: UPDATE_USER,
   value: value,
 });
 
 // TO DO : transform in saga
 export const fetch_user = () => {
-  return (dispatch) => {
+  return (dispatch: any) => {
     if (API.isAuth()) {
-      return API.get_user_info().then((data) => {
+      return API.get_user_info().then((data: RequestReturn<User>) => {
         return dispatch(setUserActionCreator(data.data.data));
       });
     } else {
