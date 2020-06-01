18df19961bd1ae60d26033a84db9019b2bed6e10
agathekieny <agkieny@gmail.com>
2020-06-01 17:45:43 +0200

    :art: TEST QA (TMP) to see env variables

--- client/src/components/Backend/UserProfile/ModifyProfile/ModifyProfile.js
+++ client/src/components/Backend/UserProfile/ModifyProfile/ModifyProfile.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Col, Row, Card, CardBody, CardFooter } from 'reactstrap';
-import ContentEditable from 'react-contenteditable';
-import { withTranslation } from 'react-i18next';
+import React from "react";
+import { Col, Row, Card, CardBody, CardFooter } from "reactstrap";
+import ContentEditable from "react-contenteditable";
+import { withTranslation } from "react-i18next";
 
-import FButton from '../../../FigmaUI/FButton/FButton';
+import FButton from "../../../FigmaUI/FButton/FButton";
 
 const modifyProfile = (props) => {
   const { t, user, editing } = props;
@@ -14,13 +14,18 @@ const modifyProfile = (props) => {
           <Row>
             <Col xl="4" lg="4" md="4" sm="4" xs="4" className="handleBetween">
               <b>{t("UserProfile.Pseudonyme", "Pseudonyme :")}</b>
+              Test {process.env.REACT_APP_ENV}{" "}
+              {process.env.REACT_APP_DATADOG_TOKEN
+                ? process.env.REACT_APP_DATADOG_TOKEN.substring(0, 5)
+                : "no key"}
             </Col>
             <Col xl="8" lg="8" md="8" sm="8" xs="8" className="handleBetween">
               <ContentEditable
                 id="username"
-                html={user.username || ''}
+                html={user.username || ""}
                 disabled={!editing}
-                onChange={props.handleChange} />
+                onChange={props.handleChange}
+              />
             </Col>
           </Row>
           <Row>
@@ -30,9 +35,10 @@ const modifyProfile = (props) => {
             <Col xl="8" lg="8" md="8" sm="8" xs="8" className="handleBetween">
               <ContentEditable
                 id="email"
-                html={user.email || ''}
+                html={user.email || ""}
                 disabled={!editing}
-                onChange={props.handleChange} />
+                onChange={props.handleChange}
+              />
             </Col>
           </Row>
           <Row>
@@ -42,39 +48,73 @@ const modifyProfile = (props) => {
             <Col xl="8" lg="8" md="8" sm="8" xs="8" className="handleBetween">
               <ContentEditable
                 id="description"
-                html={user.description || ''}
+                html={user.description || ""}
                 disabled={!editing}
-                onChange={props.handleChange} />
+                onChange={props.handleChange}
+              />
             </Col>
           </Row>
-          {editing && 
+          {editing && (
             <Row className="nb-car-row">
-              <Col xl="4" g="4" md="4" sm="4" xs="4" className="handleBetween" />
+              <Col
+                xl="4"
+                g="4"
+                md="4"
+                sm="4"
+                xs="4"
+                className="handleBetween"
+              />
               <Col xl="8" lg="8" md="8" sm="8" xs="8" className="handleBetween">
-                <span className="texte-vert">{(user.description || '').length}</span> {t("UserProfile.nbCarMax", "sur 120 caractères")}
+                <span className="texte-vert">
+                  {(user.description || "").length}
+                </span>{" "}
+                {t("UserProfile.nbCarMax", "sur 120 caractères")}
               </Col>
-            </Row>}
+            </Row>
+          )}
         </CardBody>
-        {editing ? 
+        {editing ? (
           <CardFooter className="mt-10">
-            <FButton type="light-action" name="lock-outline" onClick={()=>props.toggleModal('password')} className="mr-10">
-              {t("UserProfile.Modifier le mot de passe", "Modifier le mot de passe")}
+            <FButton
+              type="light-action"
+              name="lock-outline"
+              onClick={() => props.toggleModal("password")}
+              className="mr-10"
+            >
+              {t(
+                "UserProfile.Modifier le mot de passe",
+                "Modifier le mot de passe"
+              )}
             </FButton>
-            <FButton type="light-action" onClick={props.toggleEditing} className="mr-10">
+            <FButton
+              type="light-action"
+              onClick={props.toggleEditing}
+              className="mr-10"
+            >
               {t("Annuler", "Annuler")}
             </FButton>
-            <FButton type="validate" name="save-outline" onClick={props.validateProfile}>
+            <FButton
+              type="validate"
+              name="save-outline"
+              onClick={props.validateProfile}
+            >
               {t("Sauvegarder", "Sauvegarder")}
             </FButton>
-          </CardFooter> :
+          </CardFooter>
+        ) : (
           <CardFooter className="mt-10">
-            <FButton type="dark" name="edit-outline" onClick={props.toggleEditing}>
+            <FButton
+              type="dark"
+              name="edit-outline"
+              onClick={props.toggleEditing}
+            >
               {t("Modifier mon profil", "Modifier mon profil")}
             </FButton>
-          </CardFooter>}
+          </CardFooter>
+        )}
       </div>
     </Card>
-  )
-}
+  );
+};
 
 export default withTranslation()(modifyProfile);
