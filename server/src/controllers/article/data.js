2695b27498660c0e1f2ba1748ff2a842576434c6
agathekieny <agkieny@gmail.com>
2020-06-09 17:08:40 +0200

    :art: start fixing eslint error on server

--- server/src/controllers/article/data.js
+++ server/src/controllers/article/data.js
@@ -1,11 +1,27 @@
 const sanitizeOptions = {
-  allowedTags: [ 'div', 'p', 'strong', 'em', 'u', 'span', 'b', 'ul', 'li', 'figure', 'a', //Les principaux qui sont effectivement utilisés
-  'i', 'br', 'blockquote', 'ol', 'img' ], //des secondaires qu'on peut autoriser sans trop de risque
+  allowedTags: [
+    "div",
+    "p",
+    "strong",
+    "em",
+    "u",
+    "span",
+    "b",
+    "ul",
+    "li",
+    "figure",
+    "a", //Les principaux qui sont effectivement utilisés
+    "i",
+    "br",
+    "blockquote",
+    "ol",
+    "img",
+  ], //des secondaires qu'on peut autoriser sans trop de risque
   allowedAttributes: {
-    a: [ 'href', 'name', 'target', 'class' ],
-    '*': [ 'class' ],
-    img: [ 'src' ]
-  }
-}
+    a: ["href", "name", "target", "class"],
+    "*": ["class"],
+    img: ["src"],
+  },
+};
 
 exports.sanitizeOptions = sanitizeOptions;
