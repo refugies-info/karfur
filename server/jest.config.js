109c1ac5cf43c76fe887ca7eabbed6dce2291bba
agathekieny <agkieny@gmail.com>
2020-06-11 15:57:46 +0200

    :art: install jest server

---
+++ server/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  transform: {
+    "^.+\\.(js)$": "<rootDir>/node_modules/babel-jest",
+    "\\.(ts|tsx)$": "ts-jest",
+  },
+  transformIgnorePatterns: ["node_modules/?!(static-container)"],
+  testRegex: "(/__tests__/.*|\\.(test))\\.(ts|tsx|js)$",
+  testPathIgnorePatterns: ["\\.snap$", "<rootDir>/node_modules/"],
+  cacheDirectory: ".jest/cache",
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+    },
+  },
+  coveragePathIgnorePatterns: ["/node_modules/", "/src/environment"],
+  collectCoverage: false,
+};
