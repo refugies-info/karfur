7765672b9fb706b30cf9d01e3954710a119fff2d
agathekieny <agkieny@gmail.com>
2020-09-08 15:37:20 +0200

    :art: scroll navbar

--- client/src/containers/QuiSommesNous/QuiSommesNous.js
+++ client/src/containers/QuiSommesNous/QuiSommesNous.js
@@ -43,12 +43,18 @@ const HeaderTextContainer = styled.div`
 `;
 
 const NavBarContainer = styled.div`
+  position: sticky;
+  position: -webkit-sticky;
+  top: 75px;
   height: 64px;
   background: #f2f2f2;
   display: flex;
   flex-direction: center;
   justify-content: center;
   align-items: center;
+  z-index: 1;
+  top: ${(props) =>
+    !props.isToolbarVisible ? "0px !important" : "0px !important"};
 `;
 
 const NavBarText = styled.div`
@@ -56,11 +62,13 @@ const NavBarText = styled.div`
   line-height: 20px;
   padding: 22px;
   height: 100%;
-
   &:hover {
     background: #212121;
     color: #f2f2f2;
   }
+
+  background: ${(props) => (props.isVisibleSection ? "#212121" : "#f2f2f2")};
+  color: ${(props) => (props.isVisibleSection ? "#f2f2f2" : "#212121")};
 `;
 
 const SectionHeader = styled.div`
@@ -102,19 +110,45 @@ class QuiSommesNous extends Component {
   state = {
     sideVisible: false,
     membre: null,
+    isToolbarVisible: true,
+    visibleSection: null,
   };
 
   componentDidMount() {
     window.scrollTo(0, 0);
+    window.addEventListener("scroll", this.handleScroll);
   }
 
-  onSelectMembre = (membre) =>
-    this.setState({ sideVisible: true, membre: membre });
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+  onSelectMembre = (membre) => this.setState({ sideVisible: true, membre });
 
   closeSide = () => {
     if (this.state.sideVisible) {
       this.setState({ sideVisible: false });
     }
+  };
+
+  handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    const visible = currentScrollPos < 720;
+    let visibleSection;
+    if (720 < currentScrollPos && currentScrollPos <= 1440) {
+      visibleSection = "Missions";
+    } else if (1440 < currentScrollPos && currentScrollPos <= 2290) {
+      visibleSection = "Equipe";
+    } else if (2290 < currentScrollPos && currentScrollPos <= 3010) {
+      visibleSection = "Problematiques";
+    } else if (3010 < currentScrollPos && currentScrollPos <= 3730) {
+      visibleSection = "Contribution";
+    } else if (3730 < currentScrollPos) {
+      visibleSection = "Partenaires";
+    }
+    this.setState({
+      isToolbarVisible: visible,
+      visibleSection,
+    });
   };
 
   render() {
@@ -147,20 +181,32 @@ class QuiSommesNous extends Component {
             </SubHeaderText>
           </HeaderTextContainer>
         </HeaderContainer>
-        <NavBarContainer>
+        <NavBarContainer isToolbarVisible={this.state.isToolbarVisible}>
           <AnchorLink offset="60" href="#mission">
-            <NavBarText>{t("QuiSommesNous.Missions", "Missions")}</NavBarText>
+            <NavBarText
+              isVisibleSection={this.state.visibleSection === "Missions"}
+            >
+              {t("QuiSommesNous.Missions", "Missions")}
+            </NavBarText>
           </AnchorLink>
           <AnchorLink offset="60" href="#equipe">
-            <NavBarText>{t("QuiSommesNous.Équipe", "Équipe")}</NavBarText>
+            <NavBarText
+              isVisibleSection={this.state.visibleSection === "Equipe"}
+            >
+              {t("QuiSommesNous.Équipe", "Équipe")}
+            </NavBarText>
           </AnchorLink>
           <AnchorLink offset="60" href="#problematic">
-            <NavBarText>
+            <NavBarText
+              isVisibleSection={this.state.visibleSection === "Problematiques"}
+            >
               {t("QuiSommesNous.Problématiques", "Problématiques")}
             </NavBarText>
           </AnchorLink>
           <AnchorLink offset="60" href="#contribution">
-            <NavBarText>
+            <NavBarText
+              isVisibleSection={this.state.visibleSection === "Contribution"}
+            >
               {t(
                 "QuiSommesNous.Approche contributive",
                 "Approche contributive"
@@ -168,7 +214,9 @@ class QuiSommesNous extends Component {
             </NavBarText>
           </AnchorLink>
           <AnchorLink offset="60" href="#partners">
-            <NavBarText>
+            <NavBarText
+              isVisibleSection={this.state.visibleSection === "Partenaires"}
+            >
               {t("QuiSommesNous.Partenaires", "Partenaires")}
             </NavBarText>
           </AnchorLink>
