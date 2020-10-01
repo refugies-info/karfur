002b0d1dc5df8c9142235e34c61090e2cad87f09
Luca Mazzi <luca.mazzi.dev@gmail.com>
2020-10-01 17:18:09 +0200

    changes debug

--- client/src/containers/AdvancedSearch/AdvancedSearch.js
+++ client/src/containers/AdvancedSearch/AdvancedSearch.js
@@ -367,10 +367,10 @@ export class AdvancedSearch extends Component {
           logger.info("principal theme didn't fail",principalThemeList);
           var secondaryThemeList = dispositifs.filter((element) => {
             if (element.tags && element.tags.length > 0) {
-              for (var index = 1; index < elem.tags.length; index++) {
+              for (var index = 1; index < element.tags.length; index++) {
                 if (
-                  index !== 0 && elem.tags[index] &&
-                  elem.tags[index].short === this.state.recherche[0].short
+                  index !== 0 && element.tags[index] &&
+                  element.tags[index].short === this.state.recherche[0].short
                 )
                   return true;
               }
