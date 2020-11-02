7620c73977c9fda178a21bae5de984e826c5bb49
agathekieny <agkieny@gmail.com>
2020-11-02 16:36:45 +0100

    :art: remove datadog (change heroku to gcp)

--- client/src/logger.js
+++ client/src/logger.js
@@ -1,20 +1,18 @@
 /* eslint-disable no-console */
-import { datadogLogs } from "@datadog/browser-logs";
 
-if (process.env.REACT_APP_ENV === "staging") {
-  datadogLogs.init({
-    clientToken: process.env.REACT_APP_DATADOG_TOKEN,
-    datacenter: "eu",
-    forwardErrorsToLogs: true,
-    sampleRate: 100,
-  });
-}
+// if (process.env.REACT_APP_ENV === "staging") {
+//   datadogLogs.init({
+//     clientToken: process.env.REACT_APP_DATADOG_TOKEN,
+//     datacenter: "eu",
+//     forwardErrorsToLogs: true,
+//     sampleRate: 100,
+//   });
+// }
 export class logger {
   static info = (message, data) => {
-    console.log("react", process.env.REACT_APP_ENV);
     if (process.env.REACT_APP_ENV === "staging") {
       console.log(message, data);
-      datadogLogs.logger.debug(message, data, "info");
+      // datadogLogs.logger.debug(message, data, "info");
       return;
     }
 
@@ -23,7 +21,8 @@ export class logger {
 
   static warn = (message, data) => {
     if (process.env.REACT_APP_ENV === "staging") {
-      datadogLogs.logger.log(message, data, "warn");
+      console.log(message, data);
+      // datadogLogs.logger.log(message, data, "warn");
       return;
     }
 
@@ -32,7 +31,8 @@ export class logger {
 
   static error = (message, data) => {
     if (process.env.REACT_APP_ENV === "staging") {
-      datadogLogs.logger.log(message, data, "error");
+      console.log(message, data);
+      // datadogLogs.logger.log(message, data, "error");
       return;
     }
 
