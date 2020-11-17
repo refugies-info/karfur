d2a46e91f93b2d04856cf019045ee63c479d070a
agathekieny <agkieny@gmail.com>
2020-11-17 17:41:08 +0100

    :art: remove channel

--- server/src/controllers/channelController.js
+++
@@ -1,9 +0,0 @@
-const channel = require("./channel/lib.js");
-const checkToken = require("./account/checkToken");
-
-module.exports = function (app, io) {
-  app.post("/add_channel", checkToken.getId, (req, res) =>
-    channel.add_channel(req, res, io)
-  );
-  app.post("/get_channel", checkToken.getId, channel.get_channel);
-};
