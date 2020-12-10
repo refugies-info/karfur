5d0863bf376bd6654f1eccbcbc13d6a853d8bc4d
agathekieny <agkieny@gmail.com>
2020-12-10 18:14:06 +0100

    :art: fix case issue

--- client/src/containers/Backend/UserDashStruct/UserDashStruct.js
+++ client/src/containers/Backend/UserDashStruct/UserDashStruct.js
@@ -41,7 +41,7 @@ import variables from "scss/colors.scss";
 import {
   setUserStructureActionCreator,
   updateUserStructureActionCreator,
-} from "../../../services/UserStructure1/userStructure.actions";
+} from "../../../services/UserStructure/userStructure.actions";
 
 moment.locale("fr");
 
--- client/src/services/rootReducer.ts
+++ client/src/services/rootReducer.ts
@@ -7,7 +7,7 @@ import {
 import {
   structureReducer,
   UserStructureState,
-} from "./UserStructure1/userStructure.reducer";
+} from "./UserStructure/userStructure.reducer";
 import { userReducer, UserState } from "./User/user.reducer";
 import {
   translationReducer,
