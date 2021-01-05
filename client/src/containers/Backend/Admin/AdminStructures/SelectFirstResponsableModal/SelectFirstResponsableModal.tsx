3188859dda771bd54296444296f467a62759d7b7
agathekieny <agkieny@gmail.com>
2021-01-05 17:08:18 +0100

    :art: add searchbar

--- client/src/containers/Backend/Admin/AdminStructures/SelectFirstResponsableModal/SelectFirstResponsableModal.tsx
+++ client/src/containers/Backend/Admin/AdminStructures/SelectFirstResponsableModal/SelectFirstResponsableModal.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Modal } from "reactstrap";
 import "./SelectFirstResponsableModal.scss";
+import { SearchBar } from "containers/UI/SearchBar/SearchBar";
 
 const TEst = styled.div`
   background-color: red;
@@ -11,13 +12,25 @@ interface Props {
   show: boolean;
   toggleModal: () => void;
 }
-export const SelectFirstResponsableModal = (props: Props) => (
-  <Modal
-    isOpen={props.show}
-    toggle={props.toggleModal}
-    size="lg"
-    className="select-respo-modal"
-  >
-    <TEst>test respo </TEst>
-  </Modal>
-);
+export const SelectFirstResponsableModal = (props: Props) => {
+  const users = [{ username: "test" }];
+
+  return (
+    <Modal
+      isOpen={props.show}
+      toggle={props.toggleModal}
+      size="lg"
+      className="select-respo-modal"
+    >
+      <TEst>test respo </TEst>
+      <SearchBar
+        isArray
+        users
+        className="search-bar inner-addon right-addon"
+        placeholder="Chercher"
+        array={users}
+        selectItem={() => {}}
+      />
+    </Modal>
+  );
+};
