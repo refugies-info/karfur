ec74397d129065f33cf345d3eea40a51621fe163
agathekieny <agkieny@gmail.com>
2021-01-07 17:27:47 +0100

    :art: route createStrcuture

--- server/src/controllers/account/users.repository.ts
+++ server/src/controllers/account/users.repository.ts
@@ -1,8 +1,17 @@
 import { User } from "../../schema/schemaUser";
 import { ObjectId } from "mongoose";
 
-export const getUserById = async (id: ObjectId) =>
-  await User.findOne({ _id: id }, { username: 1, picture: 1 });
+type NeededFields = { username: number; picture: number } | { roles: 1 };
+
+export const getUserById = async (id: ObjectId, neededFields: NeededFields) =>
+  await User.findOne({ _id: id }, neededFields);
 
 export const getAllUsersFromDB = async () =>
   await User.find({}, { username: 1, picture: 1, status: 1 });
+
+export const updateUser = async (id: ObjectId, modifiedUser: any) =>
+  await User.findOneAndUpdate({ _id: id }, modifiedUser, {
+    upsert: true,
+    // @ts-ignore
+    new: true,
+  });
