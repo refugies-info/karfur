31291f3afb7fe727061202c64ed17d3ff4ce0d23
agathekieny <agkieny@gmail.com>
2021-03-02 16:53:10 +0100

    :art: remove unused

--- server/src/controllers/eventsController.js
+++ server/src/controllers/eventsController.js
@@ -2,12 +2,5 @@ const events = require("./events/lib.js");
 const checkToken = require("./account/checkToken");
 
 module.exports = function (app) {
-  app.post("/log_event", checkToken.getId, events.log_event);
   app.post("/get_event", checkToken.check, events.get_event);
-  app.post(
-    "/distinct_count_event",
-    checkToken.check,
-    events.distinct_count_event
-  );
-  app.post("/distinct_event", checkToken.check, events.distinct_event);
 };
