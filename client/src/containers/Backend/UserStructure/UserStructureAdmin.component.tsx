402ef15fa44955edc0dc4904189d660267dcb7a2
agathekieny <agkieny@gmail.com>
2021-03-24 16:55:44 +0100

    :art: user admin structure

--- client/src/containers/Backend/UserStructure/UserStructureAdmin.component.tsx
+++ client/src/containers/Backend/UserStructure/UserStructureAdmin.component.tsx
@@ -1,30 +1,13 @@
-/* eslint-disable no-console */
-/* eslint-disable @typescript-eslint/no-unused-vars-experimental */
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  fetchUserStructureActionCreator,
-  updateUserStructureActionCreator,
-} from "../../../services/UserStructure/userStructure.actions";
-import {
-  userStructureSelector,
-  userStructureMembresSelector,
-} from "../../../services/UserStructure/userStructure.selectors";
-import { LoadingStatusKey } from "../../../services/LoadingStatus/loadingStatus.actions";
-import { isLoadingSelector } from "../../../services/LoadingStatus/loadingStatus.selectors";
+import { useSelector } from "react-redux";
 import { UserStructureLoading } from "./components/UserStructureLoading";
-import {
-  MainContainer,
-  StructurePictureContainer,
-  StructureContainer,
-} from "./components/SubComponents";
-import { TitleWithNumber } from "../middleOfficeSharedComponents";
 import { UserStructureDetails } from "./components/UserStructureDetails";
 import styled from "styled-components";
 import { colors } from "../../../colors";
 import { userSelector } from "../../../services/User/user.selectors";
 import { ObjectId } from "mongodb";
 import API from "../../../utils/API";
+import { UserStructure } from "../../../types/interface";
 
 const ErrorContainer = styled.div`
   margin-top: 60px;
@@ -49,118 +32,102 @@ interface Props {
 }
 
 export const UserStructureAdminComponent = (props: Props) => {
-  const [structure, setStructure] = useState(null);
-  const isLoading = false;
+  const [structure, setStructure] = useState<null | UserStructure>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [reload, setReload] = useState(false);
 
-  console.log("location", props.location.state);
-  console.log("structur", structure);
-  //   const userStructure = useSelector(userStructureSelector);
+  const toggleReload = () => setReload(!reload);
   const user = useSelector(userSelector);
 
   const structureId = props.location.state.structure;
-  console.log("structureId", structureId);
   useEffect(() => {
     const loadStructure = async () => {
-      const data = await API.getStructureById(structureId, true, "fr", true);
-      setStructure(data.data.data);
+      if (structureId) {
+        setIsLoading(true);
+        const data = await API.getStructureById(structureId, true, "fr", true);
+        setStructure(data.data.data);
+        setIsLoading(false);
+      }
     };
     loadStructure();
-
-    // if (userStructure) {
-    //   dispatch(
-    //     fetchUserStructureActionCreator({
-    //       structureId: userStructure._id,
-    //       shouldRedirect: true,
-    //     })
-    //   );
-    // }
     window.scrollTo(0, 0);
-  }, []);
+  }, [reload]);
 
-  //   const isLoadingFetch = useSelector(
-  //     isLoadingSelector(LoadingStatusKey.FETCH_USER_STRUCTURE)
-  //   );
-  //   const isLoadingUpdate = useSelector(
-  //     isLoadingSelector(LoadingStatusKey.UPDATE_USER_STRUCTURE)
-  //   );
+  const membres = structure ? structure.membres : [];
 
-  //   const isLoading = isLoadingFetch || isLoadingUpdate;
-
-  //   const membres = useSelector(userStructureMembresSelector);
-
-  const addUserInStructure = (userId: ObjectId) => {
-    // if (!userStructure) return;
-    // dispatch(
-    //   updateUserStructureActionCreator({
-    //     modifyMembres: true,
-    //     data: { structureId: userStructure._id, userId, type: "create" },
-    //   })
-    // );
+  const addUserInStructure = async (userId: ObjectId) => {
+    if (!structure) return;
+    const query = {
+      membreId: userId,
+      structureId: structure._id,
+      action: "create",
+      role: "contributeur",
+    };
+    setIsLoading(true);
+    await API.modifyUserRoleInStructure({ query });
+    setIsLoading(false);
+    toggleReload();
   };
 
-  const modifyRole = (
+  const modifyRole = async (
     userId: ObjectId,
     role: "contributeur" | "administrateur"
   ) => {
-    // if (!userStructure) return;
-    // dispatch(
-    //   updateUserStructureActionCreator({
-    //     modifyMembres: true,
-    //     data: {
-    //       structureId: userStructure._id,
-    //       userId,
-    //       newRole: role,
-    //       type: "modify",
-    //     },
-    //   })
-    // );
+    if (!structure) return;
+    const query = {
+      membreId: userId,
+      structureId: structure._id,
+      action: "modify",
+      role,
+    };
+    setIsLoading(true);
+    await API.modifyUserRoleInStructure({ query });
+    setIsLoading(false);
+    toggleReload();
   };
 
-  const deleteUserFromStructure = (userId: ObjectId) => {
-    // if (!userStructure) return;
-    // dispatch(
-    //   updateUserStructureActionCreator({
-    //     modifyMembres: true,
-    //     data: {
-    //       structureId: userStructure._id,
-    //       userId,
-    //       type: "delete",
-    //     },
-    //   })
-    // );
+  const deleteUserFromStructure = async (userId: ObjectId) => {
+    if (!structure) return;
+    const query = {
+      membreId: userId,
+      structureId: structure._id,
+      action: "delete",
+    };
+    setIsLoading(true);
+    await API.modifyUserRoleInStructure({ query });
+    setIsLoading(false);
+    toggleReload();
   };
 
   if (isLoading) {
     return <UserStructureLoading />;
   }
 
-  return <div>Hello</div>;
+  if (!structure) return <div>No structure</div>;
 
-  //   if (!userStructure) return <div>No structure</div>;
+  if (structure.status === "En attente")
+    return (
+      <ErrorContainer>
+        <ErrorText>
+          Votre structure n'a pas été validée. Veuillez contacter l'équipe de
+          réfugiés.info via le live chat en bas à droite de votre écran.
+        </ErrorText>
+      </ErrorContainer>
+    );
 
-  //   if (userStructure.status === "En attente")
-  //     return (
-  //       <ErrorContainer>
-  //         <ErrorText>
-  //           Votre structure n'a pas été validée. Veuillez contacter l'équipe de
-  //           réfugiés.info via le live chat en bas à droite de votre écran.
-  //         </ErrorText>
-  //       </ErrorContainer>
-  //     );
-
-  //   return (
-  //     <UserStructureDetails
-  //       picture={userStructure.picture}
-  //       name={userStructure.nom}
-  //       acronyme={userStructure.acronyme}
-  //       membres={membres}
-  //       // @ts-ignore
-  //       userId={user.userId}
-  //       structureId={userStructure._id}
-  //       addUserInStructure={addUserInStructure}
-  //       isAdmin={user.admin}
-  //       modifyRole={modifyRole}
-  //       deleteUserFromStructure={deleteUserFromStructure}
-  //     />
-  //   );
+  return (
+    <UserStructureDetails
+      picture={structure.picture}
+      name={structure.nom}
+      acronyme={structure.acronyme}
+      membres={membres}
+      // @ts-ignore
+      userId={user.userId}
+      structureId={structure._id}
+      addUserInStructure={addUserInStructure}
+      isAdmin={user.admin}
+      modifyRole={modifyRole}
+      deleteUserFromStructure={deleteUserFromStructure}
+    />
+  );
 };
