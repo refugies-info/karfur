a2b19086bfeb4003a89f2114c846c2c1ad512953
agathekieny <agkieny@gmail.com>
2021-04-02 10:39:35 +0200

    :art: remove test

--- client/src/tracking/dispatch.js
+++ client/src/tracking/dispatch.js
@@ -1,18 +1,18 @@
 import ReactGA from "react-ga";
-// import { logger } from "../logger";
+import { logger } from "../logger";
 
 export const initGA = () => {
-  // if (process.env.REACT_APP_ENV !== "production") {
-  //   return;
-  // }
+  if (process.env.REACT_APP_ENV !== "production") {
+    return;
+  }
   const trackingId = process.env.REACT_APP_GOOGLE_ANALYTICS;
   ReactGA.initialize(trackingId);
 };
 
 export const PageView = () => {
-  // if (process.env.REACT_APP_ENV !== "production") {
-  //   return;
-  // }
+  if (process.env.REACT_APP_ENV !== "production") {
+    return;
+  }
   ReactGA.pageview(window.location.pathname + window.location.search);
 };
 
@@ -23,10 +23,10 @@ export const PageView = () => {
  * @param {string} label
  */
 export const Event = (category, action, label) => {
-  // if (process.env.REACT_APP_ENV !== "production") {
-  //   logger.info("Event", { category, action, label });
-  //   return;
-  // }
+  if (process.env.REACT_APP_ENV !== "production") {
+    logger.info("Event", { category, action, label });
+    return;
+  }
   ReactGA.event({
     category: category,
     action: action,
