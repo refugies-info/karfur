40eeb6d2359f85e06e7cae26871ee1bf444d1d7f
agathekieny <agkieny@gmail.com>
2021-04-06 14:06:47 +0200

    :art: correct getLanguages

--- server/src/controllers/langues/langues.service.ts
+++ server/src/controllers/langues/langues.service.ts
@@ -17,15 +17,20 @@ export const getLanguages = async (req: {}, res: Res) => {
         avancement: 1,
         avancementTrad: 1,
       }
-    )
-      .sort({
-        avancement: -1,
-      })
-      .lean();
+    ).sort({
+      avancement: -1,
+    });
+
+    const result = activeLanguages.map((langue) => {
+      if (langue.avancementTrad && langue.avancementTrad > 1)
+        return { ...langue.toJSON(), avancementTrad: 1 };
+
+      return langue;
+    });
 
     res.status(200).json({
       text: "Succès",
-      data: activeLanguages,
+      data: result,
     });
   } catch (error) {
     res.status(500).json({ text: "Erreur interne", error });
