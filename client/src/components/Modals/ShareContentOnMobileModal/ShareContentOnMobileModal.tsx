84449317c8b93c805d0c904cca14309d6cec4fe4
Camille Saillard <camillesaillard@MacBook-Air-de-Camille.local>
2021-04-09 11:06:24 +0200

    send sms

--- client/src/components/Modals/ShareContentOnMobileModal/ShareContentOnMobileModal.tsx
+++ client/src/components/Modals/ShareContentOnMobileModal/ShareContentOnMobileModal.tsx
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import Icon from "react-eva-icons";
 import "./ShareContentOnMobileModal.scss";
 import ShareButton from "../../FigmaUI/ShareButton/ShareButton";
+import Swal from "sweetalert2";
+import API from "../../../utils/API";
 
 declare const window: Window;
 interface Props {
@@ -49,6 +51,46 @@ const ButtonsContainer = styled.div`
   display: flex;
   flex-direction: column;
 `;
+const send_sms = (typeContenu: string, titreInformatif: string) =>
+  Swal.fire({
+    title: "Veuillez renseigner votre numéro de téléphone",
+    input: "tel",
+    inputPlaceholder: "0633445566",
+    inputAttributes: {
+      autocomplete: "on",
+    },
+    showCancelButton: true,
+    confirmButtonText: "Envoyer",
+    cancelButtonText: "Annuler",
+    showLoaderOnConfirm: true,
+    preConfirm: (number: number) => {
+      return API.send_sms({
+        number,
+        typeContenu,
+        url: window.location.href,
+        title: titreInformatif,
+      })
+        .then((response: { status: number; statusText: string; data: any }) => {
+          if (response.status !== 200) {
+            throw new Error(response.statusText);
+          }
+          return response.data;
+        })
+        .catch((error: Error) => {
+          Swal.showValidationMessage(`Echec d'envoi: ${error}`);
+        });
+    },
+    allowOutsideClick: () => !Swal.isLoading(),
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire({
+        title: "Yay...",
+        text: "Votre message a bien été envoyé, merci",
+        type: "success",
+        timer: 1500,
+      });
+    }
+  });
 
 export const ShareContentOnMobileModal = (props: Props) => {
   const emailBody =
@@ -78,10 +120,15 @@ export const ShareContentOnMobileModal = (props: Props) => {
           >
             Via email
           </ShareButton>
-          <ShareButton name={"smartphone-outline"} onClick={() => {}}>
+          <ShareButton
+            name={"smartphone-outline"}
+            onClick={() =>
+              send_sms(props.typeContenu, props.content.titreInformatif)
+            }
+          >
             Via sms
           </ShareButton>
-          <ShareButton
+          {/* <ShareButton
             name={"download-outline"}
             //className="ml-10"
             onClick={() => {}}
@@ -94,7 +141,7 @@ export const ShareContentOnMobileModal = (props: Props) => {
             onClick={() => {}}
           >
             Plus d'options
-          </ShareButton>
+          </ShareButton> */}
         </ButtonsContainer>
       </MainContainer>
     </Modal>
