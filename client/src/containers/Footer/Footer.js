bb509da18d240d8d16e4c54d904e301725579274
Camille Saillard <camillesaillard@MacBook-Air-de-Camille.local>
2021-05-04 16:23:32 +0200

    Footer-desktop

--- client/src/containers/Footer/Footer.js
+++ client/src/containers/Footer/Footer.js
@@ -2,14 +2,45 @@ import React, { Component } from "react";
 import { withTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
 import { NavHashLink } from "react-router-hash-link";
-import { Row, Col, Form, FormGroup } from "reactstrap";
 import Swal from "sweetalert2";
+import styled from "styled-components";
 
 import API from "../../utils/API";
 
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  wrap: no-wrap;
+`;
+
+const ColumnContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  width: 400px;
+  margin-right: 20px;
+`;
+
+const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 20px;
+  margin-left: 20x;
+  padding: 0 10px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 40px;
+`;
+
 import "./Footer.scss";
 import { colors } from "colors";
-//import EVAIcon from "../../components/UI/EVAIcon/EVAIcon";
+// //import EVAIcon from "../../components/UI/EVAIcon/EVAIcon";
 import FButton from "../../components/FigmaUI/FButton/FButton";
 
 export class Footer extends Component {
@@ -57,157 +88,110 @@ export class Footer extends Component {
     const { t } = this.props;
     return (
       <div className="animated fadeIn footer">
-        <Row className="negative-margin">
-          <Col xl="8" lg="8" md="8" sm="12" xs="12" className="right-col mt-10">
-            <Row className="main-row negative-margin">
-              <Col xl="4" lg="4" md="4" sm="4" xs="12" className="mt-10">
-                <h5 className="footer-header">
+        <MainContainer>
+          <ColumnContainer>
+            <TextContainer>
+              <h5 className="footer-header">
+                {
+                  "Réfugiés.info est un portail d’information collaboratif porté par la "
+                }
+                <a
+                  style={{ textDecoration: "underline" }}
+                  target="_blank"
+                  href="https://accueil-integration-refugies.fr/"
+                  rel="noopener noreferrer"
+                >
                   {
-                    "Réfugiés.info est un portail d’information collaboratif porté par la "
+                    "Délégation interministérielle à l’accueil et l’intégration des réfugiés"
                   }
-                  <a
-                    style={{ textDecoration: "underline" }}
-                    target="_blank"
-                    href="https://accueil-integration-refugies.fr/"
-                    rel="noopener noreferrer"
-                  >
-                    {
-                      "Délégation interministérielle à l’accueil et l’intégration des réfugiés"
-                    }
-                  </a>
-                  {" et développé par la "}
-                  <a
-                    style={{ textDecoration: "underline" }}
-                    target="_blank"
-                    href="https://lamednum.coop/"
-                    rel="noopener noreferrer"
-                  >
-                    {"Mednum"}
-                  </a>
-                </h5>
-              </Col>
-              <Col xl="4" lg="4" md="4" sm="4" xs="12" className="mt-10">
-                <div className="lien-footer">
-                  <NavHashLink to="/comment-contribuer">
-                    {t(
-                      "CommentContribuer.Participer / Contribuer",
-                      "Participer / Contribuer"
-                    )}
-                  </NavHashLink>
-                </div>
-                <div className="lien-footer">
-                  <NavLink to="/advanced-search">
-                    {t(
-                      "Dispositif d'accompagnement",
-                      "Chercher de l'information"
-                    )}
-                  </NavLink>
-                </div>
-                <div className="lien-footer">
-                  <NavLink to="/annuaire">
-                    {t(
-                      "Homepage.Consulter l’annnuaire",
-                      "Consulter l'annuaire"
-                    )}
-                  </NavLink>
-                </div>
-                <div className="lien-footer">
-                  <a href="https://avec.refugies.info/">
-                    {t(
-                      "QuiSommesNous.réseau",
-                      "Rejoindre le réseau des contributeurs"
-                    )}
-                  </a>
-                </div>
-              </Col>
-              <Col xl="4" lg="4" md="4" sm="4" xs="12" className="mt-10">
-                <div className="lien-footer">
-                  <NavHashLink to="/qui-sommes-nous">
-                    {t("Qui sommes-nous ?", "Qui sommes-nous ?")}
-                  </NavHashLink>
-                </div>
-                <div className="lien-footer">
-                  <a onClick={() => window.$crisp.push(["do", "chat:open"])}>
-                    {t("Démarche administrative", "Contacter l'èquipe")}
-                  </a>
-                </div>
-                <div className="lien-footer">
-                  <NavLink to="/politique-de-confidentialite">
-                    {t(
-                      "Politique de confidentialité",
-                      "Politique de confidentialité"
-                    )}
-                  </NavLink>
-                </div>
-                <div className="lien-footer">
-                  <NavLink to="/mentions-legales">
-                    {t("Mentions légales", "Mentions légales")}
-                  </NavLink>
-                </div>
-              </Col>
-            </Row>
-          </Col>
-          <Col xl="4" lg="4" md="4" sm="12" xs="12" className="mt-10">
-            <Form onSubmit={this.sendMail}>
-              <FormGroup>
-                {/*<Label for="emailNewsletter">Rejoindre la newsletter</Label>*/}
-                <div className="position-relative">
-                  {/* <Input
-                    type="email"
-                    name="email"
-                    value={this.state.email}
-                    onChange={this.onChange}
-                    id="emailNewsletter"
-                    placeholder={t(
-                      "Footer.Je m'abonne à la newsletter",
-                      "S'inscrire à la lettre d'information"
-                    )}
-                  />
-                  <InputGroupAddon
-                    addonType="append"
-                    className="icon-append"
-                    onClick={this.sendMail}
-                  >
-                    <EVAIcon name="paper-plane-outline" fill={colors.noir} />
-                  </InputGroupAddon> */}
-                  <FButton
-                    tag={"a"}
-                    href="https://refugies-info.mn.co"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="footer-btn"
-                    type="light-action"
-                    name="email"
-                    fill={colors.noir}
-                  >
-                    {t(
-                      "Footer.Je m'abonne à la newsletter",
-                      "S'inscrire à la lettre d'information"
-                    )}
-                  </FButton>
-                </div>
-              </FormGroup>
-            </Form>
-            <div className="ligne-footer">
-              {/*<label>Rejoindre la newsletter</label>*/}
-              {/* <FButton
+                </a>
+                {" et développé par la "}
+                <a
+                  style={{ textDecoration: "underline" }}
+                  target="_blank"
+                  href="https://lamednum.coop/"
+                  rel="noopener noreferrer"
+                >
+                  {"Mednum"}
+                </a>
+              </h5>
+            </TextContainer>
+            <LinkContainer>
+              <div className="lien-footer">
+                <NavHashLink to="/comment-contribuer">
+                  {t(
+                    "CommentContribuer.Participer / Contribuer",
+                    "Participer / Contribuer"
+                  )}
+                </NavHashLink>
+              </div>
+              <div className="lien-footer">
+                <NavLink to="/advanced-search">
+                  {t(
+                    "Dispositif d'accompagnement",
+                    "Chercher de l'information"
+                  )}
+                </NavLink>
+              </div>
+              <div className="lien-footer">
+                <NavLink to="/annuaire">
+                  {t("Homepage.Consulter l’annnuaire", "Consulter l'annuaire")}
+                </NavLink>
+              </div>
+              <div className="lien-footer">
+                <a href="https://avec.refugies.info/">
+                  {t(
+                    "QuiSommesNous.réseau",
+                    "Rejoindre le réseau des contributeurs"
+                  )}
+                </a>
+              </div>
+            </LinkContainer>
+            <LinkContainer>
+              <div className="lien-footer">
+                <NavHashLink to="/qui-sommes-nous">
+                  {t("Qui sommes-nous ?", "Qui sommes-nous ?")}
+                </NavHashLink>
+              </div>
+              <div className="lien-footer">
+                <a onClick={() => window.$crisp.push(["do", "chat:open"])}>
+                  {t("Démarche administrative", "Contacter l'èquipe")}
+                </a>
+              </div>
+              <div className="lien-footer">
+                <NavLink to="/politique-de-confidentialite">
+                  {t(
+                    "Politique de confidentialité",
+                    "Politique de confidentialité"
+                  )}
+                </NavLink>
+              </div>
+              <div className="lien-footer">
+                <NavLink to="/mentions-legales">
+                  {t("Mentions légales", "Mentions légales")}
+                </NavLink>
+              </div>
+            </LinkContainer>
+          </ColumnContainer>
+          <ButtonContainer>
+            <div className="position-relative">
+              <FButton
                 tag={"a"}
-                href="https://refugies-info.mn.co"
+                href=""
                 target="_blank"
                 rel="noopener noreferrer"
                 className="footer-btn"
                 type="light-action"
-                name="people-outline"
+                name="email"
                 fill={colors.noir}
               >
                 {t(
-                  "Footer.Participer à l'évolution de réfugiés-info",
-                  "Participer à l'évolution de réfugiés.info"
+                  "Footer.Je m'abonne à la newsletter",
+                  "S'inscrire à la lettre d'information"
                 )}
-              </FButton> */}
+              </FButton>
             </div>
             <div className="ligne-footer">
-              {/*<label>Demander de nouveaux contenus</label>*/}
               <FButton
                 tag={"a"}
                 href="https://refugies.canny.io/"
@@ -225,7 +209,6 @@ export class Footer extends Component {
               </FButton>
             </div>
             <div className="ligne-footer help">
-              {/*<label>Demander de nouveaux contenus</label>*/}
               <FButton
                 tag={"a"}
                 href="https://help.refugies.info/fr/"
@@ -239,8 +222,8 @@ export class Footer extends Component {
                 {t("Footer.Centre d'aide", "Consulter le centre d'aide")}
               </FButton>
             </div>
-          </Col>
-        </Row>
+          </ButtonContainer>
+        </MainContainer>
       </div>
     );
   }
