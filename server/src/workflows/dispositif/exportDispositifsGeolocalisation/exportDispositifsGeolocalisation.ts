859c68445f57d2bbc94f3ce603e33fd4605f33ae
agathekieny <agkieny@gmail.com>
2021-05-06 17:47:44 +0200

    :art: export dep

--- server/src/workflows/dispositif/exportDispositifsGeolocalisation/exportDispositifsGeolocalisation.ts
+++ server/src/workflows/dispositif/exportDispositifsGeolocalisation/exportDispositifsGeolocalisation.ts
@@ -1,5 +1,52 @@
 import { RequestFromClient, Res } from "../../../types/interface";
-import logger = require("../../../logger");
+import logger from "../../../logger";
+import { getActiveDispositifsFromDBWithoutPopulate } from "../../../modules/dispositif/dispositif.repository";
+import {
+  adaptDispositifDepartement,
+  getDepartementsFigures,
+} from "../../../modules/dispositif/dispositif.adapter";
+var Airtable = require("airtable");
+var base = new Airtable({ apiKey: process.env.airtableApiKey }).base(
+  process.env.AIRTABLE_BASE_USERS
+);
+
+const exportDataInAirtable = (data: {
+  departement: string;
+  region: string;
+  nbDispositifs: number;
+}) => {
+  base("Departements RI").create(
+    [
+      {
+        fields: {
+          Département: data.departement,
+          "Nb dispositifs": data.nbDispositifs,
+          Région: data.region,
+        },
+      },
+    ],
+    function (err: Error) {
+      if (err) {
+        logger.error(
+          "[exportDataInAirtable] error while exporting data to airtable",
+          {
+            dep: data.departement,
+            region: data.region,
+            nb: data.nbDispositifs,
+            error: err.message,
+          }
+        );
+        return;
+      }
+
+      logger.info("[exportDataInAirtable] successfully exported data", {
+        dep: data.departement,
+        region: data.region,
+        nb: data.nbDispositifs,
+      });
+    }
+  );
+};
 
 export const exportDispositifsGeolocalisation = async (
   req: RequestFromClient<{}>,
@@ -7,6 +54,17 @@ export const exportDispositifsGeolocalisation = async (
 ) => {
   try {
     logger.info("[exportDispositifsGeolocalisation] received");
+    logger.info("[getNbDispositifsByRegion]");
+    const neededFields = { contenu: 1 };
+    const activeDispositifs = await getActiveDispositifsFromDBWithoutPopulate(
+      neededFields
+    );
+
+    const adaptedDispositifs = adaptDispositifDepartement(activeDispositifs);
+    const depFigures = getDepartementsFigures(adaptedDispositifs);
+    depFigures.forEach((data) => {
+      exportDataInAirtable(data);
+    });
 
     return res.status(200).json({ text: "OK" });
   } catch (error) {
