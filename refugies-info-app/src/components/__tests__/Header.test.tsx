5963ebe5629e314e4c30d412c0136e4d4acc0d25
agathekieny <agkieny@gmail.com>
2021-05-26 17:26:17 +0200

    :art: add tests

--- refugies-info-app/src/components/__tests__/Header.test.tsx
+++ refugies-info-app/src/components/__tests__/Header.test.tsx
@@ -1,8 +1,21 @@
 import { wrapWithProvidersAndRender } from "../../jest/wrapWithProvidersAndRender";
 import { Header } from "../Header";
+import i18n from "../../services/i18n";
+
+jest.mock("../../services/i18n", () => ({
+  __esModule: true, // this property makes it work
+  default: { isRTL: jest.fn() },
+}));
 
 describe("Header", () => {
-  it("should render correctly", () => {
+  it("should render correctly with LTR", () => {
+    i18n.isRTL.mockReturnValueOnce(false);
+    const component = wrapWithProvidersAndRender({ Component: Header });
+    expect(component).toMatchSnapshot();
+  });
+
+  it("should render correctly with RTL", () => {
+    i18n.isRTL.mockReturnValueOnce(true);
     const component = wrapWithProvidersAndRender({ Component: Header });
     expect(component).toMatchSnapshot();
   });
