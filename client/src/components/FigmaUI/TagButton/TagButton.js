66c48a7454c8f65c96cdeece8900d2d28d612a8b
Camille Saillard <camillesaillard@MacBook-Air-de-Camille.local>
2021-05-28 15:22:06 +0200

    Tag Button no dark color

--- client/src/components/FigmaUI/TagButton/TagButton.js
+++ client/src/components/FigmaUI/TagButton/TagButton.js
@@ -1,21 +1,32 @@
+/* eslint-disable no-console */
 import React from "react";
 
 import "./TagButton.scss";
 import EVAIcon from "../../UI/EVAIcon/EVAIcon";
 
 const fSearchBtn = (props) => {
-  let { className, desactiver, active, color, lighter, noHover, filter, ...bProps } = props;
+  let {
+    className,
+    desactiver,
+    active,
+    color,
+    lighter,
+    noHover,
+    filter,
+    ...bProps
+  } = props;
 
   let onCrossClick = (e) => {
     e.stopPropagation();
     desactiver();
   };
+  console.log("bProps", bProps);
   return (
     <button
       className={
         (noHover ? "static-btn " : filter ? "filter-btn" : "search-btn ") +
         (className || "") +
-        (active ? " active" : "") +
+        //(active ? " active" : "") +
         (color ? " color bg-" + color : "") +
         (lighter ? " lighter" : "")
       }
