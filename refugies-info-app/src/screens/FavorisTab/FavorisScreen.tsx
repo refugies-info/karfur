e60c303ba5f54140216b77a89cd4ac3855b55098
agathekieny <agkieny@gmail.com>
2021-05-31 11:28:58 +0200

    :art: add header in favoris screen

--- refugies-info-app/src/screens/FavorisTab/FavorisScreen.tsx
+++ refugies-info-app/src/screens/FavorisTab/FavorisScreen.tsx
@@ -3,13 +3,26 @@ import { TextNormal } from "../../components/StyledText";
 import { View } from "react-native";
 import { t } from "../../services/i18n";
 import { Header } from "../../components/Header";
+import { useSelector } from "react-redux";
+import {
+  currentI18nCodeSelector,
+  selectedI18nCodeSelector,
+} from "../../services/redux/User/user.selectors";
 
-export const FavorisScreen = () => (
-  <View>
-    <Header />
-    <TextNormal>Favoris screen</TextNormal>
+export const FavorisScreen = () => {
+  const currentLanguageI18nCode = useSelector(currentI18nCodeSelector);
+  const selectedLanguageI18nCode = useSelector(selectedI18nCodeSelector);
 
-    <TextNormal>{t("lists", "options")}</TextNormal>
-    <TextNormal>{t("homepage.test", "options")}</TextNormal>
-  </View>
-);
+  return (
+    <View>
+      <Header
+        currentLanguageI18nCode={currentLanguageI18nCode}
+        selectedLanguageI18nCode={selectedLanguageI18nCode}
+      />
+      <TextNormal>Favoris screen</TextNormal>
+
+      <TextNormal>{t("lists", "options")}</TextNormal>
+      <TextNormal>{t("homepage.test", "options")}</TextNormal>
+    </View>
+  );
+};
