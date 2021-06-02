0b4716091f083e6ff5883a71cd9a5f4df15d73d6
agathekieny <agkieny@gmail.com>
2021-06-02 15:10:09 +0200

    :art: change header in rtl

--- refugies-info-app/src/components/Header.tsx
+++ refugies-info-app/src/components/Header.tsx
@@ -5,10 +5,10 @@ import Logo from "../theme/images/logo.svg";
 import { SmallButton } from "./SmallButton";
 import { SafeAreaView } from "react-native-safe-area-context";
 import i18n from "../services/i18n";
-import { RTLView } from "./BasicComponents";
+import { RowContainer } from "./BasicComponents";
 import { LanguageSwitch } from "./Language/LanguageSwitch";
 
-const MainContainer = styled(RTLView)`
+const MainContainer = styled(RowContainer)`
   padding-left: ${(props: { isRTL: any }) =>
     props.isRTL ? theme.margin * 3 : theme.margin * 3}px;
   padding-right: ${(props: { isRTL: any }) =>
@@ -36,14 +36,14 @@ export const Header = ({
   <SafeAreaView>
     <MainContainer isRTL={i18n.isRTL()}>
       <Logo width={LOGO_WIDTH} height={LOGO_HEIGHT} />
-      <RTLView>
+      <RowContainer>
         <LanguageSwitch
           currentLanguageI18nCode={currentLanguageI18nCode}
           selectedLanguageI18nCode={selectedLanguageI18nCode}
           onLongPressSwitchLanguage={onLongPressSwitchLanguage}
         />
         <SmallButton />
-      </RTLView>
+      </RowContainer>
     </MainContainer>
   </SafeAreaView>
 );
