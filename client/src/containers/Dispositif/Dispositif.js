18ddd23096c9e3699fc4ebbd6b806848e1b107bd
Your Name <you@example.com>
2021-06-10 11:07:02 +0200

    fix sticky menu

--- client/src/containers/Dispositif/Dispositif.js
+++ client/src/containers/Dispositif/Dispositif.js
@@ -203,9 +203,6 @@ export class Dispositif extends Component {
   componentWillUnmount() {
     this._isMounted = false;
     clearInterval(this.timer);
-    if (!isMobile) {
-      window.removeEventListener("scroll", this.handleScroll);
-    }
   }
 
   checkUserFetchedAndInitialize = () => {
@@ -213,25 +210,6 @@ export class Dispositif extends Component {
       this._initializeDispositif(this.props);
     } else {
       setTimeout(this.checkUserFetchedAndInitialize, 100); // check again in a 100 ms
-    }
-  };
-
-  handleScroll = () => {
-    const menu = document.querySelector(".left-side-col");
-    const content = document.querySelector(".col-middle");
-    let posYNav = menu.offsetTop;
-    {
-      if (window.scrollY > posYNav + 100) {
-        menu.style.position = "fixed";
-        menu.style.top = "75px";
-        menu.style.height = "100vh";
-        content.style.marginLeft = "25%";
-      }
-      if (window.scrollY < posYNav + 100) {
-        menu.style.position = "relative";
-        menu.style.top = "0px";
-        content.style.marginLeft = "0%";
-      }
     }
   };
 
