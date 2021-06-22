e7fccefe240b95928ec00b246a57693ed8cf4585
Camille <cssaillard@gmail.com>
2021-06-22 17:06:25 +0200

    acces structure

--- client/src/containers/Backend/UserStructure/UserStructureAdmin.component.tsx
+++ client/src/containers/Backend/UserStructure/UserStructureAdmin.component.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { UserStructureLoading } from "./components/UserStructureLoading";
