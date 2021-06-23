b40146f26928e5f2210a805bc93229f28ca37a38
agathekieny <agkieny@gmail.com>
2021-06-23 14:44:40 +0200

    :art: fix test contents

--- server/src/workflows/dispositif/getContentsForApp/getContentsForApp.test.ts
+++ server/src/workflows/dispositif/getContentsForApp/getContentsForApp.test.ts
@@ -20,6 +20,7 @@ describe("getDispositifsWithTranslationAvancement", () => {
   const neededFields = {
     titreInformatif: 1,
     titreMarque: 1,
+    avancement: 1,
   };
 
   it("should return 405 if not from site", async () => {
@@ -40,17 +41,21 @@ describe("getDispositifsWithTranslationAvancement", () => {
     expect(res.status).toHaveBeenCalledWith(400);
   });
 
-  it("should return 200", async () => {
-    const contents = [{ titreInformatif: "tI", titreMarque: "TM" }];
+  it("should return 200 when locale not fr", async () => {
+    const contents = [{ titreInformatif: "ti", titreMarque: "TM" }];
     getActiveContents.mockResolvedValueOnce(contents);
     const req = { fromSite: true, query: { locale: "ar" } };
     await getContentsForApp(req, res);
     expect(getActiveContents).toHaveBeenCalledWith(neededFields);
     expect(res.status).toHaveBeenCalledWith(200);
-    expect(res.json).toHaveBeenCalledWith({ text: "Succès", data: contents });
+    expect(res.json).toHaveBeenCalledWith({
+      text: "Succès",
+      data: contents,
+      dataFr: contents,
+    });
   });
 
-  it("should return 200", async () => {
+  it("should return 200 when locale is ar", async () => {
     const contentsInput = [
       {
         titreInformatif: { ar: "tI_ar", fr: "ti" },
@@ -65,6 +70,25 @@ describe("getDispositifsWithTranslationAvancement", () => {
     expect(res.json).toHaveBeenCalledWith({
       text: "Succès",
       data: [{ titreInformatif: "tI_ar", titreMarque: "TM_ar" }],
+      dataFr: [{ titreInformatif: "ti", titreMarque: "tm" }],
+    });
+  });
+
+  it("should return 200 when locale is fr", async () => {
+    const contentsInput = [
+      {
+        titreInformatif: { ar: "tI_ar", fr: "ti" },
+        titreMarque: { ar: "TM_ar", fr: "tm" },
+      },
+    ];
+    getActiveContents.mockResolvedValueOnce(contentsInput);
+    const req = { fromSite: true, query: { locale: "fr" } };
+    await getContentsForApp(req, res);
+    expect(getActiveContents).toHaveBeenCalledWith(neededFields);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      text: "Succès",
+      dataFr: [{ titreInformatif: "ti", titreMarque: "tm" }],
     });
   });
 
