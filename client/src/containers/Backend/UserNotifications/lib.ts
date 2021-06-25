c1564223a94a5471390a4e462d78044138bc4049
Camille <cssaillard@gmail.com>
2021-06-25 17:35:02 +0200

    fix order notif

--- client/src/containers/Backend/UserNotifications/lib.ts
+++ client/src/containers/Backend/UserNotifications/lib.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import { IDispositif } from "../../../types/interface";
 import { FormattedNotification } from "./types";
 
@@ -73,7 +72,6 @@ export const formatNotifications = (
     if (a.createdAt && b.createdAt && a.createdAt > b.createdAt) return -1;
     return 1;
   });
-  console.log("result", result);
   return result;
 };
 
