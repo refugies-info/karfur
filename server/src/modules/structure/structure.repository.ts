d097bacba9ac9bcc64094f6a727491277b5b4720
Camille <cssaillard@gmail.com>
2021-07-08 16:54:58 +0200

    add type in db

--- server/src/modules/structure/structure.repository.ts
+++ server/src/modules/structure/structure.repository.ts
@@ -26,7 +26,12 @@ export const getStructureFromDB = async (
 
 type Query = { status: "Actif" } | {};
 type NeededFields =
-  | { nom: number; acronyme: number; picture: number }
+  | {
+      nom: number;
+      acronyme: number;
+      picture: number;
+      structureTypes: number;
+    }
   | {
       nom: number;
       status: number;
