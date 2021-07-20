58e65d94db123ba8860af16bd95d4f9d58a070e2
agathekieny <agkieny@gmail.com>
2021-07-20 11:40:59 +0200

    :art: zoom on markers

--- refugies-info-app/src/components/Content/Map.tsx
+++ refugies-info-app/src/components/Content/Map.tsx
@@ -17,47 +17,71 @@ const styles = StyleSheet.create({
   },
 });
 
-interface Props {
+interface PropsType {
   map: MapGoogle;
   markersColor: string;
 }
-export const Map = (props: Props) => {
-  const markers = props.map.markers;
 
-  const onMarkerClick = (marker: MarkerGoogle) => {
+interface StateType {}
+
+const DEFAULT_PADDING = { top: 50, right: 50, bottom: 50, left: 50 };
+export class Map extends React.Component<PropsType, StateType> {
+  componentDidMount() {
+    if (this.props.map.markers.length > 0) {
+      this.fitAllMarkers();
+    }
+  }
+  onMarkerClick = (marker: MarkerGoogle) => {
     console.log("onmarker click", marker);
   };
-  return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 47,
-          longitude: 2,
-          latitudeDelta: 10,
-          longitudeDelta: 5,
-        }}
-      >
-        {markers.map((marker, key) => {
-          return (
-            <Marker
-              key={key}
-              coordinate={{
-                latitude: marker.latitude,
-                longitude: marker.longitude,
-              }}
-              onPress={() => onMarkerClick(marker)}
-            >
-              <Icon
-                name="pin"
-                fill={props.markersColor}
-                width={40}
-                height={40}
-              />
-            </Marker>
-          );
-        })}
-      </MapView>
-    </View>
-  );
-};
+
+  fitAllMarkers() {
+    // @ts-ignore
+    this.map.fitToCoordinates(this.props.map.markers, {
+      edgePadding: DEFAULT_PADDING,
+      animated: true,
+    });
+  }
+
+  render() {
+    const markers = this.props.map.markers;
+
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 47,
+            longitude: 2,
+            latitudeDelta: 10,
+            longitudeDelta: 5,
+          }}
+          ref={(ref) => {
+            // @ts-ignore
+            this.map = ref;
+          }}
+        >
+          {markers.map((marker, key) => {
+            return (
+              <Marker
+                key={key}
+                coordinate={{
+                  latitude: marker.latitude,
+                  longitude: marker.longitude,
+                }}
+                onPress={() => this.onMarkerClick(marker)}
+              >
+                <Icon
+                  name="pin"
+                  fill={this.props.markersColor}
+                  width={40}
+                  height={40}
+                />
+              </Marker>
+            );
+          })}
+        </MapView>
+      </View>
+    );
+  }
+}
