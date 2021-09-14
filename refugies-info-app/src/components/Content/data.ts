322b5b9236aeeb752c0c4ead9abd4b40c19724b9
agathekieny <agkieny@gmail.com>
2021-09-14 15:08:28 +0200

    :art: move app

--- refugies-info-app/src/components/Content/data.ts
+++
@@ -1,7 +0,0 @@
-export const infocardsCorrespondencyNames = [
-  { databaseName: "Zone d'action", displayedName: "Départements" },
-  { databaseName: "Âge requis", displayedName: "Âge" },
-  { databaseName: "Niveau de français", displayedName: "Niveau de français" },
-  { databaseName: "Combien ça coûte ?", displayedName: "Prix" },
-  { databaseName: "Durée", displayedName: "Durée" },
-];
