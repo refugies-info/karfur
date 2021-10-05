1f1ca59c193e4bce629a77756456b33f8607a8d8
agathekieny <agkieny@gmail.com>
2021-10-05 11:36:40 +0200

    :art: add spaces

--- src/libs/content.ts
+++ src/libs/content.ts
@@ -54,8 +54,10 @@ export const getDescription = (infocard: DispositifContent, t: any) => {
   if (infocard.title === "Âge requis") {
     if (infocard.contentTitle === "Plus de ** ans") {
       const result =
-        t("Content.Plus de", "Plus de ") +
+        t("Content.Plus de", "Plus de") +
+        " " +
         infocard.bottomValue +
+        " " +
         t("Content.ans", " ans");
 
       return result;
@@ -63,8 +65,10 @@ export const getDescription = (infocard: DispositifContent, t: any) => {
 
     if (infocard.contentTitle === "Moins de ** ans") {
       const result =
-        t("Content.Moins de", "Moins de ") +
+        t("Content.Moins de", "Moins de") +
+        " " +
         infocard.topValue +
+        " " +
         t("Content.ans", " ans");
 
       return result;
@@ -72,11 +76,15 @@ export const getDescription = (infocard: DispositifContent, t: any) => {
 
     if (infocard.contentTitle === "De ** à ** ans") {
       const result =
-        t("Content.De", "De ") +
+        t("Content.De", "De") +
+        " " +
         infocard.bottomValue +
-        t("Content.à", " à ") +
+        " " +
+        t("Content.à", "à") +
+        " " +
         infocard.topValue +
-        t("Content.ans", " ans");
+        " " +
+        t("Content.ans", "ans");
 
       return result;
     }
