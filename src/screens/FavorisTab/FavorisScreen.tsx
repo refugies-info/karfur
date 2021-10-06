d3af10ca90b0296a0da4446128709da40a8f20c4
agathekieny <agkieny@gmail.com>
2021-10-06 17:09:06 +0200

    :art: gitm
    remove test

--- src/screens/FavorisTab/FavorisScreen.tsx
+++ src/screens/FavorisTab/FavorisScreen.tsx
@@ -6,8 +6,6 @@ export const FavorisScreen = () => {
   return (
     <WrapperWithHeaderAndLanguageModal>
       <TextNormal>Favoris screen</TextNormal>
-      <TextNormal>{"api url : " + process.env.API_URL}</TextNormal>
-      <TextNormal>{"test key : " + process.env.TEST}</TextNormal>
     </WrapperWithHeaderAndLanguageModal>
   );
 };
