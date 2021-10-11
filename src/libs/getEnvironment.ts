e0c6e49b0dc74f68aec87810cb24c6d0df03e945
agathekieny <agkieny@gmail.com>
2021-10-11 11:15:50 +0200

    :art: expo go on prod

--- src/libs/getEnvironment.ts
+++ src/libs/getEnvironment.ts
@@ -4,15 +4,13 @@ export const getEnvironment = () => {
   if (Updates.releaseChannel.startsWith("staging")) {
     return {
       envName: "STAGING",
-      dbUrl: "https://api.staging.refugies.info",
+      dbUrl: "https://api.new.refugies.info",
       siteSecret: process.env.SITE_SECRET,
     };
   }
   return {
     envName: "DEVELOPMENT",
-    // dbUrl: process.env.API_URL,
-    dbUrl: "https://api.staging.refugies.info",
-
+    dbUrl: process.env.API_URL,
     siteSecret: process.env.SITE_SECRET,
   };
 };
