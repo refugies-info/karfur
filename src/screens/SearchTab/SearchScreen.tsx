20099a89111178c6e9547dabecd758794c5e2489
agathekieny <agkieny@gmail.com>
2021-10-12 10:57:27 +0200

    :art: change test

--- src/screens/SearchTab/SearchScreen.tsx
+++ src/screens/SearchTab/SearchScreen.tsx
@@ -3,15 +3,15 @@ import { TextNormal } from "../../components/StyledText";
 import { WrapperWithHeaderAndLanguageModal } from "../WrapperWithHeaderAndLanguageModal";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { logEventInFirebase } from "../../utils/logEvent";
+import { getEnvironment } from "../../libs/getEnvironment";
 
 export const SearchScreen = () => {
+  const { envName, debugModeFirebase } = getEnvironment();
   return (
     <WrapperWithHeaderAndLanguageModal>
       <TextNormal>Search screen</TextNormal>
-      <TextNormal>{"env name : " + process.env.ENV_NAME}</TextNormal>
-      <TextNormal>
-        {"debug mode : " + process.env.DEBUG_MODE_FIREBASE}
-      </TextNormal>
+      <TextNormal>{"env name : " + envName}</TextNormal>
+      <TextNormal>{"debug mode : " + debugModeFirebase}</TextNormal>
 
       <TouchableOpacity
         style={{
