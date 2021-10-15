c8237c18c32328b8793a831807ea4f6c951c8ae2
agathekieny <agkieny@gmail.com>
2021-10-15 10:38:15 +0200

    :art: staging in expo go

--- src/libs/getEnvironment.ts
+++ src/libs/getEnvironment.ts
@@ -4,7 +4,7 @@ export const getEnvironment = () => {
   if (Updates.releaseChannel.startsWith("staging")) {
     return {
       envName: "STAGING",
-      dbUrl: "https://api.new.refugies.info",
+      dbUrl: "https://api.staging.refugies.info",
       siteSecret: process.env.SITE_SECRET,
       debugModeFirebase: false,
     };
