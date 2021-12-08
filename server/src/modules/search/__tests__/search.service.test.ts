5d0cdc8ec634791829102caa6ca366b32d3faf27
Matthieu Fesselier <contact@matthieufesselier.com>
2021-12-08 13:08:48 +0100

    search service test

---
+++ server/src/modules/search/__tests__/search.service.test.ts
@@ -0,0 +1,69 @@
+// @ts-nocheck
+import { updateAlgoliaIndex } from "../search.service";
+
+describe("updateAlgoliaIndex", () => {
+  it("should return no update", async () => {
+    const res = await updateAlgoliaIndex([], []);
+    expect(res).toEqual({
+      added: 0,
+      deleted: 0,
+      updated: 0
+     });
+  });
+  it("should return 1 added", async () => {
+    const res = await updateAlgoliaIndex(
+      [{ objectID: "aaa" }],
+      []
+    );
+    expect(res).toEqual({
+      added: 1,
+      deleted: 0,
+      updated: 0
+     });
+  });
+  it("should return 1 deleted", async () => {
+    const res = await updateAlgoliaIndex(
+      [],
+      [{ objectID: "aaa" }]
+    );
+    expect(res).toEqual({
+      added: 0,
+      deleted: 1,
+      updated: 0
+     });
+  });
+  it("should return 1 updated", async () => {
+    const res = await updateAlgoliaIndex(
+      [{ objectID: "aaa", title_fr: "Mon titre" }],
+      [{ objectID: "aaa", title_fr: "Mon Titre" }]
+    );
+    expect(res).toEqual({
+      added: 0,
+      deleted: 0,
+      updated: 1
+     });
+  });
+  it("should return multiple changes", async () => {
+    const res = await updateAlgoliaIndex(
+      [
+        { objectID: "aaa", title_fr: "Mon titre A" },
+        { objectID: "bbb", title_fr: "Mon titre B" },
+        { objectID: "ccc", title_fr: "Mon titre C" },
+        { objectID: "ddd", title_fr: "Mon titre D" },
+      ],
+      [
+        { objectID: "aaa", title_fr: "Mon Titre A" },
+        { objectID: "bbb", title_fr: "Mon titre B" },
+        { objectID: "ccc", title_fr: "Mon titre C" },
+        { objectID: "eee", title_fr: "Mon titre E" },
+        { objectID: "fff", title_fr: "Mon titre F" },
+      ]
+    );
+    expect(res).toEqual({
+      added: 1,
+      deleted: 2,
+      updated: 1
+     });
+  });
+
+});
