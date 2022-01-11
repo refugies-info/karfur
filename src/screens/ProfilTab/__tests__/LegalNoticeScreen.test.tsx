b978aed4b6de0186e4977c66b973c9c9379ee0a5
Matthieu Fesselier <contact@matthieufesselier.com>
2022-01-11 10:18:59 +0100

    update tests

---
+++ src/screens/ProfilTab/__tests__/LegalNoticeScreen.test.tsx
@@ -0,0 +1,38 @@
+import { wrapWithProvidersAndRender } from "../../../jest/wrapWithProvidersAndRender";
+import { initialRootStateFactory } from "../../../services/redux/reducers";
+import { LegalNoticeScreen } from "../LegalNoticeScreen";
+
+jest.mock("../../../hooks/useTranslationWithRTL", () => ({
+  useTranslationWithRTL: jest.fn().mockReturnValue({
+    i18n: { changeLanguage: jest.fn() },
+    t: jest.fn().mockImplementation((_, arg2) => arg2),
+    isRTL: false,
+  }),
+}));
+
+jest.mock("react-native-safe-area-context", () => {
+  const { SafeAreaView } = jest.requireActual("react-native-safe-area-context");
+
+  return {
+    useSafeAreaInsets: () => ({ insets: { bottom: 0 } }),
+    SafeAreaView,
+  };
+});
+
+describe("LegalNotice screen", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render correctly", () => {
+    const navigation = { navigate: jest.fn() };
+    const component = wrapWithProvidersAndRender({
+      Component: LegalNoticeScreen,
+      reduxState: {
+        ...initialRootStateFactory(),
+      },
+      compProps: { navigation },
+    });
+    expect(component).toMatchSnapshot();
+  });
+});
