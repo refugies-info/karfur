7957dc95e1b8b31ef425f663637ec64bc10acdf1
Matthieu Fesselier <contact@matthieufesselier.com>
2022-01-20 18:55:59 +0100

    unused components

--- client/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.faketest.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import NavigationItem from "./NavigationItem";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<NavigationItem />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
