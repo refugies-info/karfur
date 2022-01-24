db0f92386f3d5d2a5d7b042d8c1b7a098c7d4468
Matthieu Fesselier <contact@matthieufesselier.com>
2022-01-24 09:31:03 +0100

    unauthorized access

---
+++ client/src/components/Navigation/UnauthorizedAccess/UnauthorizedAccess.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import FButton from "components/FigmaUI/FButton/FButton";
+import Link from "next/link";
+import {colors} from "colors";
+import styles from "./UnauthorizedAccess.module.scss";
+
+
+const UnauthorizedAccess = () => {
+  const { t } = useTranslation();
+  return (
+    <div className={styles.container}>
+      <h3>{t("UnauthorizedAccess.Accès refusé", "Accès refusé")}</h3>
+      <Link href="/">
+        <FButton
+          tag="a"
+          fill={colors.noir}
+          name="arrow-back-outline"
+          tabindex="1"
+        >
+          {t("UnauthorizedAccess.Revenir à l'accueil", "Revenir à l'accueil")}
+        </FButton>
+      </Link>
+    </div>
+  )
+}
+
+export default UnauthorizedAccess;
