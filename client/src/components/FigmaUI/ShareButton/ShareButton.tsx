560054bb5a643e116b04856b030d45f50fccdd24
Matthieu Fesselier <contact@matthieufesselier.com>
2022-01-24 11:09:29 +0100

    share button

--- client/src/components/FigmaUI/ShareButton/ShareButton.tsx
+++ client/src/components/FigmaUI/ShareButton/ShareButton.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import styled from "styled-components";
-// import Icon from "react-eva-icons";
+import EVAIcon from "components/UI/EVAIcon/EVAIcon";
+import styles from "./ShareButton.module.scss";
 declare const window: Window;
 
 interface Props {
@@ -11,28 +11,6 @@ interface Props {
   text: string;
   type: "a" | "button";
 }
-
-const MainContainer = styled.div`
-  background-color: white;
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 20px;
-  display: flex;
-  flex-direction: row;
-  border-radius: 12px;
-  height: 68px;
-  width: 100%;
-  padding: 24px;
-  cursor: pointer;
-  border: 0.5px solid transparent;
-  margin-top: 10px;
-  marfgin-bottom: 10px;
-  align-items: center;
-`;
-
-const TextContainer = styled.div`
-  margin-left: 10px;
-`;
 
 export const ShareButton = (props: Props) => {
   const emailBody = "Voici le lien vers cette fiche : " + window.location.href;
@@ -45,22 +23,32 @@ export const ShareButton = (props: Props) => {
   return (
     <>
       {props.type === "button" && props.onClick && (
-        <MainContainer onClick={props.onClick}>
-          {/* {props.name && <Icon name={props.name} fill="#000000" size="large" />} */}
-          {props.text && <TextContainer>{props.text}</TextContainer>}
-        </MainContainer>
+        <button onClick={props.onClick} className={styles.btn}>
+          {props.name &&
+            <EVAIcon
+              name={props.name}
+              fill={"#000000"}
+              size={"large"}
+            />
+          }
+          {props.text && <span className="ml-10">{props.text}</span>}
+        </button>
       )}
       {props.type === "a" && emailBody && (
         <a
           style={{ display: "flex", flexDirection: "row" }}
           href={`mailto:?subject=${mailSubject}&body=${emailBody}`}
         >
-          <MainContainer>
-            {/* {props.name && (
-              <Icon name={props.name} fill="#000000" size="large" />
-            )} */}
-            {props.text && <TextContainer>{props.text}</TextContainer>}
-          </MainContainer>
+          <div className={styles.btn}>
+            {props.name && (
+              <EVAIcon
+                name={props.name}
+                fill={"#000000"}
+                size={"large"}
+              />
+            )}
+            {props.text && <span className="ml-10">{props.text}</span>}
+          </div>
         </a>
       )}
     </>
