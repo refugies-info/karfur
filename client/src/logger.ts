922c3ea59fb8ddec31c7dc347d6f435aa27e9b51
Matthieu Fesselier <contact@matthieufesselier.com>
2022-02-09 17:11:52 +0100

    remove logs in production

--- client/src/logger.ts
+++ client/src/logger.ts
@@ -1,29 +1,20 @@
 /* eslint-disable no-console */
 export class logger {
   static info = (message: string, data?: any) => {
-    if (process.env.NEXT_PUBLIC_REACT_APP_ENV === "staging") {
+    if (process.env.NEXT_PUBLIC_REACT_APP_ENV !== "production") {
       console.log(message, data);
-      return;
     }
-
-    console.log(message, data);
   };
 
   static warn = (message: string, data?: any) => {
-    if (process.env.NEXT_PUBLIC_REACT_APP_ENV === "staging") {
+    if (process.env.NEXT_PUBLIC_REACT_APP_ENV !== "production") {
       console.log(message, data);
-      return;
     }
-
-    console.log(message, data);
   };
 
   static error = (message: string, data?: any) => {
-    if (process.env.NEXT_PUBLIC_REACT_APP_ENV === "staging") {
+    if (process.env.NEXT_PUBLIC_REACT_APP_ENV !== "production") {
       console.log(message, data);
-      return;
     }
-
-    console.log(message, data);
   };
 }
