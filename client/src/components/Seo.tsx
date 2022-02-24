311bb6c8714d8a525f9047f3896ce2eb93bdbc67
Matthieu Fesselier <contact@matthieufesselier.com>
2022-02-24 17:24:40 +0100

    update meta tags

--- client/src/components/Seo.tsx
+++ client/src/components/Seo.tsx
@@ -25,15 +25,32 @@ const SEO = (props: Props) => {
   return (
     <Head>
       <title>{prefixTitle + defaultTitle}</title>
-      {props.description && <meta name="description" content={props.description} />}
+      {props.description &&
+        <meta name="description" content={props.description} />
+      }
+
+      {/* OPENGRAPH */}
       <meta property="og:type" content="website" />
-      {props.title && <meta property="og:title" content={props.title} />}
-      {props.description && <meta property="og:description" content={props.description} />}
+      {props.title &&
+        <meta property="og:title" content={props.title} />
+      }
+      {props.description &&
+        <meta property="og:description" content={props.description} />
+      }
       <meta property="og:site_name" content={defaultTitle} />
+      {/* <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"> */}
+
+      {/* TWITTER */}
       <meta property="twitter:card" content="summary" />
       <meta property="twitter:creator" content="@refugies_info" />
-      {props.title && <meta property="twitter:title" content={props.title} />}
-      {props.description && <meta property="twitter:description" content={props.description} />}
+      {props.title &&
+        <meta property="twitter:title" content={props.title} />
+      }
+      {props.description &&
+        <meta property="twitter:description" content={props.description} />
+      }
+      {/* <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"> */}
+
 
       {getAlternateLocales(router.locales, router.locale).map((ln: string, i: number) => (
         <link
