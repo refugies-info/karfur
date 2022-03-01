d86c03fdf9b9296d554b3b58674e8b16848e8a71
Matthieu Fesselier <contact@matthieufesselier.com>
2022-03-01 09:55:55 +0100

    remove svgIcon.js

--- client/src/components/Pages/comment-contribuer/HeaderCard.tsx
+++ client/src/components/Pages/comment-contribuer/HeaderCard.tsx
@@ -1,57 +1,29 @@
 import React from "react";
-import styled from "styled-components";
 import EVAIcon from "components/UI/EVAIcon/EVAIcon";
-import SVGIcon from "components/UI/SVGIcon/SVGIcon";
 import icon_France from "assets/figma/icon_France.svg";
+import icon_translate from "assets/comment-contribuer/translate-icon.svg";
 import Image from "next/image";
-
-const CardContainer = styled.div`
-width: 200px;
-height: 200px;
-background-color: #ffffff;
-border-radius: 12px;
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-padding-bottom: 20px;
-padding-right: 20px;
-padding-left: 20px;
-padding-top: 10px;
-font-weight: bold;
-font-size: 40px;
-line-height: 51px;
-cursor: pointer;
-border: 4px solid #ffffff;
-
-&:hover {
-  border: 4px solid #212121;
-}
-`;
-const IconContainer = styled.div`
-  align-self: flex-end;
-  margin: 0px;
-  padding: 0px;
-`;
+import styles from "./HeaderCard.module.scss";
 
 interface Props {
-  title: string
-  eva?: boolean
-  iconName?: string
+  title: string;
+  eva?: boolean;
+  iconName?: string;
 }
 
 const HeaderCard = (props: Props) => (
-  <CardContainer>
-    <IconContainer>
+  <div className={styles.card}>
+    <div className={styles.icon}>
       {props.eva ? (
         <EVAIcon name={props.iconName} size="xlarge" fill="#212121" />
       ) : props.iconName === "icon_France" ? (
-          <Image src={icon_France} alt="icon_france" />
+        <Image src={icon_France} alt="icon_france" />
       ) : (
-        <SVGIcon name="translate" fill="#212121" width="40px" height="40px" />
+        <Image src={icon_translate} alt="icon_translate" />
       )}
-    </IconContainer>
+    </div>
     {props.title}
-  </CardContainer>
+  </div>
 );
 
 export default HeaderCard;
