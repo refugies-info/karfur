200afefe8ecb4b2a2685fd3f62b795cb06542636
Matthieu Fesselier <contact@matthieufesselier.com>
2022-03-15 18:47:58 +0100

    update team

--- client/src/data/members.ts
+++ client/src/data/members.ts
@@ -1,3 +1,9 @@
+import Alain from "assets/qui-sommes-nous/Alain.png";
+import Nour from "assets/qui-sommes-nous/Nour.png";
+import Hugo from "assets/qui-sommes-nous/Hugo.png";
+import Alice from "assets/qui-sommes-nous/Alice.png";
+import Margot from "assets/qui-sommes-nous/Margot.png";
+
 export type Member = {
   name: string
   roleShort?: string
@@ -9,52 +15,8 @@ export type Member = {
   color: string
   borderColor: string
   textColor: string
+  image: any
 }
-
-export const former_members: Member[] = [
-  {
-    name: "Simon Karleskind",
-    roleName: "Chef de projet",
-    linkedin: "https://www.linkedin.com/in/simon-karleskind/",
-    color: "#EDEBEB",
-    borderColor: "#212121",
-    textColor: "#212121",
-  },
-  {
-    name: "Luca Mazzi",
-    roleName: "Développeur",
-    linkedin: "https://www.linkedin.com/in/lmazzi/",
-    color: "#EDEBEB",
-    borderColor: "#212121",
-    textColor: "#212121",
-  },
-  {
-    name: "Soufiane Lamrissi",
-    roleShort: "Développeur",
-    roleName: "Développeur",
-    linkedin: "https://www.linkedin.com/in/soufiane-lamrissi-15b79261/",
-    twitter: "https://twitter.com/Wriri",
-    color: "#EDEBEB",
-    borderColor: "#212121",
-    textColor: "#212121",
-  },
-  {
-    name: "Chloé Vermeulin",
-    roleName: "Designer",
-    linkedin: "https://www.linkedin.com/in/chlo%C3%A9-vermeulin-a3773069/",
-    color: "#EDEBEB",
-    borderColor: "#212121",
-    textColor: "#212121",
-  },
-  {
-    name: "Ana Mylonas",
-    roleName: "Responsable éditoriale",
-    linkedin: "https://www.linkedin.com/in/ana-mylonas-scpobx/",
-    color: "#EDEBEB",
-    borderColor: "#212121",
-    textColor: "#212121",
-  },
-];
 
 export const members: Member[] = [
   {
@@ -65,6 +27,7 @@ export const members: Member[] = [
     color: "#F7CDD4",
     borderColor: "#B50437",
     textColor: "#212121",
+    image: Alain,
   },
   {
     name: "Nour Allazkani",
@@ -73,6 +36,7 @@ export const members: Member[] = [
     color: "#F9F1FD",
     borderColor: "#77057A",
     textColor: "#212121",
+    image: Nour,
   },
   {
     name: "Hugo Stéphan",
@@ -83,17 +47,8 @@ export const members: Member[] = [
     color: "#FEEBDE",
     borderColor: "#EA6206",
     textColor: "#212121",
+    image: Hugo,
   },
-
-  {
-    name: "Agathe Kieny",
-    roleName: "Développeuse",
-    linkedin: "https://www.linkedin.com/in/agathe-kieny-976720ab/",
-    color: "#EEF8FF",
-    borderColor: "#0A54BF",
-    textColor: "#212121",
-  },
-
   {
     name: "Alice Mugnier",
     roleName: "Responsable éditoriale",
@@ -103,14 +58,7 @@ export const members: Member[] = [
     color: "#EDFDF9",
     borderColor: "#187F73",
     textColor: "#212121",
-  },
-  {
-    name: "Gaël de Mondragon",
-    roleName: "Product owner",
-    linkedin: "https://fr.linkedin.com/in/gaeldemondragon",
-    color: "#DDF2F9",
-    borderColor: "#1174BA",
-    textColor: "#212121",
+    image: Alice,
   },
   {
     name: "Margot Gillette",
@@ -121,14 +69,6 @@ export const members: Member[] = [
     color: "#FFEAF4",
     borderColor: "#A81E63",
     textColor: "#212121",
-  },
-  {
-    name: "Camille Saillard",
-    roleName: "Développeuse",
-    linkedin: "https://www.linkedin.com/in/camille-saillard-8677a5a6/",
-    portfolio: "https://www.malt.fr/profile/camillesaillard",
-    color: "#F2F9E5",
-    borderColor: "#307205",
-    textColor: "#212121",
+    image: Margot,
   },
 ];
