7df28f1831f3db7dcb84fb378c556430fea0d5cd
Matthieu Fesselier <matthieu-fesselier@users.noreply.github.com>
2022-05-16 10:48:41 +0200

    bugfix 413 (#1198)

--- server/src/server.ts
+++ server/src/server.ts
@@ -36,8 +36,9 @@ mongoose
 //Body Parser
 app.use(compression());
 app.use(express.static(path.join(__dirname, "../../client", "build")));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({
+  limit: "50mb",
   extended: true,
 }));
 app.use(formData.parse());
