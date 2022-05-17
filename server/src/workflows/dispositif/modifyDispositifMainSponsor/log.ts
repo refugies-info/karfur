f3036a0ad867abd74f8af2bd9106cb53cbec6af9
Matthieu Fesselier <contact@matthieufesselier.com>
2022-05-17 09:16:28 +0200

    bugfix log structure

--- server/src/workflows/dispositif/modifyDispositifMainSponsor/log.ts
+++ server/src/workflows/dispositif/modifyDispositifMainSponsor/log.ts
@@ -1,4 +1,5 @@
 import { ObjectId } from "mongoose";
+import logger from "../../../logger";
 import { addLog } from "../../../modules/logs/logs.service";
 import { DispositifDoc } from "../../../schema/schemaDispositif";
 
@@ -8,92 +9,95 @@ export const log = async (
   sponsorId: ObjectId,
   authorId: ObjectId
 ) => {
-//@ts-ignore
-if (oldDispositif.mainSponsor !== sponsorId) {
-  await addLog(
+  try {
     //@ts-ignore
-    oldDispositif.mainSponsor,
-    "Structure",
-    "Fiche supprimée de cette structure et nouvelle structure attribuée : {{dynamic}}",
-    {
-      dynamicId: sponsorId,
-      model_dynamic: "Structure",
-      link: {
-        id: dispositifId,
-        model_link: "Dispositif",
-        next: "ModalContenu"
-      },
-      author: authorId
+    if (oldDispositif.mainSponsor && oldDispositif.mainSponsor !== sponsorId) {
+      await addLog(
+        //@ts-ignore
+        oldDispositif.mainSponsor,
+        "Structure",
+        "Fiche supprimée de cette structure et nouvelle structure attribuée : {{dynamic}}",
+        {
+          dynamicId: sponsorId,
+          model_dynamic: "Structure",
+          link: {
+            id: dispositifId,
+            model_link: "Dispositif",
+            next: "ModalContenu"
+          },
+          author: authorId
+        }
+      );
+      await addLog(
+        sponsorId,
+        "Structure",
+        "Fiche supprimée de la structure {{dynamic}} et attribuée à cette structure",
+        {
+          //@ts-ignore
+          dynamicId: oldDispositif.mainSponsor,
+          model_dynamic: "Structure",
+          link: {
+            id: dispositifId,
+            model_link: "Dispositif",
+            next: "ModalContenu"
+          },
+          author: authorId
+        }
+      );
     }
-  );
-  await addLog(
-    sponsorId,
-    "Structure",
-    "Fiche supprimée de la structure {{dynamic}} et attribuée à cette structure",
-    {
-      //@ts-ignore
-      dynamicId: oldDispositif.mainSponsor,
-      model_dynamic: "Structure",
-      link: {
-        id: dispositifId,
-        model_link: "Dispositif",
-        next: "ModalContenu"
-      },
-      author: authorId
+    //@ts-ignore
+    if (!oldDispositif.mainSponsor && sponsorId) {
+      await addLog(
+        sponsorId,
+        "Structure",
+        "Nouvelle fiche attribuée : {{dynamic}}",
+        {
+          dynamicId: dispositifId,
+          model_dynamic: "Dispositif",
+          link: {
+            id: dispositifId,
+            model_link: "Dispositif",
+            next: "ModalContenu"
+          },
+          author: authorId
+        }
+      );
     }
-  );
-}
-//@ts-ignore
-if (!oldDispositif.mainSponsor && sponsorId) {
-  await addLog(
-    sponsorId,
-    "Structure",
-    "Nouvelle fiche attribuée : {{dynamic}}",
-    {
-      dynamicId: dispositifId,
-      model_dynamic: "Dispositif",
-      link: {
-        id: dispositifId,
-        model_link: "Dispositif",
-        next: "ModalContenu"
-      },
-      author: authorId
+    //@ts-ignore
+    if (oldDispositif.mainSponsor && !sponsorId) {
+      await addLog(
+        sponsorId,
+        "Structure",
+        "Fiche supprimée de la structure : {{dynamic}} et non attribuée à une nouvelle structure",
+        {
+          //@ts-ignore
+          dynamicId: oldDispositif.mainSponsor,
+          model_dynamic: "Structure",
+          link: {
+            id: dispositifId,
+            model_link: "Dispositif",
+            next: "ModalContenu"
+          },
+          author: authorId
+        }
+      );
     }
-  );
-}
-//@ts-ignore
-if (oldDispositif.mainSponsor && !sponsorId) {
-  await addLog(
-    sponsorId,
-    "Structure",
-    "Fiche supprimée de la structure : {{dynamic}} et non attribuée à une nouvelle structure",
-    {
-      //@ts-ignore
-      dynamicId: oldDispositif.mainSponsor,
-      model_dynamic: "Structure",
-      link: {
-        id: dispositifId,
-        model_link: "Dispositif",
-        next: "ModalContenu"
-      },
-      author: authorId
-    }
-  );
-}
-await addLog(
-  dispositifId,
-  "Dispositif",
-  "Structure responsable modifiée : {{dynamic}}",
-  {
-    dynamicId: sponsorId,
-    model_dynamic: "Structure",
-    link: {
-      id: sponsorId,
-      model_link: "Structure",
-      next: "ModalStructure"
-    },
-    author: authorId
+    await addLog(
+      dispositifId,
+      "Dispositif",
+      "Structure responsable modifiée : {{dynamic}}",
+      {
+        dynamicId: sponsorId,
+        model_dynamic: "Structure",
+        link: {
+          id: sponsorId,
+          model_link: "Structure",
+          next: "ModalStructure"
+        },
+        author: authorId
+      }
+    );
+  } catch (e) {
+    logger.error("[modifyDispositifMainSponsor] log error", e);
   }
-);
-
 }
