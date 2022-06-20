170c115b69f5ee66bbbe5b80abdd299eb1d7e152
Matthieu Fesselier <contact@matthieufesselier.com>
2022-06-20 11:54:43 +0200

    html content

--- src/components/Content/TextFromHtml.tsx
+++ src/components/Content/TextFromHtml.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { theme } from "../../theme";
 import { TextSmallNormal } from "../StyledText";
 import { useTranslationWithRTL } from "../../hooks/useTranslationWithRTL";
+import { ReadableText } from "../ReadableText";
 
 interface Props {
   htmlContent: string;
@@ -11,30 +12,34 @@ export const TextFromHtml = (props: Props) => {
   const { isRTL } = useTranslationWithRTL();
 
   return (
-    <HTML
-      source={{ html: props.htmlContent }}
-      defaultTextProps={{ selectable: true }}
-      baseFontStyle={{
-        fontSize: theme.fonts.sizes.small,
-        fontFamily: theme.fonts.families.circularStandard,
-        textAlign: isRTL ? "right" : "left",
-        margin: 0,
-      }}
-      renderers={{
-        // eslint-disable-next-line react/display-name
-        p: (_, children, _cssStyles, passProps) => (
-          <TextSmallNormal
-            key={passProps.key}
-            style={{
-              flexShrink: 1,
-              marginBottom: 0,
-              padding: 0,
-            }}
-          >
-            {children}
-          </TextSmallNormal>
-        ),
-      }}
-    />
+    <>
+      <ReadableText text={props.htmlContent.replace(/<[^>]*>?/gm, "")}>
+        <HTML
+          source={{ html: props.htmlContent }}
+          defaultTextProps={{ selectable: true }}
+          baseFontStyle={{
+            fontSize: theme.fonts.sizes.small,
+            fontFamily: theme.fonts.families.circularStandard,
+            textAlign: isRTL ? "right" : "left",
+            margin: 0,
+          }}
+          renderers={{
+            // eslint-disable-next-line react/display-name
+            p: (_, children, _cssStyles, passProps) => (
+              <TextSmallNormal
+                key={passProps.key}
+                style={{
+                  flexShrink: 1,
+                  marginBottom: 0,
+                  padding: 0,
+                }}
+              >
+                {children}
+              </TextSmallNormal>
+            ),
+          }}
+        />
+      </ReadableText>
+    </>
   );
 };
