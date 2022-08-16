acc375c3ac52d98930eb478217336df9659780d4
Matthieu Fesselier <contact@matthieufesselier.com>
2022-08-16 17:01:38 +0200

    increment version number

--- app.config.js
+++ app.config.js
@@ -89,6 +89,6 @@ export default {
   description: "",
   plugins: ["./src/utils/withSimulatorExcludedArchitectures.js"],
   extra: {
-    displayVersionNumber: "2022.08.1"
+    displayVersionNumber: "2022.08.2"
   }
 };
