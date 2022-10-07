9d653981afa71e4634e2d2e9fd472583021d3430
Matthieu Fesselier <matthieu-fesselier@users.noreply.github.com>
2022-10-07 09:41:08 +0200

    fix search needs (#1338)

--- client/src/lib/index.ts
+++ client/src/lib/index.ts
@@ -3,6 +3,11 @@ export const jsUcfirst = (string: string) =>
   string.length > 1 &&
   string.charAt(0).toUpperCase() + string.slice(1, string.length);
 
+export const jsLcfirst = (string: string) =>
+  string &&
+  string.length > 1 &&
+  string.charAt(0).toLowerCase() + string.slice(1, string.length);
+
 export const limitNbCaracters = (string: string, nbCaractersMax: number) =>
   string.substring(0, Math.min(string.length, nbCaractersMax)) +
   (string.length > nbCaractersMax ? "..." : "");
