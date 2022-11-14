fd074742462480948d70d5e749347fbd495742a2
Sylvain LG <syl@sylvainlg.fr>
2022-11-14 17:55:28 +0100

    refactor: :recycle: Refactor SkeletonListPage to a simpler version

--- src/components/feedback/SkeletonListPage/SkeletonListPage.tsx
+++ src/components/feedback/SkeletonListPage/SkeletonListPage.tsx
@@ -1,39 +1,42 @@
+import React from "react";
 import SkeletonContent from "@03balogun/react-native-skeleton-content";
-import { withProps } from "../../../utils";
 import { useTheme } from "styled-components";
-import styled from "styled-components/native";
 
-const SkeletonListPage = styled(
-  withProps(() => {
-    const theme = useTheme();
-    return {
-      isLoading: true,
-      layout: [
+const SkeletonListPage = () => {
+  const theme = useTheme();
+  return (
+    <SkeletonContent
+      containerStyle={{
+        display: "flex",
+        flex: 1,
+        marginTop: theme.margin * 3,
+        marginHorizontal: theme.margin * 3,
+      }}
+      isLoading={true}
+      layout={[
         {
           key: "Section1",
           width: "100%",
           height: 80,
-          marginBottom: theme.layout.content.normalValue,
+          marginBottom: theme.margin * 3,
         },
         {
           key: "Section2",
           width: "100%",
           height: 80,
-          marginBottom: theme.layout.content.normalValue,
+          marginBottom: theme.margin * 3,
         },
         {
           key: "Section3",
           width: "100%",
           height: 80,
-          marginBottom: theme.layout.content.normalValue,
+          marginBottom: theme.margin * 3,
         },
-      ],
-      boneColor: theme.colors.grey,
-      highlightColor: theme.colors.lightGrey,
-    };
-  })(SkeletonContent)
-)`
-  padding: ${({ theme }) => theme.layout.content.normal};
-`;
+      ]}
+      boneColor={theme.colors.grey}
+      highlightColor={theme.colors.lightGrey}
+    />
+  );
+};
 
 export default SkeletonListPage;
