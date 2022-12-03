986ba0c6994e7638d58629e0b3a7f974743a6c7d
Sylvain LG <syl@sylvainlg.fr>
2022-12-04 00:40:20 +0100

    fix: :bug: Layout problem that does not correctly display the first favorite
    + Fix some UI issues : bottom margin + swipe background size

--- src/screens/FavorisTab/FavorisScreen.tsx
+++ src/screens/FavorisTab/FavorisScreen.tsx
@@ -1,14 +1,12 @@
 import * as React from "react";
-import styled from "styled-components/native";
-import { StackScreenProps } from "@react-navigation/stack";
-import { CompositeScreenProps, useFocusEffect } from "@react-navigation/native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import Swipeable from "react-native-gesture-handler/Swipeable";
-import { Icon } from "react-native-eva-icons";
-
 import { Image, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import useAsync from "react-use/lib/useAsync";
+import styled, { useTheme } from "styled-components/native";
+import { StackScreenProps } from "@react-navigation/stack";
+import { CompositeScreenProps, useFocusEffect } from "@react-navigation/native";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import { Icon } from "react-native-eva-icons";
 
 import { SimplifiedContent } from "../../types/interface";
 import { BottomTabParamList, FavorisParamList } from "../../../types";
@@ -31,7 +29,6 @@ import {
 import { CustomButton } from "../../components/CustomButton";
 import { ContentSummary } from "../../components/Contents/ContentSummary";
 import { ConfirmationModal } from "../../components/ConfirmationModal";
-import { styles } from "../../theme";
 import EmptyIllu from "../../theme/images/favoris/illu-empty-favorites.png";
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import { Page, Rows } from "../../components";
@@ -40,6 +37,11 @@ import { ReadableText } from "../../components/ReadableText";
 
 const EmptyContainer = styled.View`
   padding-horizontal: ${({ theme }) => theme.margin * 4}px;
+  align-items: center;
+  justify-content: center;
+  flex-grow: 1;
+  padding-bottom: ${({ theme }) =>
+    theme.margin * 5 + (theme.insets.bottom || 0)}px;
 `;
 const EmptyTitle = styled(StyledTextBigBold)`
   text-align: center;
@@ -54,19 +56,27 @@ const CardItem = styled.View`
   flex: 1;
 `;
 
+const ActionContainer = styled.View`
+background-color: ${({ theme }) => theme.colors.red};
+justify-content: center;
+align-items: ${({ theme }) => (theme.i18n.isRTL ? "flex-start" : "flex-end")};
+flex: 1;
+padding-horizontal: ${({ theme }) => theme.margin * 2}px
+border-radius: ${({ theme }) => theme.radius * 2}px
+`;
+
 type FavorisScreenProps = CompositeScreenProps<
   StackScreenProps<FavorisParamList, "FavorisScreen">,
   BottomTabScreenProps<BottomTabParamList>
 >;
 
 export const FavorisScreen = ({ navigation }: FavorisScreenProps) => {
+  const theme = useTheme();
   const currentLanguageI18nCode = useSelector(currentI18nCodeSelector);
   const favorites = useSelector(userFavorites);
   const contents = useSelector(contentsSelector);
   const { t, isRTL } = useTranslationWithRTL();
   const dispatch = useDispatch();
-
-  const insets = useSafeAreaInsets();
 
   // When the screen has focus, remove "new favorite" badge
   const hasNewFavorites = useSelector(hasUserNewFavoritesSelector);
@@ -104,137 +114,109 @@ export const FavorisScreen = ({ navigation }: FavorisScreenProps) => {
 
   const renderActions = () => {
     return (
-      <View
-        style={{
-          backgroundColor: styles.colors.red,
-          justifyContent: "center",
-          alignItems: !isRTL ? "flex-end" : "flex-start",
-          flex: 1,
-          paddingHorizontal: styles.margin * 2,
-          marginHorizontal: styles.margin * 3,
-          borderRadius: styles.radius * 2,
-          marginBottom: styles.margin * 2,
-        }}
-      >
+      <ActionContainer>
         <Icon
           name="trash-2-outline"
           width={24}
           height={24}
-          fill={styles.colors.white}
+          fill={theme.colors.white}
         ></Icon>
-      </View>
+      </ActionContainer>
     );
   };
 
   return (
-    <Page
-      hideBack
-      loading={loading}
-      noBottomMargin
-      title={t("favorites_screen.my_content", "Mes fiches")}
-    >
-      {favorites.length > 0 ? (
-        <Rows layout="1 auto">
-          <View
-            style={{
-              marginHorizontal: -styles.margin * 3,
-              marginTop: -styles.margin * 3,
-              justifyContent: "flex-start",
-            }}
-          >
-            {contentsToDisplay.map(
-              (content: SimplifiedContent, index: number) => (
-                <CardItem key={content._id}>
-                  <Swipeable
-                    renderRightActions={!isRTL ? renderActions : undefined}
-                    renderLeftActions={isRTL ? renderActions : undefined}
-                    leftThreshold={!isRTL ? 9999 : 120}
-                    rightThreshold={isRTL ? 9999 : 120}
-                    onSwipeableRightOpen={
-                      !isRTL ? () => deleteFavorite(content._id) : undefined
-                    }
-                    onSwipeableLeftOpen={
-                      isRTL ? () => deleteFavorite(content._id) : undefined
-                    }
-                    overshootFriction={8}
-                    childrenContainerStyle={{
-                      paddingBottom: styles.margin * 2,
-                    }}
-                  >
-                    <ContentSummary
-                      navigation={navigation}
-                      theme={content.theme}
-                      contentId={content._id}
-                      titreInfo={content.titreInformatif}
-                      titreMarque={content.titreMarque}
-                      typeContenu={content.typeContenu}
-                      sponsorUrl={content.sponsorUrl}
-                      actionPress={() => showDeleteModal(content._id)}
-                      actionIcon={"trash-2-outline"}
-                      actionLabel={t(
-                        "favorites_screen.delete_content_accessibility"
-                      )}
-                      style={{
-                        marginTop: index === 0 ? styles.margin * 3 : 0,
-                        marginHorizontal: styles.margin * 3,
-                      }}
-                      backScreen="Favoris"
-                    />
-                  </Swipeable>
-                </CardItem>
-              )
-            )}
-          </View>
+    <>
+      <Page
+        hideBack
+        loading={loading}
+        title={t("favorites_screen.my_content", "Mes fiches")}
+      >
+        {favorites.length > 0 ? (
+          <Rows layout="1 auto">
+            <View>
+              <Rows layout="1" verticalAlign="flex-start">
+                {contentsToDisplay.map(
+                  (content: SimplifiedContent, index: number) => (
+                    <CardItem key={content._id}>
+                      <Swipeable
+                        renderRightActions={!isRTL ? renderActions : undefined}
+                        renderLeftActions={isRTL ? renderActions : undefined}
+                        leftThreshold={!isRTL ? 9999 : 120}
+                        rightThreshold={isRTL ? 9999 : 120}
+                        onSwipeableRightOpen={
+                          !isRTL ? () => deleteFavorite(content._id) : undefined
+                        }
+                        onSwipeableLeftOpen={
+                          isRTL ? () => deleteFavorite(content._id) : undefined
+                        }
+                        overshootFriction={8}
+                      >
+                        <ContentSummary
+                          navigation={navigation}
+                          theme={content.theme}
+                          contentId={content._id}
+                          titreInfo={content.titreInformatif}
+                          titreMarque={content.titreMarque}
+                          typeContenu={content.typeContenu}
+                          sponsorUrl={content.sponsorUrl}
+                          actionPress={() => showDeleteModal(content._id)}
+                          actionIcon={"trash-2-outline"}
+                          actionLabel={t(
+                            "favorites_screen.delete_content_accessibility"
+                          )}
+                          backScreen="Favoris"
+                        />
+                      </Swipeable>
+                    </CardItem>
+                  )
+                )}
+              </Rows>
+            </View>
 
-          <CustomButton
-            textColor={styles.colors.black}
-            i18nKey="favorites_screen.delete_all_accessibility"
-            defaultText="Supprimer toutes mes fiches"
-            onPress={() => showDeleteModal("all")}
-            backgroundColor={styles.colors.grey60}
-            iconName="trash-2-outline"
-            iconFirst={true}
-            isTextNotBold={true}
-          />
-        </Rows>
-      ) : (
-        <EmptyContainer
-          style={{
-            alignItems: "center",
-            justifyContent: "center",
-            flexGrow: 1,
-            paddingBottom: styles.margin * 5 + (insets.bottom || 0),
-          }}
-        >
-          <Image
-            source={EmptyIllu}
-            style={{ width: 312, height: 250, marginTop: styles.margin * 4 }}
-          />
-          <EmptyTitle>
-            <ReadableText>
-              {t("favorites_screen.empty", "C'est vide")}
-            </ReadableText>
-          </EmptyTitle>
-          <EmptyText>
-            <ReadableText>
-              {t(
-                "favorites_screen.add_content",
-                "Pour ajouter une fiche dans tes favoris, clique sur l’étoile."
-              )}
-            </ReadableText>
-          </EmptyText>
-          <CustomButton
-            textColor={styles.colors.white}
-            i18nKey="tab_bar.explorer"
-            onPress={() => navigation.navigate("Explorer")}
-            defaultText="Explorer"
-            backgroundColor={styles.colors.black}
-            iconName="compass-outline"
-            iconFirst={true}
-            notFullWidth={true}
-          />
-        </EmptyContainer>
-      )}
+            <CustomButton
+              textColor={theme.colors.black}
+              i18nKey="favorites_screen.delete_all_accessibility"
+              defaultText="Supprimer toutes mes fiches"
+              onPress={() => showDeleteModal("all")}
+              backgroundColor={theme.colors.grey60}
+              iconName="trash-2-outline"
+              iconFirst
+              isTextNotBold
+            />
+          </Rows>
+        ) : (
+          <EmptyContainer>
+            <Image
+              source={EmptyIllu}
+              style={{ width: 312, height: 250, marginTop: theme.margin * 4 }}
+            />
+            <EmptyTitle>
+              <ReadableText>
+                {t("favorites_screen.empty", "C'est vide")}
+              </ReadableText>
+            </EmptyTitle>
+            <EmptyText>
+              <ReadableText>
+                {t(
+                  "favorites_screen.add_content",
+                  "Pour ajouter une fiche dans tes favoris, clique sur l’étoile."
+                )}
+              </ReadableText>
+            </EmptyText>
+            <CustomButton
+              textColor={theme.colors.white}
+              i18nKey="tab_bar.explorer"
+              onPress={() => navigation.navigate("Explorer")}
+              defaultText="Explorer"
+              backgroundColor={theme.colors.black}
+              iconName="compass-outline"
+              iconFirst
+              notFullWidth
+            />
+          </EmptyContainer>
+        )}
+      </Page>
       <ConfirmationModal
         isModalVisible={!!favoriteToDelete}
         toggleModal={hideDeleteModal}
@@ -254,6 +236,6 @@ export const FavorisScreen = ({ navigation }: FavorisScreenProps) => {
         defaultTextValidateButton={"Supprimer"}
         iconValidateButton={"trash-2-outline"}
       />
-    </Page>
+    </>
   );
 };
