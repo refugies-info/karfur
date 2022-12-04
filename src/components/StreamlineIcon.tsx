ce86d9fa77d093495b4ea5fce606dc4ebba8dc61
Sylvain LG <syl@sylvainlg.fr>
2022-12-04 01:04:32 +0100

    fix: :bug: Prevent error "Can't perform a React state update on an unmounted component" for StreamlineIcon

--- src/components/StreamlineIcon.tsx
+++ src/components/StreamlineIcon.tsx
@@ -1,33 +1,36 @@
-import React, { useEffect, useState } from "react";
+import * as React from "react";
 import { Picture } from "../types/interface";
 import { SvgXml } from "react-native-svg";
 import { getImageUri } from "../libs/getImageUri";
+import useAsync from "react-use/lib/useAsync";
+import { apiCaller } from "../utils/ConfigAPI";
 
-interface Props {
+interface StreamlineIconProps {
   icon: Picture;
   size: number;
   stroke?: string;
 }
-export const StreamlineIcon = (props: Props) => {
-  const [imgXml, setImgXml] = useState("<svg></svg>");
 
-  const getImgXml = async () => {
-    const url = getImageUri(props.icon.secure_url);
-    const xml = await (await fetch(url)).text();
-    setImgXml(xml);
-  };
-
-  useEffect(() => {
-    getImgXml();
-  }, []);
-
+export const StreamlineIcon = ({
+  icon,
+  size = 22,
+  stroke = "white",
+}: StreamlineIconProps) => {
+  const { value: imgXml = "<svg></svg>" } = useAsync(
+    () =>
+      apiCaller
+        .get(getImageUri(icon.secure_url))
+        .then((result: any) => result.data as string),
+    [icon]
+  );
   return (
     <SvgXml
-      width={props.size || 22}
-      height={props.size || 22}
-      xml={imgXml
-        .replace(/stroke="((#[0-9a-f]{6})|(#[0-9a-f]{3})|([a-z]+))"/g, `stroke="${props.stroke || "white"}"`)
-      }
+      width={size}
+      height={size}
+      xml={(imgXml as string).replace(
+        /stroke="((#[0-9a-f]{6})|(#[0-9a-f]{3})|([a-z]+))"/g,
+        `stroke="${stroke}"`
+      )}
     />
-  )
+  );
 };
