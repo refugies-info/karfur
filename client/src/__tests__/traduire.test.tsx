a91cbdc9553095c0d5f12d7277d5595a2ad545fd
Matthieu Fesselier <contact@matthieufesselier.com>
2022-12-08 10:36:48 +0100

    add snapshots

---
+++ client/src/__tests__/traduire.test.tsx
@@ -0,0 +1,42 @@
+//@ts-nocheck
+import { wrapWithProvidersAndRender } from "../../jest/lib/wrapWithProvidersAndRender";
+import traduire from "../pages/traduire";
+import { initialMockStore } from "__fixtures__/reduxStore";
+import { act } from "react-test-renderer";
+import "jest-styled-components";
+
+jest.mock("next/router", () => require("next-router-mock"));
+jest.mock("next/image", () => {
+  const Image = () => <></>;
+  return Image;
+});
+
+describe("traduire", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders traduire", () => {
+    window.scrollTo = jest.fn();
+    let component;
+    act(() => {
+      component = wrapWithProvidersAndRender({
+        Component: traduire,
+        reduxState: {
+          ...initialMockStore
+        },
+        compProps: {
+          translationStatistics: {
+            nbTranslators: 12,
+            nbWordsTranslated: 156,
+            nbActiveTranslators: [
+              { languageId: "en", count: 4 },
+              { languageId: "ru", count: 2 }
+            ]
+          }
+        }
+      });
+    });
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
