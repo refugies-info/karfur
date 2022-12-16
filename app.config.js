c8bc0892b858431e66cd81c402676ea0543269b1
Sylvain LG <syl@sylvainlg.fr>
2022-12-16 09:19:17 +0100

    ci: :construction_worker: Use eas update insteadof expo publish

--- app.config.js
+++ app.config.js
@@ -84,7 +84,82 @@ export default {
   description: "",
   plugins: ["./src/utils/withSimulatorExcludedArchitectures.js"],
   expo: {
+    name: process.env.EXPO_APP_NAME || "Réfugiés.info",
     slug: "refugies-info-app",
+    version: "1.0.16",
+    orientation: "portrait",
+    icon: "./src/theme/images/app-icon-ri.png",
+    scheme: "refugies",
+    userInterfaceStyle: "light",
+    backgroundColor: "#F6F6F6",
+    splash: {
+      image: "./src/theme/images/splash-screen-RI.png",
+      resizeMode: "contain",
+      backgroundColor: "#F6F6F6",
+    },
+    owner: "refugies-info",
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ["**/*"],
+    locales: {
+      en: "./src/translations/en.json",
+      fr: "./src/translations/fr.json",
+    },
+    description: "",
+    plugins: ["./src/utils/withSimulatorExcludedArchitectures.js"],
+    android: {
+      versionCode: 15,
+      userInterfaceStyle: "light",
+      adaptiveIcon: {
+        foregroundImage: "./src/theme/images/app-icon-ri-adaptive.png",
+        backgroundColor: "#0421B1",
+      },
+      package: "com.refugiesinfo.app",
+      // package:
+      //   "com.refugiesinfo.app" + process.env.EAS_BUILD_PROFILE !== "production"
+      //     ? `.${process.env.EAS_BUILD_PROFILE}`
+      //     : "",
+      config: {
+        googleMaps: { apiKey: process.env.GOOGLE_MAPS_KEY_ANDROID },
+      },
+      googleServicesFile: "./src/utils/firebase/google-services.json",
+      splash: {
+        backgroundColor: "#F6F6F6",
+        mdpi: "./src/theme/images/splash/splash_mdpi.png",
+        hdpi: "./src/theme/images/splash/splash_hdpi.png",
+        xhdpi: "./src/theme/images/splash/splash_xhdpi.png",
+        xxhdpi: "./src/theme/images/splash/splash_xxhdpi.png",
+        xxxhdpi: "./src/theme/images/splash/splash_xxxhdpi.png",
+      },
+      intentFilters: [
+        {
+          action: "VIEW",
+          autoVerify: true,
+          data: deepLinks,
+          category: ["BROWSABLE", "DEFAULT"],
+        },
+      ],
+    },
+    ios: {
+      buildNumber: "1.1.2",
+      supportsTablet: false,
+      userInterfaceStyle: "light",
+      bundleIdentifier: "refugiesInfo",
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_KEY_IOS,
+        usesNonExemptEncryption: false,
+      },
+      infoPlist: {
+        CFBundleAllowMixedLocalizations: true,
+        UIBackgroundModes: ["remote-notification"],
+      },
+      googleServicesFile: "./src/utils/firebase/GoogleService-Info.plist",
+      associatedDomains: [
+        "applinks:refugies.info",
+        "applinks:www.refugies.info",
+      ],
+    },
     extra: {
       eas: {
         projectId: "985bc919-57f5-4851-9f2f-748af3408606",
