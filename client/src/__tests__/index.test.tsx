463ea1760a1e3bb54fd67cf926914c8f6944a633
Matthieu Fesselier <contact@matthieufesselier.com>
2022-12-19 15:37:45 +0100

    add tests

---
+++ client/src/__tests__/index.test.tsx
@@ -0,0 +1,58 @@
+//@ts-nocheck
+import { wrapWithProvidersAndRender } from "../../jest/lib/wrapWithProvidersAndRender";
+import index from "../pages/index";
+import { initialMockStore } from "__fixtures__/reduxStore";
+import { act } from "react-test-renderer";
+import "jest-styled-components";
+import { setupGoogleMock } from "__mocks__/react-google-autocomplete";
+jest.mock("components/Modals/WriteContentModal/WriteContentModal", () => jest.fn().mockReturnValue(<></>));
+
+jest.mock("next/router", () => require("next-router-mock"));
+jest.mock("next/image", () => {
+  const Image = () => <></>;
+  return Image;
+});
+
+describe("homepage", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    setupGoogleMock();
+  });
+
+  it("renders homepage", () => {
+    window.scrollTo = jest.fn();
+    let component;
+    act(() => {
+      component = wrapWithProvidersAndRender({
+        Component: index,
+        reduxState: {
+          ...initialMockStore
+        },
+        compProps: {
+          contentStatistics: {
+            nbMercis: 2344,
+            nbVues: 120432,
+            nbVuesMobile: 100324,
+            nbDispositifs: 500,
+            nbDemarches: 80,
+            nbUpdatedRecently: 23
+          },
+          structuresStatistics: {
+            nbStructures: 320,
+            nbCDA: 24,
+            nbStructureAdmins: 300
+          },
+          translationStatistics: {
+            nbTranslators: 12,
+            nbWordsTranslated: 156,
+            nbActiveTranslators: [
+              { languageId: "en", count: 4 },
+              { languageId: "ru", count: 2 }
+            ]
+          }
+        }
+      });
+    });
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
