920b39ea12c89b8d3aca5c290961d938de760db0
Matthieu Fesselier <contact@matthieufesselier.com>
2022-12-20 15:47:35 +0100

    feat: :lipstick: add new hero background

--- client/src/components/Pages/homepage/Sections/Hero/Hero.tsx
+++ client/src/components/Pages/homepage/Sections/Hero/Hero.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Container } from "reactstrap";
 import { useTranslation } from "next-i18next";
+import Image from "next/image";
 import { cls } from "lib/classname";
 import { HeroArrow } from "components/Pages/staticPages/common";
 import HomeSearchHeader from "../../HomeSearchHeader";
 import commonStyles from "scss/components/staticPages.module.scss";
+import Character from "assets/homepage/hero/character.svg";
 import styles from "./Hero.module.scss";
 
 interface Props {
@@ -19,8 +21,11 @@ const Hero = (props: Props) => {
       <Container className={cls(commonStyles.container)}>
         <h1>{t("Homepage.title")}</h1>
         <HomeSearchHeader />
-        <HeroArrow center target={props.targetArrow} />
       </Container>
+      <HeroArrow center target={props.targetArrow} />
+      <div className={styles.bottom_img}>
+        <Image src={Character} width={207} height={274} alt="" />
+      </div>
     </div>
   );
 };
