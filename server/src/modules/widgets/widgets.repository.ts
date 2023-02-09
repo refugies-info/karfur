70cc0258e8f414485637f24ef3748cdc5d098ee9
Matthieu Fesselier <contact@matthieufesselier.com>
2023-02-09 15:06:07 +0100

    refactor: widgets workflow

--- server/src/modules/widgets/widgets.repository.ts
+++ server/src/modules/widgets/widgets.repository.ts
@@ -1,20 +1,20 @@
 import { Widget, WidgetId, WidgetModel } from "src/typegoose";
 
 export const getAllWidgets = async () => {
-  return WidgetModel.find().populate("author", "username").populate("themes").sort({ created_at: -1 });
+  return WidgetModel.find()
+    .populate<{ author: string }>("author", "username")
+    .sort({ created_at: -1 });
 };
 
 export const createWidget = async (widget: Widget) => {
   return new WidgetModel(widget)
     .save()
-    .then((w) => w.populate("author", "username"))
-    .then((w) => w.populate("themes"));
+    .then((w) => w.populate<{ author: string }>("author", "username"));
 };
 
 export const updateWidget = async (widgetId: WidgetId, widget: Partial<Widget>) => {
   return WidgetModel.findOneAndUpdate({ _id: widgetId }, widget, { upsert: true, new: true })
-    .populate("author", "username")
-    .populate("themes");
+    .populate<{ author: string }>("author", "username");
 };
 
 export const deleteWidgetById = async (widgetId: WidgetId) => {
