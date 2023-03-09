845a86ea764ae510f9d44a714892e1e812aa6f2f
Matthieu Fesselier <contact@matthieufesselier.com>
2023-03-09 09:45:09 +0100

    style: :lipstick: contributor image

--- client/src/components/Pages/dispositif/Contributors/ContributorCard/ContributorCard.tsx
+++ client/src/components/Pages/dispositif/Contributors/ContributorCard/ContributorCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { SimpleUser } from "api-types";
 import DefaultAvatar from "assets/dispositif/default-avatar.png";
+import { cls } from "lib/classname";
 import { getRole } from "./functions";
 import AdminIcon from "assets/dispositif/admin-icon.svg";
 import styles from "./ContributorCard.module.scss";
@@ -12,10 +13,17 @@ interface Props {
 
 const ContributorCard = ({ user }: Props) => {
   const role = getRole(user.roles);
+  const hasProfilePicture = !!user.picture?.secure_url;
   return (
     <div className={styles.container}>
       {/* TODO: handle image style */}
-      <Image src={user.picture?.secure_url || DefaultAvatar} width={88} height={88} alt={user.username} />
+      <Image
+        src={user.picture?.secure_url || DefaultAvatar}
+        width={88}
+        height={88}
+        alt={user.username}
+        className={cls(styles.image, hasProfilePicture && styles.profile)}
+      />
       <span className={styles.username}>{user.username}</span>
       <span className={styles.role}>
         {role}
