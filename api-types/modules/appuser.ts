afece9c079fbe3460e1a75a07ebeb78e6db928fa
Sylvain Le Gléau <syl@sylvainlg.fr>
2023-03-28 18:22:18 +0200

    fix: :bug: Set AppUserRequest attributes to nullable

--- api-types/modules/appuser.ts
+++ api-types/modules/appuser.ts
@@ -4,11 +4,11 @@ import { Languages } from "../generics";
  * @url POST /appuser
  */
 export interface AppUserRequest {
-  city?: string;
+  city?: string | null;
   department?: string | null;
   selectedLanguage?: Languages;
-  age?: string;
-  frenchLevel?: string;
+  age?: string | null;
+  frenchLevel?: string | null;
   expoPushToken?: string;
 }
 
