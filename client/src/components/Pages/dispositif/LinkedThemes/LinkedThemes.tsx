bd358ad28c3244e5c7c0f419ef1174c75bcc3190
Matthieu Fesselier <contact@matthieufesselier.com>
2023-04-05 09:22:13 +0200

    style: :lipstick: update themes buttons

--- client/src/components/Pages/dispositif/LinkedThemes/LinkedThemes.tsx
+++ client/src/components/Pages/dispositif/LinkedThemes/LinkedThemes.tsx
@@ -2,28 +2,32 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Image from "next/image";
 import Link from "next/link";
+import styled from "styled-components";
 import { getPath } from "routes";
+import { cls } from "lib/classname";
 import { getLinkedThemesReadableText } from "lib/getReadableText";
 import { buildUrlQuery } from "lib/recherche/buildUrlQuery";
 import { secondaryThemesSelector, themeSelector, themesSelector } from "services/Themes/themes.selectors";
 import { selectedDispositifSelector } from "services/SelectedDispositif/selectedDispositif.selector";
 import { dispositifNeedsSelector } from "services/Needs/needs.selectors";
-import ThemeIcon from "components/UI/ThemeIcon";
+import SearchThemeButton from "components/UI/SearchThemeButton";
 import SectionButtons from "../SectionButtons";
 import styles from "./LinkedThemes.module.scss";
 
-interface ButtonProps {
-  children?: string;
-  image: React.ReactNode;
-  color: string;
-  pathParams?: string;
+interface LinkNeedProps {
+  color100: string;
+  color40: string;
+  color30: string;
 }
-const Button = (props: ButtonProps) => (
-  <Link href={getPath("/recherche", "fr", props.pathParams)} className={styles.btn} style={{ color: props.color }}>
-    {props.children}
-    <span className="ms-2">{props.image}</span>
-  </Link>
-);
+const LinkNeed = styled(Link)<LinkNeedProps>`
+  color: ${(props) => props.color100} !important;
+  background-color: ${(props) => props.color30} !important;
+  border-color: ${(props) => props.color40} !important;
+
+  &:hover {
+    border-color: ${(props) => props.color100} !important;
+  }
+`;
 
 const LinkedThemes = () => {
   const themes = useSelector(themesSelector);
@@ -37,34 +41,38 @@ const LinkedThemes = () => {
       <p className={styles.title}>Thématiques liées</p>
       <div className={styles.row}>
         {theme && (
-          <Button
-            image={<ThemeIcon theme={theme} color={theme.colors.color100} size={16} />}
-            color={theme.colors.color100}
-            pathParams={`?${buildUrlQuery({ themes: [theme._id] })}`}
-          >
-            {theme.short?.fr}
-          </Button>
+          <SearchThemeButton
+            theme={theme}
+            href={getPath("/recherche", "fr", `?${buildUrlQuery({ themes: [theme._id] })}`)}
+            small
+            className={styles.btn}
+          />
         )}
         {secondaryThemes.map((theme, i) => (
-          <Button
+          <SearchThemeButton
             key={i}
-            image={<ThemeIcon theme={theme} color={theme.colors.color100} size={16} />}
-            color={theme.colors.color100}
-            pathParams={`?${buildUrlQuery({ themes: [theme._id] })}`}
-          >
-            {theme.short?.fr}
-          </Button>
+            theme={theme}
+            href={getPath("/recherche", "fr", `?${buildUrlQuery({ themes: [theme._id] })}`)}
+            small
+            className={styles.btn}
+          />
         ))}
-        {needs.map((need, i) => (
-          <Button
-            key={i}
-            image={<Image src={need.image?.secure_url || ""} width={16} height={16} alt="" />}
-            color={themes.find((t) => t._id === need.theme._id)?.colors.color100 || "black"}
-            pathParams={`?${buildUrlQuery({ needs: [need._id] })}`}
-          >
-            {need.fr.text}
-          </Button>
-        ))}
+        {needs.map((need, i) => {
+          const theme = themes.find((t) => t._id === need.theme._id);
+          return (
+            <LinkNeed
+              key={i}
+              href={getPath("/recherche", "fr", `?${buildUrlQuery({ needs: [need._id] })}`)}
+              className={cls(styles.btn, styles.need)}
+              color100={theme?.colors.color100 || "black"}
+              color40={theme?.colors.color40 || "#DDD"}
+              color30={theme?.colors.color30 || "#EEE"}
+            >
+              {need.fr.text}
+              <Image src={need.image?.secure_url || ""} width={32} height={32} alt="" className="ms-3" />
+            </LinkNeed>
+          );
+        })}
       </div>
       <SectionButtons id="themes" content={getLinkedThemesReadableText(theme, secondaryThemes, needs)} />
     </div>
