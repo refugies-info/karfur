f398bc843f1279e8afe51a70a00ee4b3bcd2568b
Matthieu Fesselier <contact@matthieufesselier.com>
2023-05-03 15:14:48 +0200

    feat(client): :sparkles: show error on autosave edit

--- client/src/components/Pages/dispositif/Edition/CustomNavbar/CustomNavbarEdit/CustomNavbarEdit.tsx
+++ client/src/components/Pages/dispositif/Edition/CustomNavbar/CustomNavbarEdit/CustomNavbarEdit.tsx
@@ -25,7 +25,6 @@ interface Props {
 }
 
 const CustomNavbarEdit = (props: Props) => {
-  const { isSaving } = useAutosave();
   const { user } = useUser();
   const router = useRouter();
   const values = useWatch<CreateDispositifRequest>();
@@ -40,6 +39,14 @@ const CustomNavbarEdit = (props: Props) => {
   }, [values, props.typeContenu]);
 
   const { showMissingSteps, setShowMissingSteps } = useContext(PageContext);
+
+  // Save
+  const { isSaving, hasError } = useAutosave();
+  const saveText = useMemo(() => {
+    if (isSaving) return "Sauvegarde en cours...";
+    if (hasError) return "Erreur lors de la sauvegarde !";
+    return "Sauvegardé il y a quelques secondes";
+  }, [isSaving, hasError]);
 
   // Quit
   const [showQuitModal, setShowQuitModal] = useState(false);
@@ -143,7 +150,7 @@ const CustomNavbarEdit = (props: Props) => {
               fill={styles.darkBackgroundElevationContrast}
               className="me-2"
             />
-            <span>{isSaving ? "Sauvegarde en cours..." : "Sauvegardé il y a quelques secondes"}</span>
+            <span>{saveText}</span>
           </span>
           <Tooltip target="save-status" placement="top">
             Toutes les modifications sont sauvegardées automatiquement
