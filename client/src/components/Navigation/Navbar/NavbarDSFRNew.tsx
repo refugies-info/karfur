6bca15439a279aef8dfe00486e9b3de295fbe4e5
Sylvain Le Gléau <syl@sylvainlg.fr>
2023-05-03 11:47:20 +0200

    fix(client): :recycle: Refactor NavbarDSFR & remove dataesr/react-dsfr

--- client/src/components/Navigation/Navbar/NavbarDSFRNew.tsx
+++ client/src/components/Navigation/Navbar/NavbarDSFRNew.tsx
@@ -1,9 +1,7 @@
 import { Header, HeaderProps } from "@codegouvfr/react-dsfr/Header";
-import Image from "next/image";
 import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
 import { getPath } from "routes";
-import styled from "styled-components";
 import useSpeekToolItem from "./NavbarItems/SpeekToolItem/useSpeekToolItem";
 import useSubscribeToolItem from "./NavbarItems/SubscribeToolItem/useSubscribeToolItem";
 import useUserToolItem from "./NavbarItems/UserToolItem/useUserToolItem";
@@ -11,6 +9,9 @@ import isInBrowser from "lib/isInBrowser";
 import { toggleNewsletterModalAction } from "services/Miscellaneous/miscellaneous.actions";
 import { useDispatch } from "react-redux";
 import useBackendNavigation from "containers/Backend/Navigation/useBackendNavigation";
+import { useLanguageItem } from "./NavbarItems";
+import styled from "styled-components";
+import Image from "next/image";
 
 const LogoImage = styled(Image)`
   max-width: 80%;
@@ -24,12 +25,16 @@ const Navbar = () => {
   const speekItem = useSpeekToolItem();
   const subscribeItem = useSubscribeToolItem();
   const userItem = useUserToolItem();
+  const languageItem = useLanguageItem();
 
   const quickAccessItems: HeaderProps.QuickAccessItem[] = [];
   if (speekItem) quickAccessItems.push(speekItem);
   if (subscribeItem) quickAccessItems.push(subscribeItem);
   if (userItem) quickAccessItems.push(userItem);
+  quickAccessItems.push(languageItem);
 
+  const isCurrent = (href: string) =>
+    isInBrowser() && (window?.location?.pathname || "") === "/" + router.locale + href;
   const isBackend = router.pathname.includes("/backend");
   const backendNavigation = useBackendNavigation();
 
@@ -53,12 +58,18 @@ const Navbar = () => {
               {
                 linkProps: { href: getPath("/recherche", router.locale) },
                 text: t("Toolbar.Trouver de l'information", "Trouver de l'information"),
+                isActive: isCurrent(getPath("/recherche", router.locale)),
               },
               {
                 linkProps: { href: getPath("/publier", router.locale) },
                 text: t("Toolbar.Publier une fiche", "Publier une fiche"),
+                isActive: isCurrent(getPath("/publier", router.locale)),
               },
-              { linkProps: { href: getPath("/traduire", router.locale) }, text: t("Toolbar.Traduire", "Traduire") },
+              {
+                linkProps: { href: getPath("/traduire", router.locale) },
+                text: t("Toolbar.Traduire", "Traduire"),
+                isActive: isCurrent(getPath("/traduire", router.locale)),
+              },
               {
                 text: t("Toolbar.Parler de nous", "Parler de nous"),
                 menuLinks: [
@@ -81,12 +92,13 @@ const Navbar = () => {
                 ],
               },
               {
-                linkProps: { href: "https://parrainage.refugies.info/" },
+                linkProps: { href: "https://parrainage.refugies.info/", target: "_blank" },
                 text: t("Toolbar.Pour l'Ukraine", "Pour l'Ukraine"),
               },
               {
                 linkProps: { href: `${getPath("/", router.locale)}#application` },
                 text: t("MobileAppModal.Télécharger l'application", "Télécharger l'application"),
+                isActive: isCurrent(getPath("/", router.locale)),
               },
               {
                 linkProps: {
