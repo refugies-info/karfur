5839a7b44ee4af98b7dc236afd8c8bf007f3bf73
Matthieu Fesselier <contact@matthieufesselier.com>
2023-05-11 11:38:23 +0200

    fix(server): :bug: content for app filter

--- server/src/workflows/dispositif/getFilteredContentsForApp/getFilteredContentsForApp.ts
+++ server/src/workflows/dispositif/getFilteredContentsForApp/getFilteredContentsForApp.ts
@@ -5,66 +5,66 @@ import { Dispositif } from "../../../typegoose";
 
 const filterByAge =
   (age: GetContentsForAppRequest["age"] | null) =>
-  (dispositifs: Dispositif[]): Dispositif[] => {
-    let bottomValue: number | null = null,
-      topValue: number | null = null;
-    switch (age) {
-      case "0 à 17 ans":
-        bottomValue = 0;
-        topValue = 17;
-        break;
-
-      case "18 à 25 ans":
-        bottomValue = 18;
-        topValue = 25;
-        break;
-
-      case "26 ans et plus":
-        bottomValue = 26;
-        topValue = Number.MAX_SAFE_INTEGER;
-        break;
-      default:
-        // Pas de filtrage par âge => c'est ok
-        return dispositifs;
-    }
-
-    return dispositifs.filter((dispositif) => {
-      if (!dispositif.metadatas.age) return true;
-      // Initialisation d'une intersection d'intervale invalide
-      // car la borne supérieure est inférieure à la borne inférieure
-      let intersection = { bottom: 0, top: -1 };
-      switch (dispositif.metadatas.age.type) {
-        case "between":
-          intersection = {
-            bottom: Math.max(bottomValue, dispositif.metadatas.age.ages[0]),
-            top: Math.min(topValue, dispositif.metadatas.age.ages[1]),
-          };
+    (dispositifs: Dispositif[]): Dispositif[] => {
+      let bottomValue: number | null = null,
+        topValue: number | null = null;
+      switch (age) {
+        case "0 à 17 ans":
+          bottomValue = 0;
+          topValue = 17;
           break;
-        case "lessThan":
-          intersection = {
-            // 0 comme borne inférieure
-            bottom: Math.max(bottomValue, 0),
-            top: Math.min(topValue, dispositif.metadatas.age.ages[0]),
-          };
+
+        case "18 à 25 ans":
+          bottomValue = 18;
+          topValue = 25;
           break;
-        case "moreThan":
-          intersection = {
-            bottom: Math.max(bottomValue, dispositif.metadatas.age.ages[0]),
-            // Infini comme borne supérieure
-            top: Math.min(topValue, Number.MAX_SAFE_INTEGER),
-          };
+
+        case "26 ans et plus":
+          bottomValue = 26;
+          topValue = Number.MAX_SAFE_INTEGER;
           break;
+        default:
+          // Pas de filtrage par âge => c'est ok
+          return dispositifs;
       }
 
-      /**
-       * Si l'intersection est valide le dispositif est gardé par le filtrage
-       *
-       * Un intervalle d'intersection est valide lorsque sa borne inférieure
-       * est bien inférieure à sa borne supérieure.
-       */
-      return intersection.bottom - intersection.top <= 0;
-    });
-  };
+      return dispositifs.filter((dispositif) => {
+        if (!dispositif.metadatas.age) return true;
+        // Initialisation d'une intersection d'intervale invalide
+        // car la borne supérieure est inférieure à la borne inférieure
+        let intersection = { bottom: 0, top: -1 };
+        switch (dispositif.metadatas.age.type) {
+          case "between":
+            intersection = {
+              bottom: Math.max(bottomValue, dispositif.metadatas.age.ages[0]),
+              top: Math.min(topValue, dispositif.metadatas.age.ages[1]),
+            };
+            break;
+          case "lessThan":
+            intersection = {
+              // 0 comme borne inférieure
+              bottom: Math.max(bottomValue, 0),
+              top: Math.min(topValue, dispositif.metadatas.age.ages[0]),
+            };
+            break;
+          case "moreThan":
+            intersection = {
+              bottom: Math.max(bottomValue, dispositif.metadatas.age.ages[0]),
+              // Infini comme borne supérieure
+              top: Math.min(topValue, Number.MAX_SAFE_INTEGER),
+            };
+            break;
+        }
+
+        /**
+         * Si l'intersection est valide le dispositif est gardé par le filtrage
+         *
+         * Un intervalle d'intersection est valide lorsque sa borne inférieure
+         * est bien inférieure à sa borne supérieure.
+         */
+        return intersection.bottom - intersection.top <= 0;
+      });
+    };
 
 const getFilteredContentsForApp = async (req: GetContentsForAppRequest) => {
   const { age, county, frenchLevel, strictLocation } = req;
@@ -82,13 +82,13 @@ const getFilteredContentsForApp = async (req: GetContentsForAppRequest) => {
    * Pas de filtre si tous niveaux
    */
   const frenchLevelFilter =
-    frenchLevel === MobileFrenchLevel["Tous les niveaux"]
+    !frenchLevel || frenchLevel === MobileFrenchLevel["Tous les niveaux"]
       ? []
       : [
-          // @ts-ignore
-          { "metadatas.frenchLevel": { $eq: null } },
-          { "metadatas.frenchLevel": { $exists: false } },
-        ];
+        // @ts-ignore
+        { "metadatas.frenchLevel": { $eq: null } },
+        { "metadatas.frenchLevel": { $exists: false } },
+      ];
   switch (frenchLevel) {
     case MobileFrenchLevel["Je ne lis et n'écris pas le français"]:
       // query.push({ "metadatas.frenchLevel": { $eq: "A1.1" } });
