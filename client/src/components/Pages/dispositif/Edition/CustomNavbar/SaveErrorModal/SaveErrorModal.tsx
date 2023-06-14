a732cc76e401f9415ef24c0e262e1b0e75ec7f8e
Matthieu Fesselier <contact@matthieufesselier.com>
2023-06-14 09:19:15 +0200

    style(client): :speech_balloon: update wordings

--- client/src/components/Pages/dispositif/Edition/CustomNavbar/SaveErrorModal/SaveErrorModal.tsx
+++ client/src/components/Pages/dispositif/Edition/CustomNavbar/SaveErrorModal/SaveErrorModal.tsx
@@ -1,6 +1,8 @@
 import { useCallback } from "react";
+import Image from "next/image";
 import { BaseModal } from "components/Pages/dispositif";
 import Button from "components/UI/Button";
+import TutorielImage from "assets/dispositif/tutoriel-image.svg";
 
 interface Props {
   show: boolean;
@@ -10,10 +12,22 @@ const SaveErrorModal = (props: Props) => {
   const reload = useCallback(() => location.reload(), []);
 
   return (
-    <BaseModal show={props.show} title={"Erreur lors de la sauvegarde"} small>
-      <p>Erreur lors de la sauvegarde, veuillez recharger la page</p>
+    <BaseModal show={props.show} title={"Oups, il y a un problème avec la sauvegarde automatique !"} small>
+      <p>
+        Pas d’inquiétudes, seules vos dernières modifications n’ont pas été prises en compte. Cliquez sur « Rafraîchir
+        la page ».
+      </p>
+      <p>
+        Si cela ne fonctionne toujours pas, vous pouvez quitter l’éditeur et revenir ensuite sur votre fiche.
+        Contactez-nous via le chat en bas à droite si besoin.
+      </p>
+      <div className="text-center mb-8">
+        <Image src={TutorielImage} width={176} height={120} alt="" />
+      </div>
       <div className="text-end">
-        <Button onClick={reload}>Recharger la page</Button>
+        <Button onClick={reload} evaIcon="refresh-outline" iconPosition="right">
+          Rafraîchir la page
+        </Button>
       </div>
     </BaseModal>
   );
