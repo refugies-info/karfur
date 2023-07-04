db25c1f13294b3d33135c428cc468b02590dae75
Matthieu Fesselier <contact@matthieufesselier.com>
2023-07-04 10:11:21 +0200

    feat(client): :lipstick: translation needs list style

--- client/src/containers/Backend/UserTranslation/components/TranslationNeedsList/TranslationNeedsList.tsx
+++ client/src/containers/Backend/UserTranslation/components/TranslationNeedsList/TranslationNeedsList.tsx
@@ -1,16 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { Table } from "reactstrap";
-import styled from "styled-components";
+import React, { useEffect, useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
-import { GetNeedResponse, Id } from "@refugies-info/api-types";
+import { Id, Languages } from "@refugies-info/api-types";
 import { needsSelector } from "services/Needs/needs.selectors";
 import { fetchNeedsActionCreator } from "services/Needs/needs.actions";
 import { isLoadingSelector } from "services/LoadingStatus/loadingStatus.selectors";
 import { LoadingStatusKey } from "services/LoadingStatus/loadingStatus.actions";
-import FButton from "components/UI/FButton/FButton";
-import FrameModal from "components/Modals/FrameModal/FrameModal";
-import { TagButton } from "../../../Admin/Needs/TagButton";
 import { getStatusColorAndText } from "./functions";
 import styles from "./TranslationNeedsList.module.scss";
 
@@ -19,163 +14,73 @@ interface Props {
   toggle: () => void;
   setSelectedNeedId: (arg: Id) => void;
   langueSelectedFr?: string;
-  langueI18nCode?: string;
+  langueI18nCode: Languages;
 }
-const Header = styled.div`
-  font-weight: bold;
-  font-size: 32px;
-  margin-bottom: 8px;
-`;
 
-const StatusContainer = styled.div`
-  background-color: ${(props: { backgroundColor: string }) => props.backgroundColor};
-  border-radius: 8px;
-  padding: 10px;
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 20px;
-  color: #ffffff;
-  width: fit-content;
-  white-space: nowrap;
-`;
+const arrayLines = new Array(6).fill("a");
 
 const TranslationNeedsList = (props: Props) => {
-  const arrayLines = new Array(6).fill("a");
-  const arrayContent = new Array(3).fill("a");
-  const [showTutorielModal, setShowTutorielModal] = useState(false);
-
   const dispatch = useDispatch();
 
   const isLoading = useSelector(isLoadingSelector(LoadingStatusKey.FETCH_NEEDS));
   useEffect(() => {
     dispatch(fetchNeedsActionCreator());
   }, [dispatch]);
-  const toggleTutorielModal = () => setShowTutorielModal(!showTutorielModal);
+
   const needs = useSelector(needsSelector);
+  const sortedNeeds = useMemo(() => {
+    return needs
+      .map((need) => {
+        const { statusColor, statusText } = getStatusColorAndText(need, props.langueI18nCode);
+        return { ...need, statusText, statusColor };
+      })
+      .sort((a, b) => {
+        if (a.statusText === b.statusText) {
+          return a.theme.name.fr > b.theme.name.fr ? 1 : -1;
+        }
+        if (a.statusText === "À revoir") return -1;
+        if (b.statusText === "À revoir") return 1;
 
-  const needsWithStatus = needs.map((need) => {
-    const { statusColor, statusText } = getStatusColorAndText(
-      need,
-      // @ts-ignore
-      props.langueI18nCode,
-    );
-    return { ...need, statusText, statusColor };
-  });
+        if (a.statusText === "À traduire") return -1;
+        if (a.statusText === "À traduire") return 1;
 
-  const sortedNeeds = needsWithStatus.sort((a, b) => {
-    if (a.statusText === b.statusText) {
-      return a.theme.name.fr > b.theme.name.fr ? 1 : -1;
-    }
-    if (a.statusText === "À revoir") return -1;
-    if (b.statusText === "À revoir") return 1;
-
-    if (a.statusText === "À traduire") return -1;
-    if (a.statusText === "À traduire") return 1;
-
-    return 1;
-  });
-
-  const onNeedClick = (need: GetNeedResponse) => {
-    props.setSelectedNeedId(need._id);
-  };
+        return 1;
+      });
+  }, [needs, props.langueI18nCode]);
 
   if (!props.langueSelectedFr || !props.langueI18nCode) return <div>Erreur</div>;
 
-  if (isLoading) {
-    return (
-      <div>
-        <Header>{"Traduction des besoins"}</Header>
-        <Table responsive borderless>
-          <thead>
-            <tr>
-              {["Nom du besoin", "Thème", "Traduction", "Statut"].map((element, key) => (
-                <th key={key}>
-                  <div>{element}</div>
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>
+        Liste des besoins en {props.langueSelectedFr} <span className={styles.badge}>{(sortedNeeds || []).length}</span>
+      </h2>
+
+      <div className={styles.content}>
+        {isLoading ? (
+          <>
             {arrayLines.map((_, key) => {
               return (
-                <tr key={key} className={"bg-white"}>
-                  <td>
-                    <SkeletonTheme baseColor="#CDCDCD">
-                      <Skeleton width={170} count={1} />
-                    </SkeletonTheme>
-                  </td>
-                  {arrayContent.map((_, key) => (
-                    <td key={key}>
-                      <SkeletonTheme baseColor="#CDCDCD">
-                        <Skeleton width={100} count={1} />
-                      </SkeletonTheme>
-                    </td>
-                  ))}
-                </tr>
+                <div key={key} className={"bg-white"}>
+                  <SkeletonTheme baseColor="#CDCDCD">
+                    <Skeleton width={170} count={1} />
+                  </SkeletonTheme>
+                </div>
               );
             })}
-          </tbody>
-        </Table>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <Header>{"Traduction des besoins en " + props.langueSelectedFr}</Header>
-      <Table responsive borderless>
-        <thead>
-          <tr>
-            {["Nom du besoin", "Thème", "Traduction", "Statut"].map((element, key) => (
-              <th key={key}>
-                <div>{element}</div>
-              </th>
+          </>
+        ) : (
+          <>
+            {sortedNeeds.map((need, key) => (
+              <div key={key} onClick={() => props.setSelectedNeedId(need._id)}>
+                <div>{need[props.langueI18nCode]?.text || need.fr.text}</div>
+                <div>{need[props.langueI18nCode]?.subtitle || need.fr.subtitle}</div>
+                <span>{need.statusText}</span>
+              </div>
             ))}
-          </tr>
-        </thead>
-        <tbody>
-          {sortedNeeds.map((need, key) => {
-            const translatedNeed =
-              // @ts-ignore
-              need[props.langueI18nCode] && need[props.langueI18nCode].text
-                ? //@ts-ignore
-                  need[props.langueI18nCode].text
-                : "";
-            if (!need.theme) return;
-            return (
-              <tr key={key} onClick={() => onNeedClick(need)}>
-                <td className="align-middle" style={{ width: 300 }}>
-                  {need.fr.text}
-                </td>
-                <td className="align-middle">
-                  <div style={{ marginLeft: -4, width: 130 }}>
-                    <TagButton theme={{ ...need.theme, active: true }} isSelected={true} />
-                  </div>
-                </td>
-                <td className="align-middle">{translatedNeed}</td>
-                <td className="align-middle" style={{ width: 120 }}>
-                  <StatusContainer backgroundColor={need.statusColor}>{need.statusText}</StatusContainer>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <FButton type="tuto" name={"play-circle-outline"} onClick={toggleTutorielModal}>
-          Tutoriel
-        </FButton>
-        <FButton className="me-2" type="white" name="arrow-back-outline" onClick={props.toggle}>
-          Retour
-        </FButton>
+          </>
+        )}
       </div>
-      <FrameModal show={showTutorielModal} toggle={toggleTutorielModal} section={"Traduction besoin"} />
     </div>
   );
 };
--- client/src/containers/Backend/UserTranslation/components/TranslationNeedsList/functions.ts
+++ client/src/containers/Backend/UserTranslation/components/TranslationNeedsList/functions.ts
@@ -1,25 +1,20 @@
-import { GetNeedResponse } from "@refugies-info/api-types";
+import { GetNeedResponse, Languages } from "@refugies-info/api-types";
 import { colors } from "colors";
-import { AvailableLanguageI18nCode } from "types/interface";
 
-const isNotTranslated = (need: GetNeedResponse, ln: AvailableLanguageI18nCode) => {
+const isNotTranslated = (need: GetNeedResponse, ln: Languages) => {
   return (
     !need[ln]?.text || // no title translated
     (need.fr.subtitle && !need[ln]?.subtitle)
   ); // or no subtitle if defined in french
 };
 
-const isTranslationOutdated = (need: GetNeedResponse, ln: AvailableLanguageI18nCode) => {
+const isTranslationOutdated = (need: GetNeedResponse, ln: Languages) => {
   const translationUpdatedAt = need[ln]?.updatedAt;
   if (!need.fr.updatedAt || !translationUpdatedAt) return true;
   return need.fr.updatedAt > translationUpdatedAt;
 };
 
-export const getStatusColorAndText = (need: GetNeedResponse, langueI18nCode: AvailableLanguageI18nCode) => {
-  if (!langueI18nCode) {
-    return { statusColor: colors.darkGrey, statusText: "Erreur" };
-  }
-
+export const getStatusColorAndText = (need: GetNeedResponse, langueI18nCode: Languages) => {
   if (isNotTranslated(need, langueI18nCode)) {
     return { statusColor: colors.blue, statusText: "À traduire" };
   }
@@ -28,5 +23,5 @@ export const getStatusColorAndText = (need: GetNeedResponse, langueI18nCode: Ava
     return { statusColor: colors.rouge, statusText: "À revoir" };
   }
 
-  return { statusColor: colors.green, statusText: "À jour" };
+  return { statusColor: colors.green, statusText: "Traduit" };
 };
