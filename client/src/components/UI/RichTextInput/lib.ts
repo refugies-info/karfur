0b96f55326b97f00498e23a7de9ad89ac6b1c18c
Matthieu Fesselier <contact@matthieufesselier.com>
2023-07-27 11:06:32 +0200

    fix(client): :bug: force link protocol when not defined by the user

--- client/src/components/UI/RichTextInput/lib.ts
+++ client/src/components/UI/RichTextInput/lib.ts
@@ -25,16 +25,15 @@ export function getSelectedNode(selection: RangeSelection): TextNode | ElementNo
 }
 
 export function sanitizeUrl(url: string): string {
+  const URL_PROTOCOL = /^(?:https?|mailto|ftp|tel|file|sms)/gi;
+
   /** A pattern that matches safe  URLs. */
   const SAFE_URL_PATTERN = /^(?:(?:https?|mailto|ftp|tel|file|sms):|[^&:/?#]*(?:[/?#]|$))/gi;
 
-  /** A pattern that matches safe data URLs. */
-  const DATA_URL_PATTERN =
-    /^data:(?:image\/(?:bmp|gif|jpeg|jpg|png|tiff|webp)|video\/(?:mpeg|mp4|ogg|webm)|audio\/(?:mp3|oga|ogg|opus));base64,[a-z0-9+/]+=*$/i;
-
   url = String(url).trim();
 
-  if (url.match(SAFE_URL_PATTERN) || url.match(DATA_URL_PATTERN)) return url;
+  if (!url.match(URL_PROTOCOL)) return `https://${url}`;
+  if (url.match(SAFE_URL_PATTERN)) return url;
 
   return "https://";
 }
