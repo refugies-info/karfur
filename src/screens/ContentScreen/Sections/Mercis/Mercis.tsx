09e5aaae8e60ac983d3cd98074239cc0cd9764e7
Matthieu Fesselier <contact@matthieufesselier.com>
2023-08-16 16:19:05 +0200

    fix: :bug: prevent multiple thanks

--- src/screens/ContentScreen/Sections/Mercis/Mercis.tsx
+++ src/screens/ContentScreen/Sections/Mercis/Mercis.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { GetDispositifResponse } from "@refugies-info/api-types";
 import styled from "styled-components/native";
 import {
   Button,
   Card,
   Columns,
-  IconButton,
   Rows,
   SectionTitle,
   TextNormal,
 } from "../../../../components";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import Background from "./Background";
 import { View } from "react-native";
-import { addMerci, deleteMerci } from "../../../../utils/API";
+import { addMerci } from "../../../../utils/API";
 import { setSelectedContentActionCreator } from "../../../../services/redux/SelectedContent/selectedContent.actions";
 import { useDispatch, useSelector } from "react-redux";
 import { currentI18nCodeSelector } from "../../../../services";
@@ -44,48 +44,43 @@ const Mercis = ({ dispositif }: MercisProps) => {
   const dispatch = useDispatch();
   const { t } = useTranslationWithRTL();
   const currentLanguage = useSelector(currentI18nCodeSelector) || "fr";
-  const [{ loading: loadingMerci }, merci] = useAsyncFn(
-    () =>
-      addMerci(dispositif._id.toString()).then(() => {
-        dispatch(
-          setSelectedContentActionCreator({
-            content: {
-              ...dispositif,
-              merci: [...dispositif.merci, { created_at: new Date() }],
-            },
-            locale: currentLanguage,
-          })
-        );
-      }),
-    [
-      addMerci,
-      currentLanguage,
-      dispatch,
-      setSelectedContentActionCreator,
-      dispositif,
-    ]
+
+  const [thanks, setThanks] = useState<string[]>([]);
+  const hasThanked = useMemo(
+    () => thanks.includes(dispositif._id.toString()),
+    [thanks, dispositif._id]
   );
-  const [{ loading: loadingNonMerci }, nonMerci] = useAsyncFn(
-    () =>
-      deleteMerci(dispositif._id.toString()).then(() => {
-        dispatch(
-          setSelectedContentActionCreator({
-            content: {
-              ...dispositif,
-              merci: dispositif.merci.slice(1),
-            },
-            locale: currentLanguage,
-          })
-        );
-      }),
-    [
-      deleteMerci,
-      currentLanguage,
-      dispatch,
-      setSelectedContentActionCreator,
-      dispositif,
-    ]
-  );
+  useEffect(() => {
+    AsyncStorage.getItem("THANKS").then((thanks) =>
+      setThanks((thanks || "")?.split(","))
+    );
+  }, []);
+
+  const [{ loading: loadingMerci }, merci] = useAsyncFn(() => {
+    if (hasThanked || loadingMerci) return Promise.resolve();
+    return addMerci(dispositif._id.toString()).then(() => {
+      const newThanks = [...thanks, dispositif._id.toString()];
+      setThanks(newThanks);
+      AsyncStorage.setItem("THANKS", newThanks.join(","));
+      dispatch(
+        setSelectedContentActionCreator({
+          content: {
+            ...dispositif,
+            merci: [...dispositif.merci, { created_at: new Date() }],
+          },
+          locale: currentLanguage,
+        })
+      );
+    });
+  }, [
+    addMerci,
+    currentLanguage,
+    dispatch,
+    setSelectedContentActionCreator,
+    dispositif,
+    thanks,
+    hasThanked,
+  ]);
 
   return (
     <Card backgroundColor="#E3E3FD">
@@ -98,21 +93,15 @@ const Mercis = ({ dispositif }: MercisProps) => {
             <Columns layout="auto" horizontalAlign="center">
               <Button
                 accessibilityLabel="Dire merci"
-                color="#000091"
+                color={hasThanked ? "#fff" : "#000091"}
+                iconColor={hasThanked ? "#fff" : "#000091"}
+                backgroundColor={hasThanked ? "#000091" : undefined}
                 iconName="thumb_up"
                 loading={loadingMerci}
                 onPress={merci}
                 title={t("content_screen.nbThanks", {
                   count: dispositif.merci.length,
                 })}
-              />
-              <IconButton
-                accessibilityLabel="Dire non merci"
-                color="#000091"
-                iconName="thumb_down"
-                loading={loadingNonMerci}
-                onPress={nonMerci}
-                outlined
               />
             </Columns>
           </View>
