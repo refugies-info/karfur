f393e1de0a77fbf7d94ec0c886280b707306d47c
Sylvain Le Gléau <syl@sylvainlg.fr>
2023-11-04 11:27:54 +0100

    feat(client): :sparkles: allow to follow utm_campaign information when user send SMS to another

--- client/src/lib/tracking.ts
+++ client/src/lib/tracking.ts
@@ -1,5 +1,6 @@
 import ReactGA from "react-ga4";
 import { logger } from "logger";
+import { isEmpty } from "lodash";
 
 /**
  * Inits GA. Must be fired once only
@@ -9,6 +10,45 @@ export const initGA = () => {
   const trackingId = process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_ANALYTICS;
   if (trackingId) ReactGA.initialize(trackingId);
   // }
+
+  /**
+   * Le code suivant permet de stocker dans un cookie
+   * les informations de campagne par laquelle l'utilisateur
+   * est arrivé sur le site.
+   */
+
+  // Récupérer l'URL actuelle de la page
+  var url = window.location.href;
+
+  // Fonction pour extraire les paramètres UTM de l'URL
+  function getUTMParameters(url: string) {
+    var utmParams: { [key: string]: string } = {};
+    var queryString = url.split("?")[1];
+
+    if (queryString) {
+      var params = queryString.split("&");
+      for (var i = 0; i < params.length; i++) {
+        var param = params[i].split("=");
+        if (param[0].indexOf("utm_") === 0) {
+          utmParams[param[0]] = param[1];
+        }
+      }
+    }
+
+    return utmParams;
+  }
+
+  var utmData = getUTMParameters(url);
+
+  if (!isEmpty(utmData)) {
+    // Convertir les paramètres UTM en une chaîne de requête
+    var utmQueryString = Object.keys(utmData)
+      .map((key) => key + "=" + utmData[key])
+      .join("&");
+
+    // Créer un cookie __utmz de remplacement
+    document.cookie = "__utmz=" + utmQueryString + "; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+  }
 };
 
 /**
