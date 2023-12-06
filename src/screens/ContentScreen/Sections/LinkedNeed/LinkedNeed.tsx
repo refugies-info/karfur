07d5586ef97bcfadd9daf50da9a7f7a468fff5c0
Matthieu Fesselier <contact@matthieufesselier.com>
2023-12-06 17:09:25 +0100

    fix: :bug: use need translated text

--- src/screens/ContentScreen/Sections/LinkedNeed/LinkedNeed.tsx
+++ src/screens/ContentScreen/Sections/LinkedNeed/LinkedNeed.tsx
@@ -1,9 +1,13 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
 import { useSelector } from "react-redux";
 import { Id } from "@refugies-info/api-types";
 import { useNavigation } from "@react-navigation/native";
 
-import { needSelector, themeSelector } from "../../../../services";
+import {
+  currentI18nCodeSelector,
+  needSelector,
+  themeSelector,
+} from "../../../../services";
 import { logEventInFirebase } from "../../../../utils/logEvent";
 import { FirebaseEvent } from "../../../../utils/eventsUsedInFirebase";
 import { TagButton } from "../../../../components/Explorer/TagButton";
@@ -15,6 +19,7 @@ interface LinkedNeedProps {
 const LinkedNeed = ({ needId }: LinkedNeedProps) => {
   const navigation = useNavigation();
   const need = useSelector(needSelector(needId));
+  const currentLanguageI18nCode = useSelector(currentI18nCodeSelector);
   const theme = useSelector(themeSelector(need?.theme._id.toString()));
   const goToContent = useCallback(() => {
     if (!need) return null;
@@ -34,12 +39,18 @@ const LinkedNeed = ({ needId }: LinkedNeedProps) => {
   if (!need || !need.image) return null;
   if (!theme) return null;
 
+  const needText = useMemo(() => {
+    return currentLanguageI18nCode && need[currentLanguageI18nCode]?.text
+      ? need[currentLanguageI18nCode].text
+      : need.fr.text;
+  }, [currentLanguageI18nCode, need]);
+
   return (
     <TagButton
       backgroundColor={theme.colors.color40}
       icon={need.image}
       iconSize={60}
-      name={need.fr.text}
+      name={needText}
       onPress={goToContent}
       textColor={theme?.colors.color100 || "black"}
     />
