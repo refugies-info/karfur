ec96af02c708bea3a7127bdd1699a1dc5c0ee1a0
Matthieu Fesselier <contact@matthieufesselier.com>
2024-02-08 16:22:34 +0100

    style(client): :lipstick: update spacing small screens

--- client/src/pages/auth/reinitialiser-mot-de-passe/mail-envoye.tsx
+++ client/src/pages/auth/reinitialiser-mot-de-passe/mail-envoye.tsx
@@ -29,30 +29,22 @@ const AuthResetMailSent = () => {
       <Button priority="tertiary" size="small" iconId="fr-icon-arrow-left-line" onClick={() => router.back()}>
         Retour
       </Button>
-      <div className={styles.content}>
-        <div className={styles.title}>
-          <h1>Un lien a été envoyé</h1>
-          <p className={styles.subtitle}>
-            Nous venons de vous envoyer un mail pour réinitialiser votre mot de passe à l’adresse suivante&nbsp;:{" "}
-            {email}.
-            <br />
-            <br />
-            Si vous ne recevez pas de mail d’ici quelques minutes, pensez à vérifier vos courriers indésirables ou vos
-            spams.
-          </p>
-        </div>
-        <Button
-          iconId="fr-icon-mail-line"
-          iconPosition="right"
-          onClick={sendMail}
-          className={cls(styles.button, "mb-4")}
-        >
-          Renvoyer le lien de réinitialisation
-        </Button>
-        <Button onClick={openChat} className={cls(styles.button)} priority="tertiary">
-          Contacter le chat
-        </Button>
+      <div className={styles.title}>
+        <h1>Un lien a été envoyé</h1>
+        <p className={styles.subtitle}>
+          Nous venons de vous envoyer un mail pour réinitialiser votre mot de passe à l’adresse suivante&nbsp;: {email}.
+          <br />
+          <br />
+          Si vous ne recevez pas de mail d’ici quelques minutes, pensez à vérifier vos courriers indésirables ou vos
+          spams.
+        </p>
       </div>
+      <Button iconId="fr-icon-mail-line" iconPosition="right" onClick={sendMail} className={cls(styles.button, "mb-4")}>
+        Renvoyer le lien de réinitialisation
+      </Button>
+      <Button onClick={openChat} className={cls(styles.button)} priority="tertiary">
+        Contacter le chat
+      </Button>
     </div>
   );
 };
