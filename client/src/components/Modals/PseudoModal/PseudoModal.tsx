d90cb4b83fdc98eb2913e59d71a4eefa013746f7
Matthieu Fesselier <contact@matthieufesselier.com>
2024-02-16 10:15:28 +0100

    feat(client): :sparkles: add pseudo modal

---
+++ client/src/components/Modals/PseudoModal/PseudoModal.tsx
@@ -0,0 +1,31 @@
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { createModal } from "@codegouvfr/react-dsfr/Modal";
+import { EditPseudo } from "components/User";
+import { fetchUserActionCreator } from "services/User/user.actions";
+
+interface Props {
+  successCallback: () => void;
+}
+
+export const pseudoModal = createModal({
+  id: "user-pseudo-modal",
+  isOpenedByDefault: false,
+});
+
+export const PseudoModal = ({ successCallback }: Props) => {
+  const dispatch = useDispatch();
+
+  const onSuccess = useCallback(() => {
+    dispatch(fetchUserActionCreator());
+    pseudoModal.close();
+    successCallback();
+  }, [dispatch, successCallback]);
+
+  return (
+    <pseudoModal.Component title="Choisissez un pseudonyme">
+      <p>Au pied de chaque fiche, nous affichons les contributeurs ayant participé à sa rédaction et sa traduction.</p>
+      <EditPseudo successCallback={onSuccess} />
+    </pseudoModal.Component>
+  );
+};
---
+++ client/src/components/Modals/PseudoModal/index.ts
@@ -0,0 +1,1 @@
+export { PseudoModal, pseudoModal } from "./PseudoModal";
