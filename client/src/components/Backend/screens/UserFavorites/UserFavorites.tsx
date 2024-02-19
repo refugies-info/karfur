5b85f20c08e01a514bb3a54b6afa88521321e9b2
Matthieu Fesselier <contact@matthieufesselier.com>
2024-02-19 14:41:09 +0100

    feat(client): :sparkles: show toast on delete all

--- client/src/components/Backend/screens/UserFavorites/UserFavorites.tsx
+++ client/src/components/Backend/screens/UserFavorites/UserFavorites.tsx
@@ -7,13 +7,13 @@ import {
 import { LoadingStatusKey } from "services/LoadingStatus/loadingStatus.actions";
 import { isLoadingSelector } from "services/LoadingStatus/loadingStatus.selectors";
 import { userFavoritesSelector } from "services/UserFavoritesInLocale/UserFavoritesInLocale.selectors";
-import styled from "styled-components";
 import { NoFavorites } from "./components/NoFavorites.component";
 import { FrameModal } from "components/Modals";
 import TitleWithNumber from "components/Backend/TitleWithNumber";
 import FButton from "components/UI/FButton/FButton";
 import DemarcheCard from "components/UI/DemarcheCard";
 import DispositifCard from "components/UI/DispositifCard";
+import Toast from "components/UI/Toast";
 import { FavoritesLoading } from "./components/FavoritesLoading";
 import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
@@ -47,10 +47,10 @@ const UserFavorites = (props: Props) => {
   const isLoading = isLoadingFetch || isLoadingUpdate;
 
   const favorites = useSelector(userFavoritesSelector);
-
-  if (isLoading) return <FavoritesLoading t={t} />;
+  const [showDeleteToast, setShowDeleteToast] = useState(false);
 
   const removeAllFavorites = () => {
+    setShowDeleteToast(true);
     dispatch(
       updateUserFavoritesActionCreator({
         type: "remove-all",
@@ -59,41 +59,50 @@ const UserFavorites = (props: Props) => {
     );
   };
 
-  if (favorites.length === 0)
-    return (
-      <>
-        <NoFavorites t={t} toggleTutoModal={toggleTutoModal} />
-        {showTutoModal && <FrameModal show={showTutoModal} toggle={toggleTutoModal} section={"Mes favoris"} />}
-      </>
-    );
   return (
-    <div className={styles.container}>
-      <div className={styles.title}>
-        <TitleWithNumber
-          amount={favorites.length}
-          textSingular={t("UserFavorites.content_saved", "fiche sauvegardée")}
-          textPlural={t("UserFavorites.contents_saved", "fiches sauvegardées")}
-        />
-        <div>
-          <FButton
-            type="outline-black"
-            name="trash-outline"
-            onClick={removeAllFavorites}
-            data-test-id="test-delete-button"
-          >
-            {t("UserFavorites.Tout supprimer", "Tout supprimer")}
-          </FButton>
-        </div>
-      </div>
+    <>
+      {isLoading ? (
+        <FavoritesLoading t={t} />
+      ) : favorites.length === 0 ? (
+        <NoFavorites t={t} toggleTutoModal={toggleTutoModal} />
+      ) : (
+        <>
+          <div className={styles.container}>
+            <div className={styles.title}>
+              <TitleWithNumber
+                amount={favorites.length}
+                textSingular={t("UserFavorites.content_saved", "fiche sauvegardée")}
+                textPlural={t("UserFavorites.contents_saved", "fiches sauvegardées")}
+              />
+              <div>
+                <FButton
+                  type="outline-black"
+                  name="trash-outline"
+                  onClick={removeAllFavorites}
+                  data-test-id="test-delete-button"
+                >
+                  {t("UserFavorites.Tout supprimer", "Tout supprimer")}
+                </FButton>
+              </div>
+            </div>
 
-      <div className={styles.cards}>
-        {favorites.map((fav, index) => (
-          <div key={index}>
-            {fav.typeContenu === "demarche" ? <DemarcheCard demarche={fav} /> : <DispositifCard dispositif={fav} />}
+            <div className={styles.cards}>
+              {favorites.map((fav, index) => (
+                <div key={index}>
+                  {fav.typeContenu === "demarche" ? (
+                    <DemarcheCard demarche={fav} />
+                  ) : (
+                    <DispositifCard dispositif={fav} />
+                  )}
+                </div>
+              ))}
+            </div>
           </div>
-        ))}
-      </div>
-    </div>
+        </>
+      )}
+      {showDeleteToast && <Toast close={() => setShowDeleteToast(false)}>Vos favoris ont été supprimés</Toast>}
+      {showTutoModal && <FrameModal show={showTutoModal} toggle={toggleTutoModal} section={"Mes favoris"} />}
+    </>
   );
 };
 
