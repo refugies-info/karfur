81bf48815d828ba24531b108819198f5ac9da32b
Matthieu Fesselier <contact@matthieufesselier.com>
2024-03-14 11:37:59 +0100

    fix(client): :bug: fix re-run query too often

--- client/src/hooks/useUtmz.ts
+++ client/src/hooks/useUtmz.ts
@@ -1,17 +1,21 @@
+import { useMemo } from "react";
 import { useCookie } from "react-use";
 
 const useUtmz = () => {
   const [utmz] = useCookie("__utmz");
 
-  const params: { [key: string]: string } = {};
-  if (utmz) {
-    const utm_value: any = new Proxy(new URLSearchParams((utmz || "").trim().replace("__utmz=", "")), {
-      get: (searchParams, prop) => searchParams.get(prop.toString()),
-    });
-    params.utm_source = utm_value.utm_source;
-    params.utm_campaign = utm_value.utm_campaign;
-    params.utm_medium = utm_value.utm_medium;
-  }
+  const params: { [key: string]: string } = useMemo(() => {
+    const newParams: { [key: string]: string } = {};
+    if (utmz) {
+      const utm_value: any = new Proxy(new URLSearchParams((utmz || "").trim().replace("__utmz=", "")), {
+        get: (searchParams, prop) => searchParams.get(prop.toString()),
+      });
+      newParams.utm_source = utm_value.utm_source;
+      newParams.utm_campaign = utm_value.utm_campaign;
+      newParams.utm_medium = utm_value.utm_medium;
+    }
+    return newParams;
+  }, [utmz]);
 
   return {
     utmz,
