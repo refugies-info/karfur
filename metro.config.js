9e3c9f8e7010bd526c11225b030881ca4a61f21d
Luis Arias <luis@inclusif.eu>
2024-06-07 11:18:37 +0200

    fix: :bug: Fix issues with existing workarounds causing bundling errors

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,17 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = (async () => {
-  const config = await getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-  const { transformer, resolver } = config;
+const { transformer, resolver } = config;
 
-  config.transformer = {
-    ...transformer,
-    babelTransformerPath: require.resolve("react-native-svg-transformer"),
-  };
-  config.resolver = {
-    ...resolver,
-    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...resolver.sourceExts, "svg"],
-  };
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer"),
+};
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+  sourceExts: [...resolver.sourceExts, "svg"],
+};
 
-  return config;
-})();
+module.exports = config;
