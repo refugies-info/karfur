04ecb4d019aeb800acb93749bdab8c342427686c
Matthieu Fesselier <contact@matthieufesselier.com>
2024-06-26 09:03:57 +0200

    feat: :sparkles: add share message

--- src/screens/ProfilTab/ShareScreen.tsx
+++ src/screens/ProfilTab/ShareScreen.tsx
@@ -64,7 +64,7 @@ export const ShareScreen = ({
     () =>
       Share.share({
         title: selectedShareLanguage,
-        message: `https://refugies.info/${selectedShareLanguage}/download-app`,
+        message: `${t("profile_screens.share_message")} https://refugies.info/${selectedShareLanguage}/download-app`,
       }),
     [selectedShareLanguage]
   );
