e88fb3634db9f4c6dd91929ee995d0cab637ecff
Luis Arias <luis@refugies.info>
2024-07-16 11:03:18 +0200

    test(client): :white_check_mark: Add mock IntersectionObserver

--- client/jest/setup.js
+++ client/jest/setup.js
@@ -80,3 +80,28 @@ jest.mock("next-i18next", () => ({
     return WrappedComponent;
   },
 }));
+
+// Mock the IntersectionObserver, see https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+export class IntersectionObserver {
+  root = null;
+  rootMargin = "";
+  thresholds = [];
+
+  disconnect() {
+    return null;
+  }
+
+  observe() {
+    return null;
+  }
+
+  takeRecords() {
+    return [];
+  }
+
+  unobserve() {
+    return null;
+  }
+}
+window.IntersectionObserver = IntersectionObserver;
+global.IntersectionObserver = IntersectionObserver;
