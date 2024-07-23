96b2331aee190f4433bbe8557f53d56ab88d6f9c
Luis Arias <luis@refugies.info>
2024-07-23 16:38:04 +0200

    fix(client): :bug: Update modal sorting

--- client/src/components/Pages/recherche/SearchHeader/Filters.tsx
+++ client/src/components/Pages/recherche/SearchHeader/Filters.tsx
@@ -1,13 +1,13 @@
-import { useMemo } from "react";
-import { useSelector } from "react-redux";
-import { useTranslation } from "next-i18next";
-import { Container } from "reactstrap";
 import { ageFilters, frenchLevelFilter, publicOptions, statusOptions } from "data/searchFilters";
 import { cls } from "lib/classname";
+import { useTranslation } from "next-i18next";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { Container } from "reactstrap";
 import { allLanguesSelector } from "services/Langue/langue.selectors";
 import { searchQuerySelector, themesDisplayedValueSelector } from "services/SearchResults/searchResults.selector";
-import ThemeDropdown from "../ThemeDropdown";
 import LocationDropdown from "../LocationDropdown";
+import ThemeDropdown from "../ThemeDropdown";
 import Filter from "./Filter";
 import styles from "./Filters.module.scss";
 
@@ -47,7 +47,9 @@ const Filters = (props: Props) => {
   // LANGUAGE
   const languages = useSelector(allLanguesSelector);
   const languagesOptions = useMemo(() => {
-    return languages.map((ln) => ({
+    // Sort languages by langueFr
+    const sorted = languages.sort((a, b) => a.langueFr.localeCompare(b.langueFr));
+    return sorted.map((ln) => ({
       key: ln.i18nCode,
       value: ln.langueFr,
     }));
