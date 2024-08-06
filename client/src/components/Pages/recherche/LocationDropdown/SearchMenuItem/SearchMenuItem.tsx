666a596ec99f9b66785a4d723419e0c10276233a
Luis Arias <luis@refugies.info>
2024-08-06 17:28:21 +0200

    feat(client): :sparkles: Search menu item should acquire input focus when rendered

--- client/src/components/Pages/recherche/LocationDropdown/SearchMenuItem/SearchMenuItem.tsx
+++ client/src/components/Pages/recherche/LocationDropdown/SearchMenuItem/SearchMenuItem.tsx
@@ -1,7 +1,7 @@
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { cls } from "lib/classname";
 import { useTranslation } from "next-i18next";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./SearchMenuItem.module.css";
 
 interface Props {
@@ -10,11 +10,25 @@ interface Props {
 
 const SearchMenuItem: React.FC<Props> = ({ onChange }) => {
   const { t } = useTranslation();
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.focus();
+    }
+  }, []);
+
   return (
     <DropdownMenu.DropdownMenuItem className={styles.item} onClick={(e) => e.preventDefault()}>
       <div className={styles.zone}>
         <i className={cls("fr-icon-search-line", styles.icon)} />
-        <input type="text" className={styles.input} placeholder={t("Rechercher", "Rechercher")} onChange={onChange} />
+        <input
+          type="text"
+          ref={ref}
+          className={styles.input}
+          placeholder={t("Rechercher", "Rechercher")}
+          onChange={onChange}
+        />
       </div>
     </DropdownMenu.DropdownMenuItem>
   );
