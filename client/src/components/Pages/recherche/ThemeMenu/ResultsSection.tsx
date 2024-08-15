5b7b6aeca18aec124a1f785b9a1ab78b3e74f61d
Luis Arias <luis@refugies.info>
2024-08-15 10:26:15 +0200

    feat(client): :construction: WIP setting up results sections

--- client/src/components/Pages/recherche/ThemeMenu/ResultsSection.tsx
+++ client/src/components/Pages/recherche/ThemeMenu/ResultsSection.tsx
@@ -1,8 +1,24 @@
+import { GetNeedResponse, GetThemeResponse } from "@refugies-info/api-types";
+import { useLocale } from "hooks";
 import React from "react";
 import styles from "./ResultsSection.module.css";
 
-const ResultsSection: React.FC = () => {
-  return <div className={styles.container}></div>;
+interface Props {
+  theme: GetThemeResponse;
+  needs: GetNeedResponse[];
+}
+
+const ResultsSection: React.FC<Props> = ({ theme, needs }) => {
+  const locale = useLocale();
+
+  return (
+    <div className={styles.container}>
+      <div>{theme.short[locale]}</div>
+      {needs.map((need, i) => (
+        <div key={i}>{need[locale].text}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ResultsSection;
--- client/src/components/Pages/recherche/ThemeMenu/SearchResults.tsx
+++ client/src/components/Pages/recherche/ThemeMenu/SearchResults.tsx
@@ -1,20 +1,38 @@
+import { GetNeedResponse, GetThemeResponse } from "@refugies-info/api-types";
 import { ThemeMenuContext } from "components/Pages/recherche/ThemeMenu/ThemeMenuContext";
 import { useLocale } from "hooks";
+import { sortThemes } from "lib/sortThemes";
 import React, { useContext } from "react";
 import { useSelector } from "react-redux";
 import { needsSelector } from "services/Needs/needs.selectors";
+import { themesSelector } from "services/Themes/themes.selectors";
+import ResultsSection from "./ResultsSection";
 import styles from "./SearchResults.module.css";
+
+interface SectionData {
+  theme: GetThemeResponse;
+  needs: GetNeedResponse[];
+}
 
 const SearchResults: React.FC = () => {
   const { search } = useContext(ThemeMenuContext);
   const allNeeds = useSelector(needsSelector);
   const locale = useLocale();
   const selectedNeeds = allNeeds.filter((need) => need[locale].text.toLowerCase().includes(search.toLowerCase()));
+  const themes = useSelector(themesSelector);
+  const sortedThemes = themes.sort(sortThemes);
+
+  const sectionData = sortedThemes.reduce<SectionData[]>((acc, theme) => {
+    return [...acc, { theme, needs: selectedNeeds.filter((need) => need.theme._id === theme._id) }];
+  }, []);
+
   return (
     <div className={styles.container}>
-      {selectedNeeds.map((need, i) => {
-        return <div key={i}>{need[locale].text}</div>;
-      })}
+      {sectionData
+        .filter(({ needs }) => needs.length > 0)
+        .map(({ theme, needs }, i) => {
+          return <ResultsSection key={i} theme={theme} needs={needs} />;
+        })}
     </div>
   );
 };
