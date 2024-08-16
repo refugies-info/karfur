6394f0724fbe2cc150272af655fbda0ecfc15d07
Luis Arias <luis@refugies.info>
2024-08-16 11:33:00 +0200

    fix(client): :zap: Memoize theme menu search results

--- client/src/components/Pages/recherche/ThemeMenu/SearchResults.tsx
+++ client/src/components/Pages/recherche/ThemeMenu/SearchResults.tsx
@@ -1,9 +1,8 @@
 import { GetNeedResponse, GetThemeResponse } from "@refugies-info/api-types";
 import { ThemeMenuContext } from "components/Pages/recherche/ThemeMenu/ThemeMenuContext";
-import Separator from "components/UI/Separator";
 import { useLocale } from "hooks";
 import { sortThemes } from "lib/sortThemes";
-import React, { useContext } from "react";
+import React, { useContext, useMemo } from "react";
 import { useSelector } from "react-redux";
 import { needsSelector } from "services/Needs/needs.selectors";
 import { themesSelector } from "services/Themes/themes.selectors";
@@ -19,13 +18,15 @@ const SearchResults: React.FC = () => {
   const { search } = useContext(ThemeMenuContext);
   const allNeeds = useSelector(needsSelector);
   const locale = useLocale();
-  const selectedNeeds = allNeeds.filter((need) => need[locale].text.toLowerCase().includes(search.toLowerCase()));
   const themes = useSelector(themesSelector);
-  const sortedThemes = themes.sort(sortThemes);
 
-  const sectionData = sortedThemes.reduce<SectionData[]>((acc, theme) => {
-    return [...acc, { theme, needs: selectedNeeds.filter((need) => need.theme._id === theme._id) }];
-  }, []);
+  const sectionData = useMemo(() => {
+    const selectedNeeds = allNeeds.filter((need) => need[locale].text.toLowerCase().includes(search.toLowerCase()));
+    const sortedThemes = themes.sort(sortThemes);
+    return sortedThemes.reduce<SectionData[]>((acc, theme) => {
+      return [...acc, { theme, needs: selectedNeeds.filter((need) => need.theme._id === theme._id) }];
+    }, []);
+  }, [themes, allNeeds, locale, search]);
 
   return (
     <div className={styles.container}>
