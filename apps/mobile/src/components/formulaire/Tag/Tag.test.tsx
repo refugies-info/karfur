8d342ad11f074af1a68ffb56a55b5c44b5707f46
Luis Arias <luis@refugies.info>
2024-08-20 11:16:01 +0200

    test(mobile): :white_check_mark: Fix test

--- apps/mobile/src/components/formulaire/Tag/Tag.test.tsx
+++ apps/mobile/src/components/formulaire/Tag/Tag.test.tsx
@@ -4,7 +4,7 @@ import { render } from "../../utils/tests";
 
 describe("Tag snapshot test suite", () => {
   it("should render without bug", async () => {
-    const test = await render(<Tag />);
+    const test = await render(<Tag onRemove={() => {}}>Test</Tag>);
     expect(test).toMatchSnapshot();
   });
 });
