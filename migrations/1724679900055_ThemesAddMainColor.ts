46d2f22269ab17fe6d1380e72de5ff5d8c7f2579
Luis Arias <luis@refugies.info>
2024-08-26 15:45:46 +0200

    chore: :truck: Give migration a proper name

--- migrations/1724679900055_ThemesAddMainColor.ts
+++ migrations/1724679900055_ThemesAddMainColor.ts
@@ -3,10 +3,12 @@ import { Db } from "mongodb";
 
 export class Migration1724679449317 implements MigrationInterface {
   public async up(db: Db): Promise<void | never> {
+    // Add the mainColor property to each theme
     await db.collection("themes").updateMany({}, { $set: { mainColor: "#FFFFFF" } });
   }
 
   public async down(db: Db): Promise<void | never> {
+    // Remove mainColor property from each theme
     await db.collection("themes").updateMany({}, { $unset: { mainColor: "" } });
   }
 }
