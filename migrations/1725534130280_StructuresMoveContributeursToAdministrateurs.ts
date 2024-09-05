18a861f63f670e18ae0d3b03a64a4dd6e19e6f08
Luis Arias <luis@refugies.info>
2024-09-05 17:43:53 +0200

    fix: :card_file_box: Fix migration

--- migrations/1725534130280_StructuresMoveContributeursToAdministrateurs.ts
+++ migrations/1725534130280_StructuresMoveContributeursToAdministrateurs.ts
@@ -5,29 +5,38 @@ export class StructuresMoveContributeursToAdministrateurs1725534130280 implement
   public async up(db: Db): Promise<void | never> {
     const collection = db.collection("structures");
 
-    await collection.updateMany(
-      { "membres.roles": "contributeur" },
+    await collection.updateMany({ "membres.roles": "contributeur" }, [
       {
         $set: {
-          "membres.$[elem].roles": {
+          membres: {
             $map: {
-              input: "$$elem.roles",
-              as: "role",
+              input: "$membres",
+              as: "membre",
               in: {
-                $cond: {
-                  if: { $eq: ["$$role", "contributeur"] },
-                  then: "administrateur",
-                  else: "$$role",
-                },
+                $mergeObjects: [
+                  "$$membre",
+                  {
+                    roles: {
+                      $map: {
+                        input: "$$membre.roles",
+                        as: "role",
+                        in: {
+                          $cond: {
+                            if: { $eq: ["$$role", "contributeur"] },
+                            then: "administrateur",
+                            else: "$$role",
+                          },
+                        },
+                      },
+                    },
+                  },
+                ],
               },
             },
           },
         },
       },
-      {
-        arrayFilters: [{ "elem.roles": "contributeur" }],
-      },
-    );
+    ]);
   }
 
   public async down(db: Db): Promise<void | never> {}
