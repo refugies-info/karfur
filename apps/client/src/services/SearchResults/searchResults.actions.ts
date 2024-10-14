c2be67b1a058a79a70274918e28c4d93743776a6
Matthieu Fesselier <contact@matthieufesselier.com>
2024-10-14 16:09:22 +0200

    feat(client): :sparkles: update search with no result

--- apps/client/src/services/SearchResults/searchResults.actions.ts
+++ apps/client/src/services/SearchResults/searchResults.actions.ts
@@ -1,8 +1,10 @@
+import { GetDispositifsResponse } from "@refugies-info/api-types";
 import { action, ActionType } from "typesafe-actions";
-import { ADD_TO_QUERY, SET_RESULTS } from "./searchResults.actionTypes";
+import { ADD_TO_QUERY, SET_NO_RESULTS, SET_RESULTS } from "./searchResults.actionTypes";
 import { Results, SearchQuery } from "./searchResults.reducer";
 
 export const setSearchResultsActionCreator = (results: Results) => action(SET_RESULTS, results);
+export const setNoResultsActionCreator = (results: GetDispositifsResponse[]) => action(SET_NO_RESULTS, results);
 export const addToQueryActionCreator = (query: Partial<SearchQuery>) => action(ADD_TO_QUERY, query);
 export const resetQueryActionCreator = () =>
   action(ADD_TO_QUERY, {
@@ -19,6 +21,6 @@ export const resetQueryActionCreator = () =>
     type: "all",
   });
 
-const actions = { setSearchResultsActionCreator, addToQueryActionCreator };
+const actions = { setSearchResultsActionCreator, setNoResultsActionCreator, addToQueryActionCreator };
 
 export type SearchResultsActions = ActionType<typeof actions>;
--- apps/client/src/services/SearchResults/searchResults.selector.ts
+++ apps/client/src/services/SearchResults/searchResults.selector.ts
@@ -1,8 +1,11 @@
+import { GetDispositifsResponse } from "@refugies-info/api-types";
 import { getThemesDisplayed } from "~/lib/recherche/functions";
 import { RootState } from "../rootReducer";
 import { Results, SearchQuery } from "./searchResults.reducer";
 
 export const searchResultsSelector = (state: RootState): Results => state.searchResults.results;
+
+export const noResultsSelector = (state: RootState): GetDispositifsResponse[] => state.searchResults.noResults;
 
 export const searchQuerySelector = (state: RootState): SearchQuery => state.searchResults.query;
 
