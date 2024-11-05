2b6944cffb30ba7623b49c3c7dc6a95b6e243946
Luis Arias <luis@refugies.info>
2024-11-05 14:48:37 +0100

    fix: :recycle: Move cli.ts to ESM format because of dotenv dependency update

--- migrations/cli.mjs
+++ migrations/cli.mjs
@@ -1,5 +1,10 @@
 import { config } from "dotenv";
 import { mongoMigrateCli } from "mongo-migrate-ts";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 config();
 
