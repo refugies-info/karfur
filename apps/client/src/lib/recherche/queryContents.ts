604c0f78b5076dc26d53723db2dd2e8ba1aee032
Jérémie Gisserot <j@jeremie-gisserot.net>
2024-11-06 16:05:43 +0100

    Fix: 🐛 Broken Sort function

--- apps/client/src/lib/recherche/queryContents.ts
+++ apps/client/src/lib/recherche/queryContents.ts
@@ -82,9 +82,7 @@ export const filterDispositifs = (
     .filter((dispositif) => skip === "public" || filterByPublic(dispositif, query.public))
     .filter((dispositif) => skip === "status" || filterByStatus(dispositif, query.status));
 
-  return rule?.sortFunction && !!skip
-    ? [...filteredDispositifs].sort((a, b) => rule.sortFunction(a, b))
-    : filteredDispositifs;
+  return rule?.sortFunction ? [...filteredDispositifs].sort((a, b) => rule.sortFunction(a, b)) : filteredDispositifs;
 };
 
 const filterSuggestions = (
