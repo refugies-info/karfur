fd07da5f7ee254f7ede39f0481033b7d11fd05a9
Jérémie Gisserot <j@jeremie-gisserot.net>
2024-11-07 15:40:08 +0100

    Fix: adapt Window size hook to use the base font size

--- apps/client/src/hooks/useWindowSize.ts
+++ apps/client/src/hooks/useWindowSize.ts
@@ -11,8 +11,10 @@ const useWindowSize = () => {
     height: typeof window !== "undefined" ? window.innerHeight : undefined,
   });
 
-  const isMobile = windowSize.width && windowSize.width <= 768;
-  const isTablet = windowSize.width && windowSize.width < 992;
+  const [baseFontSize, setBaseFontSize] = useState<number>(16);
+
+  const isMobile = windowSize.width && windowSize.width <= baseFontSize * 48; // 768px
+  const isTablet = windowSize.width && windowSize.width < baseFontSize * 61.5; // 992px
 
   useEffect(() => {
     function handleResize() {
@@ -21,10 +23,28 @@ const useWindowSize = () => {
         height: window.innerHeight,
       });
     }
+
+    function handleFontSizeChange() {
+      // Get the actual font size of the page
+      const computedStyle = window.getComputedStyle(document.body);
+      const fontSize = parseFloat(computedStyle.fontSize);
+      setBaseFontSize(fontSize);
+    }
+
     window.addEventListener("resize", handleResize);
+    window.addEventListener("load", handleFontSizeChange); // Check font size on page load
+    window.addEventListener("zoom", handleFontSizeChange); // Check font size on zoom
+
     handleResize();
-    return () => window.removeEventListener("resize", handleResize);
+    handleFontSizeChange();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("load", handleFontSizeChange);
+      window.removeEventListener("zoom", handleFontSizeChange);
+    };
   }, []);
+
   return { windowSize, isMobile, isTablet };
 };
 
