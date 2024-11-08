a53456f1cb764b264ecf8c665cb25e6155b760f5
Luis Arias <luis@refugies.info>
2024-11-08 10:02:00 +0100

    ci(mobile): :bug: owner property is required for ci/cd eas deployments

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -6,6 +6,7 @@ const androidVersionCode = 33;
 
 export default {
   name: "Réfugiés.info",
+  owner: "refugies-info",
   slug: "refugies-info-app",
   version: "1.0.16",
   orientation: "portrait",
@@ -155,8 +156,7 @@ export default {
       // package: "com.refugiesinfo.app",
       package:
         "com.refugiesinfo.app" +
-        (process.env.EAS_BUILD_PROFILE &&
-        process.env.EAS_BUILD_PROFILE !== "production"
+        (process.env.EAS_BUILD_PROFILE && process.env.EAS_BUILD_PROFILE !== "production"
           ? `.${process.env.EAS_BUILD_PROFILE}`
           : ""),
       config: {
@@ -194,10 +194,7 @@ export default {
         UIBackgroundModes: ["remote-notification"],
       },
       googleServicesFile: "./src/utils/firebase/GoogleService-Info.plist",
-      associatedDomains: [
-        "applinks:refugies.info",
-        "applinks:www.refugies.info",
-      ],
+      associatedDomains: ["applinks:refugies.info", "applinks:www.refugies.info"],
     },
     extra: {
       eas: {
