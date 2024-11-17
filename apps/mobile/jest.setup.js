8c5608f4a3bcd69358f809c2f4b4f595fa6b8077
Luis Arias <luis@refugies.info>
2024-11-17 08:25:17 +0100

    test(mobile): :white_check_mark: Stop mocking missing module

--- apps/mobile/jest.setup.js
+++ apps/mobile/jest.setup.js
@@ -4,9 +4,6 @@
 // Import built-in Jest matchers
 import "@testing-library/react-native/extend-expect";
 
-// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
-jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
-
 jest.useFakeTimers();
 
 // jest.mock("react-native/Libraries/EventEmitter/NativeEventEmitter.js", () => {
