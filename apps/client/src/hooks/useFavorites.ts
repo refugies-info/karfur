e508c874b2a7f5be6f2423541948e2a8c4aa8cbf
Luis Arias <luis@refugies.info>
2024-11-18 18:30:51 +0100

    refactor(client): :recycle: Attempt to only use per local favorites

--- apps/client/src/hooks/useFavorites.ts
+++ apps/client/src/hooks/useFavorites.ts
@@ -1,47 +1,45 @@
-import { GetUserInfoResponse, Id } from "@refugies-info/api-types";
+import { Id, SimpleDispositif } from "@refugies-info/api-types";
 import { useRouter } from "next/router";
 import { useCallback, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUserActionCreator } from "~/services/User/user.actions";
-import { userDetailsSelector } from "~/services/User/user.selectors";
 import { fetchUserFavoritesActionCreator } from "~/services/UserFavoritesInLocale/UserFavoritesInLocale.actions";
+import { userFavoritesSelector } from "~/services/UserFavoritesInLocale/UserFavoritesInLocale.selectors";
 import API from "~/utils/API";
 import useAuth from "./useAuth";
 
-export const isContentFavorite = (userDetails: GetUserInfoResponse | null, id: Id | null) => {
+export const isContentFavorite = (favorites: SimpleDispositif[], id: Id | null) => {
   if (id === null) return false;
-  if ((userDetails?.favorites || []).length === 0) return false;
-  return !!(userDetails?.favorites || []).find((c) => c.dispositifId === id);
+  if (favorites.length === 0) return false;
+  return !!favorites.find((c) => c._id === id);
 };
 
 const useFavorites = (contentId: Id | null) => {
-  const userDetails = useSelector(userDetailsSelector);
+  const favorites = useSelector(userFavoritesSelector);
   const router = useRouter();
   const locale = useMemo(() => router.locale || "fr", [router.locale]);
 
   const dispatch = useDispatch();
   const { isAuth } = useAuth();
 
-  const isFavorite = useMemo(() => isContentFavorite(userDetails, contentId), [userDetails, contentId]);
+  const isFavorite = useMemo(() => isContentFavorite(favorites, contentId), [favorites, contentId]);
 
   const successCallback = useCallback(() => {
-    dispatch(fetchUserActionCreator());
     dispatch(fetchUserFavoritesActionCreator(locale));
   }, [dispatch, locale]);
 
   const addToFavorites = useCallback(() => {
-    if (isAuth && userDetails) {
+    if (isAuth) {
       if (isFavorite || !contentId) return;
       API.addUserFavorite({ dispositifId: contentId.toString() }).then(successCallback);
     }
-  }, [userDetails, contentId, isFavorite, successCallback, isAuth]);
+  }, [contentId, isFavorite, successCallback, isAuth]);
 
   const deleteFromFavorites = useCallback(() => {
-    if (isAuth && userDetails) {
+    if (isAuth) {
       if (!isFavorite || !contentId) return;
       API.deleteUserFavorites({ dispositifId: contentId.toString() }).then(successCallback);
     }
-  }, [userDetails, contentId, isFavorite, successCallback, isAuth]);
+  }, [contentId, isFavorite, successCallback, isAuth]);
 
   return { isFavorite, addToFavorites, deleteFromFavorites };
 };
