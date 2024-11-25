b4519e8579e3a64fbc7d63e5d0086bedda4cecd6
Jérémie Gisserot <j@jeremie-gisserot.net>
2024-11-25 11:09:59 +0100

    Fix: wrong search label

--- apps/client/src/components/Pages/recherche/SearchHeader/SearchInput.tsx
+++ apps/client/src/components/Pages/recherche/SearchHeader/SearchInput.tsx
@@ -2,6 +2,7 @@ import Input from "@codegouvfr/react-dsfr/Input";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
+import useStylesDisabled from "~/hooks/useStyleDisabled";
 import { cls } from "~/lib/classname";
 import { searchQuerySelector } from "~/services/SearchResults/searchResults.selector";
 import styles from "./SearchInput.module.css";
@@ -15,12 +16,16 @@ const SearchInput: React.FC<Props> = ({ onChange, className }) => {
   const { t } = useTranslation();
   const query = useSelector(searchQuerySelector);
 
+  const styleDisabled = useStylesDisabled();
+
+  const hintText = styleDisabled ? t("Recherche.keyword", "Rechercher par mot-clé") : "";
+
   return (
     <Input
       iconId="fr-icon-search-line"
       className={cls(styles.container, className)}
-      label="Url du site :"
-      hintText="Saisissez une url valide, commençant par https://"
+      label=""
+      hintText={hintText}
       nativeInputProps={{
         placeholder: t("Recherche.keyword", "Rechercher par mot-clé"),
         onChange,
