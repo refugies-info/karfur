e124394f52634a759b477bc528b1161777f0e784
Luis Arias <luis@refugies.info>
2024-11-29 19:05:32 +0100

    fix(client): :bug: Handle special case where the user that is added or removed from a structure is the current user

--- apps/client/src/components/Backend/screens/UserStructure/UserStructureAdmin.component.tsx
+++ apps/client/src/components/Backend/screens/UserStructure/UserStructureAdmin.component.tsx
@@ -1,8 +1,10 @@
 import { GetStructureResponse, Id, PatchStructureRolesRequest } from "@refugies-info/api-types";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Swal from "sweetalert2";
 import { userSelector } from "~/services/User/user.selectors";
+import { fetchUserContributionsActionCreator } from "~/services/UserContributions/userContributions.actions";
+import { fetchUserStructureActionCreator } from "~/services/UserStructure/userStructure.actions";
 import API from "~/utils/API";
 import { colors } from "~/utils/colors";
 import { UserStructureDetails } from "./components/UserStructureDetails";
@@ -22,6 +24,8 @@ export const UserStructureAdminComponent = (props: Props) => {
 
   const toggleReload = () => setReload(!reload);
   const user = useSelector(userSelector);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     document.title = props.title;
@@ -57,6 +61,15 @@ export const UserStructureAdminComponent = (props: Props) => {
     await API.updateStructureMembers(structure._id, query);
     setIsLoading(false);
     toggleReload();
+    if (user.userId?.toString() === userId.toString()) {
+      dispatch(fetchUserContributionsActionCreator());
+      dispatch(
+        fetchUserStructureActionCreator({
+          structureId: structure._id,
+          shouldRedirect: false,
+        }),
+      );
+    }
   };
 
   const deleteUserFromStructure = async (userId: Id) => {
@@ -81,6 +94,15 @@ export const UserStructureAdminComponent = (props: Props) => {
         await API.updateStructureMembers(structure._id, query);
         setIsLoading(false);
         toggleReload();
+        if (user.userId?.toString() === userId.toString()) {
+          dispatch(fetchUserContributionsActionCreator());
+          dispatch(
+            fetchUserStructureActionCreator({
+              structureId: structure._id,
+              shouldRedirect: false,
+            }),
+          );
+        }
       }
     });
   };
