dc10bd36bf578936e0328bef17fd789873b25a33
Luis Arias <luis@refugies.info>
2024-12-09 16:39:21 +0100

    refactor(server): :recycle: Rename sendinblue connector to brevo and fix typing issues

--- apps/server/src/modules/dispositif/dispositif.repository.ts
+++ apps/server/src/modules/dispositif/dispositif.repository.ts
@@ -178,12 +178,13 @@ export const getStructureDispositifs = async (
   )
     .then(async (dispositifs) => {
       const usernames = await Promise.all(
-        dispositifs.map((dispositif) =>
-          dispositif.suggestions.length > 0
-            ? getUsersById(uniq(dispositif.suggestions.map((s: any) => s.userId).filter((id: any) => !!id)), {
-                username: 1,
-              })
-            : [],
+        dispositifs.map(
+          (dispositif): Promise<any[]> =>
+            dispositif.suggestions.length > 0
+              ? getUsersById(uniq(dispositif.suggestions.map((s: any) => s.userId).filter((id: any) => !!id)), {
+                  username: 1,
+                })
+              : Promise.resolve([]),
         ),
       );
       return { dispositifs, usernames: union(...usernames) };
--- apps/server/src/workflows/users/register/register.test.ts
+++ apps/server/src/workflows/users/register/register.test.ts
@@ -1,5 +1,5 @@
 import { RoleName } from "@refugies-info/api-types";
-import { addToNewsletter } from "~/connectors/sendinblue/addToNewsletter";
+import { addToNewsletter } from "~/connectors/brevo/addToNewsletter";
 import * as password from "~/libs/validatePassword";
 import { loginExceptionsManager } from "~/modules/users/auth";
 import { LoginErrorType } from "~/modules/users/LoginError";
