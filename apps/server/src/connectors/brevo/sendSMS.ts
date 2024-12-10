8730cc31da9567e08042ee05894e06b01f27c06c
Luis Arias <luis@refugies.info>
2024-12-10 17:15:50 +0100

    feat(server): :sparkles: Make sure phone number is e.164 formatted

--- apps/server/src/connectors/brevo/sendSMS.ts
+++ apps/server/src/connectors/brevo/sendSMS.ts
@@ -1,4 +1,5 @@
 import { HttpError, SendTransacSms, TransactionalSMSApi, TransactionalSMSApiApiKeys } from "@getbrevo/brevo";
+import { phone as parse } from "phone";
 import { SendSMSResult } from "~/services";
 
 const apiInstance = new TransactionalSMSApi();
@@ -7,10 +8,18 @@ const { BREVO_API_KEY, SMS_SENDER } = process.env;
 
 apiInstance.setApiKey(TransactionalSMSApiApiKeys.apiKey, BREVO_API_KEY);
 
+const e164 = (phone: string): string => {
+  let res = parse(phone, { country: null });
+  if (res.isValid) return res.phoneNumber;
+  res = parse(phone, { country: "FR" });
+  if (res.isValid) return res.phoneNumber;
+  return phone;
+};
+
 export const sendSMS = async (text: string, phone: string): Promise<SendSMSResult> => {
   const sms = new SendTransacSms();
   sms.content = text;
-  sms.recipient = phone;
+  sms.recipient = e164(phone);
   sms.sender = SMS_SENDER.replace("+", "00");
   sms.organisationPrefix = "Réfugiés.info";
 
