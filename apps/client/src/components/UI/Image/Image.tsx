af609e65ccb9d6bb9646328d3d167447804d520b
Luis Arias <luis@refugies.info>
2024-12-13 10:59:28 +0100

    fix(client): :bug: Handle full image URLs in cloudinary loader

--- apps/client/src/components/UI/Image/Image.tsx
+++ apps/client/src/components/UI/Image/Image.tsx
@@ -6,7 +6,11 @@ import { forwardRef } from "react";
 // Demo: https://res.cloudinary.com/demo/image/upload/w_300,c_limit,q_auto/turtles.jpg
 export const cloudinaryLoader: ImageLoader = ({ src, width, quality }) => {
   const params = ["f_auto", "c_limit", `w_${width}`, `q_${quality || "auto"}`];
-  return `https://res.cloudinary.com/dlmqnnhp6/image/upload/${params.join(",")}${src}`;
+  if (src.startsWith("/")) {
+    return `https://res.cloudinary.com/dlmqnnhp6/image/upload/${params.join(",")}${src}`;
+  }
+  const parts = src.split("/upload/");
+  return `${parts[0]}/upload/${params.join(",")}/${parts[1]}`;
 };
 
 export const Image: typeof NextImage = forwardRef(({ src, loader, ...props }, ref) => {
