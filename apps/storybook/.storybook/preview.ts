6b80bfd0a606fdf26af247f6a5518608bf975856
Jérémie Gisserot <j@jeremie-gisserot.net>
2024-12-19 16:00:26 +0100

    Chore: update Storybook + plugins install

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,6 +1,49 @@
 import type { Preview } from "@storybook/react";
 import "./globals.css";
 
+// mobile: 36em; /* 576px */
+// tablet : 48em; /* 768px */
+// desktop-md : 62em; /* 992px */
+// desktop-lg : 75em; /* 1200px */
+
+const RIViewports = {
+  "mobile": {
+    name: "mobile",
+    styles: {
+      width: "576px",
+      height: "100%",
+    },
+  },
+  "tablet": {
+    name: "tablet",
+    styles: {
+      width: "768px",
+      height: "100%",
+    },
+  },
+  "desktop-md": {
+    name: "Desktop medium",
+    styles: {
+      width: "992px",
+      height: "100%",
+    },
+  },
+  "desktop-lg": {
+    name: "Desktop large",
+    styles: {
+      width: "1200px",
+      height: "100%",
+    },
+  },
+  "desktop-xl": {
+    name: "Desktop Xl",
+    styles: {
+      width: "1440px",
+      height: "100%",
+    },
+  },
+};
+
 const preview: Preview = {
   parameters: {
     controls: {
@@ -21,6 +64,12 @@ const preview: Preview = {
           value: "#0a0a0a",
         },
       ],
+    },
+
+    viewport: {
+      viewports: {
+        ...RIViewports,
+      },
     },
   },
 };
