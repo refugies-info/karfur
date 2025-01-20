5b7aca5cb9093ec579a7a46d242f8b2c9b31919d
Matthieu Fesselier <contact@matthieufesselier.com>
2025-01-20 15:48:44 +0100

    feat(client): :sparkles: add section mission

--- apps/client/src/pages/mission-et-impact.tsx
+++ apps/client/src/pages/mission-et-impact.tsx
@@ -3,7 +3,9 @@ import { useTranslation } from "next-i18next";
 import Image from "next/image";
 import HeroBack from "~/assets/staticPages/mission-et-impact/hero-back.svg";
 import HeroMobile from "~/assets/staticPages/mission-et-impact/hero-illu-mobile.svg";
-import { Section } from "~/components/Pages/staticPages/common";
+import AlainRegnier from "~/assets/staticPages/mission-et-impact/photo-alain-regnier.png";
+import ScreenshotRI from "~/assets/staticPages/mission-et-impact/ri-screenshot.png";
+import { Section, Title2 } from "~/components/Pages/staticPages/common";
 import SEO from "~/components/Seo";
 import { defaultStaticProps } from "~/lib/getDefaultStaticProps";
 
@@ -24,6 +26,37 @@ const MissionImpact: NextPage = () => {
           <p className="!text-chapo !mb-0">{t("MissionImpact.subtitle")}</p>
         </div>
         <Image src={HeroMobile} alt="" width={343} className="lg:hidden mt-10 mx-auto max-w-full" />
+      </Section>
+
+      <Section>
+        <div className="fr-container">
+          <div className="flex gap-10 md:gap-20 flex-col md:flex-row">
+            <div className="flex-1">
+              <Title2 smallMb className="!text-left">
+                {t("MissionImpact.mission_title")}
+              </Title2>
+              <p className="!text-h4 md:!text-h3 text-purple-france !mb-0">{t("MissionImpact.mission_subtitle")}</p>
+              <Image src={ScreenshotRI} alt="" width={540} height={357} className="mt-10 md:mt-14 mx-auto max-w-full" />
+            </div>
+            <div className="flex-1">
+              <p
+                className="!text-large"
+                dangerouslySetInnerHTML={{
+                  __html: t("MissionImpact.mission_p1"),
+                }}
+              />
+              <p className="!text-large">{t("MissionImpact.mission_p2")}</p>
+              <p className="!text-large !mb-0">{t("MissionImpact.mission_p3")}</p>
+              <div className="mt-10 flex gap-6 items-center">
+                <Image src={AlainRegnier} alt="Alain Regnier - Photo" width={120} height={120} />
+                <p className="!text-large !mb-0">
+                  <strong className="block">Alain Régnier</strong>
+                  <span>{t("MissionImpact.mission_alain_position")}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </Section>
     </div>
   );
