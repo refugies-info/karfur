65ce2862c8436e8afb22875a819f25e791ca16aa
Luis Arias <luis@refugies.info>
2025-02-24 14:08:26 +0100

    refactor(mobile): :recycle: Move to using native env var support in RN

--- apps/mobile/build-scripts/withGoogleMapsApiKey.js
+++ apps/mobile/build-scripts/withGoogleMapsApiKey.js
@@ -9,12 +9,11 @@
 const { AndroidConfig, withAndroidManifest } = require("@expo/config-plugins");
 
 // Using helpers keeps error messages unified and helps cut down on XML format changes.
-const { addMetaDataItemToMainApplication, getMainApplicationOrThrow } =
-  AndroidConfig.Manifest;
+const { addMetaDataItemToMainApplication, getMainApplicationOrThrow } = AndroidConfig.Manifest;
 
 // Splitting this function out of the mod makes it easier to test.
 async function setCustomConfigAsync(config, androidManifest) {
-  const appId = process.env.GOOGLE_API_KEY || "";
+  const appId = process.env.EXPO_PUBLIC_GOOGLE_API_KEY || "";
   // Get the <application /> tag and assert if it doesn't exist.
   const mainApplication = getMainApplicationOrThrow(androidManifest);
 
@@ -23,7 +22,7 @@ async function setCustomConfigAsync(config, androidManifest) {
     // value for `android:name`
     "com.google.android.geo.API_KEY",
     // value for `android:value`
-    appId
+    appId,
   );
 
   return androidManifest;
