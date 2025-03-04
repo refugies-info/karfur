c4aca2bd24331ea3de8d6bfacf532ae0f46495a4
Luis Arias <luis@refugies.info>
2025-03-04 13:55:44 +0100

    chore(mobile): :wrench: Try building for android 8

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -44,7 +44,8 @@ export default {
   },
   android: {
     versionCode: androidVersionCode,
-    minSdkVersion: 29,
+    // Support for Android 8 - https://endoflife.date/android
+    minSdkVersion: 26,
     userInterfaceStyle: "light",
     adaptiveIcon: {
       foregroundImage: "./src/theme/images/app-icon-ri-adaptive.png",
