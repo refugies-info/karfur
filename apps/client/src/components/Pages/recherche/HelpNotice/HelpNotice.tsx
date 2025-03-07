6dba057764ca2a755060ac2d0a0ce1c992a770b9
Luis Arias <luis@refugies.info>
2025-03-07 16:54:27 +0100

    feat(client): :sparkles: Only view the notice if in French

--- apps/client/src/components/Pages/recherche/HelpNotice/HelpNotice.tsx
+++ apps/client/src/components/Pages/recherche/HelpNotice/HelpNotice.tsx
@@ -1,21 +1,25 @@
 import Notice from "@codegouvfr/react-dsfr/Notice";
+import { useLocale } from "~/hooks";
 
-export const HelpNotice = () => (
-  <Notice
-    isClosable
-    title={
-      <>
-        Réfugiés.info a été nommé Service numérique à impact national !&nbsp;
-        <a
-          href="https://beta.gouv.fr/startups/refugies.info#impact"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline"
-        >
-          Découvrir l'impact du projet
-        </a>
-        .
-      </>
-    }
-  />
-);
+export const HelpNotice = () => {
+  const locale = useLocale();
+  return locale === "fr" ? (
+    <Notice
+      isClosable
+      title={
+        <>
+          Réfugiés.info a été nommé Service numérique à impact national !&nbsp;
+          <a
+            href="https://beta.gouv.fr/startups/refugies.info#impact"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            Découvrir l'impact du projet
+          </a>
+          .
+        </>
+      }
+    />
+  ) : null;
+};
