738b2090634c3cf06e2525b75ef3febd31c9ec09
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-03-13 17:56:26 +0100

    Chore: Fix attribute js error

--- packages/ui/src/components/Carrousel.tsx
+++ packages/ui/src/components/Carrousel.tsx
@@ -234,7 +234,7 @@ export const Carrousel = ({ texts, children, className, seeMoreUrl }: CarrouselP
           "overscrollBehaviorX": "contain",
           "cursor": "grab",
           // @ts-ignore
-          "&::-webkit-scrollbar": { display: "none" },
+          "&::WebkitScrollbar": { display: "none" },
           "&:active": { cursor: "grabbing" },
         }}
       >
