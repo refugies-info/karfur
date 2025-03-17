06ec85518301ddee11aaa85bb65bbf97886808f5
Luis Arias <luis@refugies.info>
2025-03-17 11:45:13 +0100

    fix(client): :bug: Add phone number to correct department

--- apps/client/src/data/agirOperators.ts
+++ apps/client/src/data/agirOperators.ts
@@ -27,6 +27,7 @@ export const operators: Operator[] = [
     department: "03 - Allier",
     operator: "Coallia",
     email: "agir.allier@coallia.org",
+    phone: "06 24 42 52 06",
   },
   {
     dispositifId: "660d1f38de63124662360680",
@@ -40,7 +41,6 @@ export const operators: Operator[] = [
     department: "05 - Hautes-Alpes",
     operator: "France terre d'asile - ACPM",
     email: "agir05@france-terre-asile.org",
-    phone: "06 24 42 52 06",
   },
   {
     dispositifId: "660d1f36de6312466236065e",
