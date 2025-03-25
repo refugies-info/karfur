0a5bae3dbf4ca71bb04be2c2f81b54c1dbe152cd
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-03-25 16:13:57 +0100

    Chore: update translations

--- apps/client/src/components/Pages/homepage/Sections/MobileApp/MobileApp.tsx
+++ apps/client/src/components/Pages/homepage/Sections/MobileApp/MobileApp.tsx
@@ -115,7 +115,7 @@ const MobileApp = () => {
               className={cn("justify-center max-md:w-full", isAndroid && "hidden")}
               onClick={() => handleOpenStoreLink(iosStoreLink)}
             >
-              {t("MobileApp.downloadButtonText", "Je télécharge l’application")}
+              {t("MobileApp.downloadButtonText", "Télécharger l’applicationn")}
             </Button>
             <Button
               iconId={"ri-android-fill"}
@@ -123,7 +123,7 @@ const MobileApp = () => {
               className={cn("justify-center max-md:w-full", isIOS && "hidden")}
               onClick={() => handleOpenStoreLink(androidStoreLink)}
             >
-              {t("MobileApp.downloadButtonText", "Je télécharge l’application")}
+              {t("MobileApp.downloadButtonText", "Télécharger l’application")}
             </Button>
           </span>
         ) : (
