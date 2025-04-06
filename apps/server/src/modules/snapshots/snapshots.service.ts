1f0a4ab58b666670ee68450978131619b916424f
Luis Arias <luis@refugies.info>
2025-04-06 19:20:01 +0200

    feat(server): :construction: Add version tracking for snapshots and error handling for missing content

--- apps/server/src/modules/snapshots/snapshots.service.ts
+++ apps/server/src/modules/snapshots/snapshots.service.ts
@@ -1,13 +1,27 @@
+import logger from "~/logger";
 import { createSnapshot } from "~/modules/snapshots/snapshots.repository";
-import { Dispositif, ObjectId, Snapshot, SnapshotType } from "~/typegoose";
+import { Dispositif, ObjectId, Snapshot, SnapshotModel, SnapshotType } from "~/typegoose";
 
 export const takeSnapshot = async (dispositif: Dispositif, type: SnapshotType, from: string, to: string) => {
+  const content = dispositif.translations?.fr?.content;
+
+  if (!content) {
+    logger.error("[takeSnapshot] No content found for dispositif", dispositif._id);
+    return undefined;
+  }
+
+  // Find the highest version for the given dispositifId
+  const latestSnapshot = await SnapshotModel.findOne({ dispositifId: dispositif._id }).sort({ version: -1 }).lean();
+  const newVersion = latestSnapshot ? latestSnapshot.version + 1 : 1;
+
+  // Create snapshot
   const snapshot = new Snapshot();
   snapshot.dispositifId = new ObjectId(dispositif._id.toString());
-  snapshot.version = 1;
+  snapshot.version = newVersion;
   snapshot.snapshotType = type;
   snapshot.transitionFrom = from;
   snapshot.transitionTo = to;
-  snapshot.dispositifData = dispositif.translations["fr"].content;
+  snapshot.dispositifData = content;
+
   return createSnapshot(snapshot);
 };
