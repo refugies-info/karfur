69f46ac104afaa91ccbafc0de968c53df8793e38
Luis Arias <luis@refugies.info>
2025-04-16 20:03:59 +0200

    fix(client): :lipstick: Remove shadow from Contributors component temporarily

--- apps/client/src/components/Pages/dispositif/Contributors/Contributors.tsx
+++ apps/client/src/components/Pages/dispositif/Contributors/Contributors.tsx
@@ -20,7 +20,8 @@ const Contributors = () => {
   }, [dispositif?.participants]);
 
   return (
-    <div className="bg-alt-blue-france flex w-full flex-col gap-14 p-4 shadow-(--shadow-ri) xl:p-14">
+    // TODO: Add shadow back before full release: shadow-(--shadow-ri)
+    <div className="bg-alt-blue-france flex w-full flex-col gap-14 p-4 xl:p-14">
       <p className="text-title-grey mb-0 text-[2rem] leading-[2.5rem] font-bold">
         {t("Dispositif.contributors", { count: participants.length })}
       </p>
