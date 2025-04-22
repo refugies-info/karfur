deb2313b3c30e45742b620491b82171b12807444
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-04-22 14:20:09 +0200

    Chore: fix console.error

--- packages/ui/src/components/primitives/icons/thumb-up-animated/ThumbUpAnimated.tsx
+++ packages/ui/src/components/primitives/icons/thumb-up-animated/ThumbUpAnimated.tsx
@@ -42,6 +42,7 @@ export const ThumbUpAnimated = forwardRef<ThumbUpAnimatedRef, ThumbUpAnimatedPro
               dotLottieInstance.stop();
               dotLottieInstance.setFrame(frameNumber);
             } catch (error) {
+              // eslint-disable-next-line no-console
               console.error("Error setting frame:", error);
             }
           }
