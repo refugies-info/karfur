ce81e3df3de2768c1ceba6690a6163d831fd6334
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-05-13 22:35:41 +0200

    Chore: fix accordions

--- apps/client/src/components/Pages/dispositif/Accordions/Accordions.tsx
+++ apps/client/src/components/Pages/dispositif/Accordions/Accordions.tsx
@@ -1,13 +1,23 @@
+/* eslint-disable no-use-before-define */
 "use client";
 
 import { fr } from "@codegouvfr/react-dsfr";
 import Accordion from "@codegouvfr/react-dsfr/Accordion";
-import { ContentType, InfoSections } from "@refugies-info/api-types";
-import { useContext } from "react";
+import { ContentType, InfoSection, InfoSections } from "@refugies-info/api-types";
+import { useContext, useMemo, useState } from "react";
+import { cn } from "~/lib/classname";
 import PageContext from "~/utils/pageContext";
 import { AccordionsEdit } from "../Edition";
 import SectionButtons from "../SectionButtons";
 import Text from "../Text";
+import styles from "./Accordions.module.scss";
+
+interface AccordionItemProps {
+  sectionKey: string;
+  sectionId: string;
+  section: InfoSection;
+  mode: string;
+}
 
 interface Props {
   content: InfoSections | undefined;
@@ -23,36 +33,66 @@ const Accordions = ({ content, sectionKey, contentType }: Props) => {
 
   return pageContext.mode !== "edit" ? (
     <div className={fr.cx("fr-accordions-group")}>
-      {Object.entries(content || []).map((section) => {
-        return (
-          <Accordion
-            key={section[0]}
-            className="[&_h3_button]:text-title-grey [&_h3_button]:grid-cols-[1fr_auto_auto] [&_h3_button]:items-center [&_h3_button]:gap-1 md:[&_h3_button]:grid"
-            label={
-              <>
-                {section[1].title}
-                <SectionButtons id={`${sectionKey}.${section[0]}`} content={section[1]} />
-              </>
-            }
-          >
-            <div className="flex items-start justify-between gap-2 max-sm:flex-col-reverse [&_p:last-child]:mb-0">
-              <Text className="prose max-w-full" id={`${sectionKey}.${section[0]}.text`} html>
-                {section[1].text}
-              </Text>
-              {pageContext.mode === "view" && (
-                <SectionButtons
-                  className="w-fit flex-col-reverse"
-                  id={`${sectionKey}.${section[0]}`}
-                  content={section[1]}
-                />
-              )}
-            </div>
-          </Accordion>
-        );
-      })}
+      {Object.entries(content || []).map(([sectionId, section]) => (
+        <AccordionItem
+          key={sectionId}
+          sectionId={sectionId}
+          sectionKey={sectionKey}
+          section={section}
+          mode={pageContext.mode}
+        />
+      ))}
     </div>
   ) : (
     <AccordionsEdit sectionKey={sectionKey} contentType={contentType} />
+  );
+};
+
+const AccordionItem = ({ sectionKey, sectionId, section, mode }: AccordionItemProps) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const sectionTitle = useMemo(() => {
+    const titleMatch = section.title.match(/^(\d+)\s*-?\s*(.*)$/);
+    return titleMatch ? (
+      <span className="flex items-center gap-2">
+        <span className="bg-action-high-blue-france rounded-full px-2 py-0 text-white">{titleMatch[1]}</span>
+        {titleMatch[2]}
+      </span>
+    ) : (
+      section.title
+    );
+  }, [section.title]);
+
+  return (
+    <Accordion
+      key={sectionId}
+      defaultExpanded={expanded}
+      onExpandedChange={(value) => setExpanded(!value)}
+      className={cn(
+        styles.accordion,
+        "[&_h3_button]:text-title-grey",
+        "[&_h3_button]:after:hidden",
+        "[&_h3_button]:grid-cols-[1fr_auto_auto] [&_h3_button]:items-center [&_h3_button]:justify-center [&_h3_button]:gap-1 md:[&_h3_button]:grid",
+      )}
+      label={
+        <>
+          <span>{sectionTitle}</span>{" "}
+          <span className="flex items-center">
+            <i className="ri-add-fill scale-75" />
+            <i className="ri-subtract-fill scale-75" />
+          </span>
+        </>
+      }
+    >
+      <div className="flex items-start justify-between gap-2 max-sm:flex-col-reverse [&_p:last-child]:mb-0">
+        <Text className="prose max-w-full" id={`${sectionKey}.${sectionId}.text`} html>
+          {section.text}
+        </Text>
+        {mode === "view" && (
+          <SectionButtons className="w-fit flex-col-reverse" id={`${sectionKey}.${sectionId}`} content={section} />
+        )}
+      </div>
+    </Accordion>
   );
 };
 
