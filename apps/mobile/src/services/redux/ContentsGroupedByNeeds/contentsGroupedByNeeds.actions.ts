a37a3e642bb387a22b640edd20c7374075e2642b
Luis Arias <luis@refugies.info>
2025-07-09 12:06:27 +0200

    chore: add eslint-disable comments for unused actions objects

--- apps/mobile/src/services/redux/ContentsGroupedByNeeds/contentsGroupedByNeeds.actions.ts
+++ apps/mobile/src/services/redux/ContentsGroupedByNeeds/contentsGroupedByNeeds.actions.ts
@@ -5,7 +5,9 @@ import { SET_GROUPED_CONTENTS } from "./contentsGroupedByNeeds.actionTypes";
 export const setGroupedContentsActionCreator = (value: Record<ObjectId, ObjectId[]>) =>
   action(SET_GROUPED_CONTENTS, value);
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const actions = {
   setGroupedContentsActionCreator,
 };
+
 export type GroupedContentsActions = ActionType<typeof actions>;
