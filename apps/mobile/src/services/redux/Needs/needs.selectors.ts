fae94c050b0ed490ab4d5cb1762b07940ce3bb01
Luis Arias <luis@refugies.info>
2025-07-09 11:59:30 +0200

    fix: update locale type to Languages enum in needNameSelector function

--- apps/mobile/src/services/redux/Needs/needs.selectors.ts
+++ apps/mobile/src/services/redux/Needs/needs.selectors.ts
@@ -1,4 +1,4 @@
-import { GetNeedResponse, Id } from "@refugies-info/api-types";
+import { GetNeedResponse, Id, Languages } from "@refugies-info/api-types";
 import { RootState } from "../reducers";
 
 export const needsSelector = (state: RootState) => state.needs;
@@ -7,11 +7,10 @@ export const needSelector =
   (state: RootState): GetNeedResponse | undefined =>
     needsSelector(state).find((need) => need._id === id);
 
-export const needNameSelector = (id: string, locale: string | null) => (state: RootState) => {
+export const needNameSelector = (id: string, locale: Languages | null) => (state: RootState) => {
   if (!locale) return "";
   const filteredNeeds = state.needs.filter((need) => need._id.toString() === id);
   if (filteredNeeds.length > 0) {
-    // @ts-ignore
     if (filteredNeeds[0][locale]) return filteredNeeds[0][locale].text;
     return filteredNeeds[0]["fr"].text;
   }
