b2490d36b0ffe3d5a98ce0a75a4b53d8ff7f8257
Luis Arias <luis@refugies.info>
2025-07-09 12:18:03 +0200

    refactor: add type safety to i18next translation loader with BackendModule interface

--- apps/mobile/src/services/i18n/translation-loader.ts
+++ apps/mobile/src/services/i18n/translation-loader.ts
@@ -1,20 +1,19 @@
 import { Languages } from "@refugies-info/api-types";
+import { BackendModule, CallbackError, ReadCallback } from "i18next";
 import * as config from "~/config/i18n";
 
-const translationLoader = {
+const translationLoader: BackendModule = {
   type: "backend",
   init: () => {},
-  // eslint-disable-next-line no-unused-vars
-  read: function (language: Languages, _: unknown, callback: (error: unknown, resource: unknown) => void) {
+  read: function (language: string, namespace: string, callback: ReadCallback) {
     let resource,
-      error = null;
+      error: CallbackError | null = null;
     try {
-      resource = config.supportedLocales[language].translationFileLoader();
+      resource = config.supportedLocales[language as Languages].translationFileLoader();
     } catch (_error) {
-      error = _error;
+      error = _error as CallbackError;
     }
     callback(error, resource);
   },
 };
-
 export default translationLoader;
