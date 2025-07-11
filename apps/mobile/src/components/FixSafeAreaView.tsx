a32d78a360ab5d440dd6dceac084bf778232e05d
Luis Arias <luis@refugies.info>
2025-07-11 19:01:00 +0200

    refactor: add proper TypeScript types to FixSafeAreaView component

--- apps/mobile/src/components/FixSafeAreaView.tsx
+++ apps/mobile/src/components/FixSafeAreaView.tsx
@@ -1,4 +1,5 @@
-import { SafeAreaView } from "react-native-safe-area-context";
+import { type PropsWithChildren } from "react";
+import { SafeAreaView, type SafeAreaViewProps } from "react-native-safe-area-context";
 import styled from "styled-components/native";
 
 const StyledSafeAreaView = styled(SafeAreaView)`
@@ -6,7 +7,9 @@ const StyledSafeAreaView = styled(SafeAreaView)`
   position: relative;
 `;
 
-export const FixSafeAreaView = (props: any) => {
+type FixSafeAreaViewProps = PropsWithChildren<SafeAreaViewProps>;
+
+export const FixSafeAreaView = (props: FixSafeAreaViewProps) => {
   // there is an issue with react-native-safe-area-context on iPhone12 height is too big. I have not found simple fix other than what is below
   return <StyledSafeAreaView {...props}>{props.children}</StyledSafeAreaView>;
 };
