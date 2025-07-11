e3ae5e58340054fe6186b544c43e32abd312214f
Luis Arias <luis@refugies.info>
2025-07-11 07:12:17 +0200

    refactor: add type safety to back button navigation with proper TypeScript types

--- apps/mobile/src/libs/backButton.ts
+++ apps/mobile/src/libs/backButton.ts
@@ -1,12 +1,29 @@
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { CompositeNavigationProp } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
 import { BackHandler } from "react-native";
+import { BottomTabParamList, RootStackParamList } from "~/types/navigation";
 
-export const registerBackButton = (backScreen: string | undefined, navigation: any) => {
+type NavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<BottomTabParamList>,
+  StackNavigationProp<RootStackParamList>
+>;
+
+type ValidScreen = keyof RootStackParamList | keyof BottomTabParamList;
+
+export const registerBackButton = (backScreen: ValidScreen | undefined, navigation: NavigationProp) => {
   if (backScreen) {
     const backAction = () => {
       try {
         navigation.popToTop();
-      } catch (e) {}
-      navigation.navigate(backScreen);
+        // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+      } catch (e: unknown) {
+        // Do nothing
+      }
+      // We need to use type assertion here because TypeScript can't infer the exact type
+      // of the screen name at compile time, but we've already validated it with ValidScreen type
+      // Using 'as never' is safe here because we've already validated the screen name
+      navigation.navigate(backScreen as never);
       return true;
     };
     const backHandler = BackHandler.addEventListener("hardwareBackPress", backAction);
