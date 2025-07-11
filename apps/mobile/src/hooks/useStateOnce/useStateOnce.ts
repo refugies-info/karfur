5464f3892decbf97b316fba50f57d9ad7ef47f6d
Luis Arias <luis@refugies.info>
2025-07-11 07:20:12 +0200

    refactor: simplify generic type and callback typing in useStateOnce hook

--- apps/mobile/src/hooks/useStateOnce/useStateOnce.ts
+++ apps/mobile/src/hooks/useStateOnce/useStateOnce.ts
@@ -1,11 +1,11 @@
 import { useCallback, useState } from "react";
 import useBoolean from "react-use/lib/useBoolean";
 
-const useStateOnce = <T extends unknown>(defaultValue?: T) => {
+const useStateOnce = <T>(defaultValue?: T) => {
   const [setted, toggleSetted] = useBoolean(false);
   const [value, setValue] = useState<T | undefined>(defaultValue);
-  const setter = useCallback<any>(
-    (newValue: T): any => {
+  const setter = useCallback(
+    (newValue: T) => {
       if (!setted) {
         setValue(newValue);
         toggleSetted();
