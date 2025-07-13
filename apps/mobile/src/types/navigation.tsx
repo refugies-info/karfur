bdd53f0b054d33d24330c5b709c79c440037e44e
Luis Arias <luis@refugies.info>
2025-07-13 15:01:52 +0200

    feat: add SearchScreen to navigation types and simplify SearchResultsScreen props

--- apps/mobile/src/types/navigation.tsx
+++ apps/mobile/src/types/navigation.tsx
@@ -10,6 +10,7 @@ export type RootStackParamList = {
   Root: undefined;
   OnboardingNavigator: undefined;
   LanguageChoiceNavigator: undefined;
+  SearchScreen: undefined;
 };
 
 export type BottomTabParamList = {
