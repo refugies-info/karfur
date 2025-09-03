052da3273baddace22a98b1cae3aa0bd0d2a28a7
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-09-03 16:59:33 +0200

    :lipstick: Fix modale look

--- apps/client/src/components/Modals/LanguageModal/LanguageModal.tsx
+++ apps/client/src/components/Modals/LanguageModal/LanguageModal.tsx
@@ -3,7 +3,7 @@ import { GetLanguagesResponse } from "@refugies-info/api-types";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import { isMobile } from "react-device-detect";
-import { Col, ListGroup, ListGroupItem, Modal, ModalBody, ModalHeader, Row } from "reactstrap";
+import { Col, ListGroup, ListGroupItem, Modal, ModalBody, Row } from "reactstrap";
 import { getPath } from "routes";
 import { LanguageSelector } from "~/components/UI/LanguageSelector";
 import useLocale from "~/hooks/useLocale";
@@ -26,11 +26,21 @@ const LanguageModal = (props: Props) => {
 
   return (
     <Modal isOpen={props.show} toggle={props.toggle} className={styles.modal} contentClassName={styles.modal_content}>
-      <ModalHeader toggle={props.toggle} className={styles.modal_header}>
-        <span className={cn(styles.title)}>{t("Homepage.modalLangTitle", "Quelle langue parlez-vous ?")}</span>
-      </ModalHeader>
       <ModalBody className={styles.modal_body}>
-        <ListGroup>
+        <div className="flex flex-col gap-8">
+          <Button
+            onClick={props.toggle}
+            className="!ms-auto block translate-x-4"
+            iconId="fr-icon-close-line"
+            priority="tertiary no outline"
+            iconPosition="right"
+          >
+            {t("fermer", "Fermer")}
+          </Button>
+          <h5 className={cn(styles.title)}>{t("Homepage.modalLangTitle", "Quelle langue parlez-vous ?")}</h5>
+        </div>
+
+        <ListGroup className="!pl-0">
           <LanguageSelector onChangeLang={props.toggle} itemsDesign="radio" />
 
           {!isMobile && (
