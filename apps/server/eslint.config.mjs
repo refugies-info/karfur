58f2e16fabd0c2e71d7f97f0e54dbeedc48d0f7f
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-09-10 14:22:48 +0200

    :wrench: Fix No tsconfigRootDir was set, and multiple candidate TSConfigRootDirs are present error

--- apps/server/eslint.config.mjs
+++ apps/server/eslint.config.mjs
@@ -1,6 +1,23 @@
 import eslint from "@eslint/js";
+import path from "path";
 import tseslint from "typescript-eslint";
+import { fileURLToPath } from "url";
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
-  ignores: ["dist"],
-});
+// Get the directory name of the current module
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  {
+    ...tseslint.configs.recommended,
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
+      },
+    },
+  },
+  {
+    ignores: ["dist"],
+  },
+);
