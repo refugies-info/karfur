45b04774a8952c101b47a3d89fc666cea65c1ed2
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-09-10 14:30:12 +0200

    :recycle: Better configuration thanks to gemini !

--- apps/server/eslint.config.mjs
+++ apps/server/eslint.config.mjs
@@ -1,15 +1,6 @@
-import eslint from "@eslint/js";
-import path from "path";
-import tseslint from "typescript-eslint";
-import { fileURLToPath } from "url";
-
-// Get the directory name of the current module
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 export default tseslint.config(
-  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
   {
-    ...tseslint.configs.recommended,
     languageOptions: {
       parserOptions: {
         tsconfigRootDir: __dirname,
