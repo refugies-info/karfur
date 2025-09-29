1d1f980e1187759e99a6bbb15ea8b2a45d3236b1
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-09-29 14:40:13 +0200

    loadMore

--- apps/client/src/components/Content/Dispositif/Dispositif.tsx
+++ apps/client/src/components/Content/Dispositif/Dispositif.tsx
@@ -37,6 +37,7 @@ const Dispositif = (props: Props) => {
   // Use the hook to ensure all rtri-link class links open in a new tab
   useRtriLinks(dispositifRef);
   const { isTablet, isMobile, isDesktop, isLargeDesktop } = useWindowSize();
+  console.log(isTablet, isMobile, isDesktop, isLargeDesktop);
   const pageContext = useContext(PageContext);
   const dispositif = useSelector(selectedDispositifSelector);
   const theme = useSelector(themeSelector(dispositif?.theme));
