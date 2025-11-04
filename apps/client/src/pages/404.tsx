4203daa9bb71d1871e443ef6f76b0924f6c816d5
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-11-04 12:15:40 +0100

    feat: redesign 404 error page with DSFR styling
    - Added custom illustration and updated layout to use DSFR components
    - Implemented internationalization support for 404 page content
    - Created Page404Layout component to handle 404-specific styling and layout structure

--- apps/client/src/pages/404.tsx
+++ apps/client/src/pages/404.tsx
@@ -1,23 +1,35 @@
-import Link from "next/link";
+import { Button } from "@codegouvfr/react-dsfr/Button";
+import { useTranslation } from "next-i18next";
+import Image from "next/image";
+import { ReactElement } from "react";
+import Page404Layout from "~/components/Layout/Page404Layout";
 import SEO from "~/components/Seo";
-import FButton from "~/components/UI/FButton/FButton";
 import { defaultStaticProps } from "~/lib/getDefaultStaticProps";
-import styles from "~/scss/pages/404.module.scss";
 
 const NotFound = () => {
+  const { t } = useTranslation();
+
   return (
-    <div className={styles.container + " animated fadeIn texte-small"}>
-      <SEO title="Page non trouvée" />
-      <h1>Page non trouvée</h1>
-      <Link legacyBehavior href="/" passHref>
-        <FButton tag="a" type="dark">
-          Retour à l'accueil
-        </FButton>
-      </Link>
-    </div>
+    <>
+      <SEO title={t("404.title", "Erreur 404")} />
+      <div className="bg-alt-blue-france grid w-full items-center justify-center">
+        <div className="flex flex-col items-center">
+          <h1 className="text-center">{t("404.title", "Erreur 404")}</h1>
+          <p className="text-center">
+            {t("404.text1", "Oups, un problème est survenu.")} <br />{" "}
+            {t("404.text2", "Veuillez réessayer ou revenez dans quelques minutes.")}
+          </p>
+          <Button className="m-auto" linkProps={{ href: "/" }} iconId="ri-home-4-line" iconPosition="right">
+            {t("404.cta", "Retour à la page d’accueil")}
+          </Button>
+          <Image className="mt-10 md:mt-20" src="/images/404/illustration_404.svg" alt="" width={316} height={224} />
+        </div>
+      </div>
+    </>
   );
 };
 
+NotFound.getLayout = (page: ReactElement) => <Page404Layout>{page}</Page404Layout>;
 export const getStaticProps = defaultStaticProps;
 
 export default NotFound;
