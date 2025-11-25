47444ffbb5489861cc6097e388ccee402857df60
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-11-25 17:26:39 +0100

    feat(client): improve breadcrumb accessibility with semantic HTML and ARIA labels
    - Wrap breadcrumb in <nav> with aria-label
    - Convert breadcrumb items to semantic <ol>/<li> structure
    - Add aria-current="page" to current page item
    - Add aria-hidden="true" to decorative chevron separators
    - Add sr-only label for home icon link
    - Add breadcrumbs translation key to common.json
    - Reorder imports for consistency

--- apps/client/src/components/Pages/dispositif/Breadcrumb/BreadcrumbDetails.tsx
+++ apps/client/src/components/Pages/dispositif/Breadcrumb/BreadcrumbDetails.tsx
@@ -1,5 +1,5 @@
 import { ContentType, GetDispositifResponse } from "@refugies-info/api-types";
-import { cn } from "@refugies-info/ui";
+import { cn, useWindowSize } from "@refugies-info/ui";
 import { useTranslation } from "next-i18next";
 import Link from "next/link";
 import { useMemo, useState } from "react";
@@ -10,7 +10,6 @@ import { buildUrlQuery } from "~/lib/recherche/buildUrlQuery";
 import { needSelector } from "~/services/Needs/needs.selectors";
 import { themeSelector } from "~/services/Themes/themes.selectors";
 import { getDepartments } from "./functions";
-import { useWindowSize } from "@refugies-info/ui";
 
 interface Props {
   dispositif: GetDispositifResponse;
@@ -44,55 +43,80 @@ const BreadcrumbDetails = ({ dispositif }: Props) => {
         </button>
       )}
       {(!isTablet || showBreadcrumb) && (
-        <div className="">
-          <Link href={getPath("/", "fr")} className="" title={t("homepage")}>
-            <i className="ri-home-4-line text-mention-grey !bg-transparent !bg-none text-xs [&::before]:![--icon-size:1.25rem]" />
-          </Link>
-
-          {chevron}
-
-          <Link
-            href={getPath("/recherche", "fr", `?${buildUrlQuery({ type: dispositif.typeContenu })}`)}
-            className="text-mention-grey !bg-transparent !bg-none underline decoration-current underline-offset-[0.125rem] [text-decoration-skip-ink:auto]"
-          >
-            {dispositif.typeContenu === ContentType.DISPOSITIF ? t("Dispositif.dispositif") : t("Dispositif.demarche")}
-          </Link>
-
-          {chevron}
-
-          {theme && (
-            <>
-              <Link
-                href={getPath("/recherche", "fr", `?${buildUrlQuery({ themes: [theme._id] })}`)}
-                className="text-mention-grey !bg-transparent !bg-none underline decoration-solid decoration-auto underline-offset-[0.125rem] [text-decoration-skip-ink:auto]"
-              >
-                {theme.short[locale] || theme.short.fr}
+        <nav aria-label={t("breadcrumbs", "Fil d'Ariane")}>
+          <ol className="!m-0 flex !list-none flex-wrap items-center !p-0 [&>li]:!list-none [&>li]:before:!content-none [&>li::marker]:!content-none">
+            <li>
+              <Link href={getPath("/", "fr")} className="" title={t("homepage")}>
+                <i className="ri-home-4-line text-mention-grey !bg-transparent !bg-none text-xs [&::before]:![--icon-size:1.25rem]" />
+                <span className="sr-only">{t("homepage")}</span>
               </Link>
-              {chevron}
-            </>
-          )}
+            </li>
 
-          {dispositif.needs.length === 1 && need && (
-            <>
+            <li aria-hidden="true" className="mx-1 select-none">
+              {chevron}
+            </li>
+
+            <li>
               <Link
-                href={getPath("/recherche", "fr", `?${buildUrlQuery({ needs: [need._id] })}`)}
-                className="text-mention-grey !bg-transparent !bg-none underline decoration-solid decoration-auto underline-offset-[0.125rem] [text-decoration-skip-ink:auto]"
+                href={getPath("/recherche", "fr", `?${buildUrlQuery({ type: dispositif.typeContenu })}`)}
+                className="text-mention-grey !bg-transparent !bg-none underline decoration-current underline-offset-[0.125rem] [text-decoration-skip-ink:auto]"
               >
-                {need[locale]?.text || need.fr.text}
+                {dispositif.typeContenu === ContentType.DISPOSITIF
+                  ? t("Dispositif.dispositif")
+                  : t("Dispositif.demarche")}
               </Link>
-              {chevron}
-            </>
-          )}
+            </li>
 
-          {dispositif.typeContenu === ContentType.DISPOSITIF && (
-            <span className="text-active-grey">
-              {`${dispositif.titreMarque || ""} ${getDepartments(dispositif.metadatas.location, t)}`}
-            </span>
-          )}
-          {dispositif.typeContenu === ContentType.DEMARCHE && (
-            <span className="text-active-grey">{dispositif.titreInformatif}</span>
-          )}
-        </div>
+            <li aria-hidden="true" className="mx-1 select-none">
+              {chevron}
+            </li>
+
+            {theme && (
+              <>
+                <li>
+                  <Link
+                    href={getPath("/recherche", "fr", `?${buildUrlQuery({ themes: [theme._id] })}`)}
+                    className="text-mention-grey !bg-transparent !bg-none underline decoration-solid decoration-auto underline-offset-[0.125rem] [text-decoration-skip-ink:auto]"
+                  >
+                    {theme.short[locale] || theme.short.fr}
+                  </Link>
+                </li>
+                <li aria-hidden="true" className="mx-1 select-none">
+                  {chevron}
+                </li>
+              </>
+            )}
+
+            {dispositif.needs.length === 1 && need && (
+              <>
+                <li>
+                  <Link
+                    href={getPath("/recherche", "fr", `?${buildUrlQuery({ needs: [need._id] })}`)}
+                    className="text-mention-grey !bg-transparent !bg-none underline decoration-solid decoration-auto underline-offset-[0.125rem] [text-decoration-skip-ink:auto]"
+                  >
+                    {need[locale]?.text || need.fr.text}
+                  </Link>
+                </li>
+                <li aria-hidden="true" className="mx-1 select-none">
+                  {chevron}
+                </li>
+              </>
+            )}
+
+            <li>
+              {dispositif.typeContenu === ContentType.DISPOSITIF && (
+                <span className="text-active-grey" aria-current="page">
+                  {`${dispositif.titreMarque || ""} ${getDepartments(dispositif.metadatas.location, t)}`}
+                </span>
+              )}
+              {dispositif.typeContenu === ContentType.DEMARCHE && (
+                <span className="text-active-grey" aria-current="page">
+                  {dispositif.titreInformatif}
+                </span>
+              )}
+            </li>
+          </ol>
+        </nav>
       )}
     </div>
   );
