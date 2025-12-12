dce0e921b6fdc864b914686c6c81bdb4a4c2bccf
Jérémie Gisserot <j@jeremie-gisserot.net>
2025-12-12 14:52:40 +0100

    fix: resolve 404 errors on content fetch

--- apps/server/src/workflows/dispositif/getContentById/getContentById.ts
+++ apps/server/src/workflows/dispositif/getContentById/getContentById.ts
@@ -105,15 +105,6 @@ export const getContentById = async (
     hasDraftVersion: 1,
     administrationLogo: 1,
     origin: 1,
-
-    "translations.fr.content.markdown": 1,
-    "translations.en.content.markdown": 1,
-    "translations.ar.content.markdown": 1,
-    "translations.ps.content.markdown": 1,
-    "translations.uk.content.markdown": 1,
-    "translations.ti.content.markdown": 1,
-    "translations.ru.content.markdown": 1,
-    "translations.fa.content.markdown": 1,
   };
   let draftDispositif = null;
   const originalDispositif = await (await getDispositifById(id, fields))?.populate<{
