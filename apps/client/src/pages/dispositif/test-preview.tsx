b5c5d6bf7eee2aa99094c7544fa185675ffadf6d
Jérémie Gisserot <j@jeremie-gisserot.net>
2026-01-12 16:41:20 +0100

    Feat: mode preview cleanup and optimisations

--- apps/client/src/pages/dispositif/test-preview.tsx
+++ apps/client/src/pages/dispositif/test-preview.tsx
@@ -4,12 +4,25 @@ import { useState } from "react";
 const DEFAULT_JSON = {
   dispositif: {
     typeContenu: "dispositif",
-    theme: "sante",
-    titreInformatif: "Titre prévisualisé",
+    theme: "63286a015d31b2c0cad99615",
+    titreInformatif: "Certificat de capacité à l'enseignement du français langue étrangère - FLE",
+    origin: "RCO",
     translations: {
       fr: {
         content: {
-          markdown: "Contenu markdown",
+          markdown: `
+## Objectifs
+
+Acquérir et développer les compétences et savoir-faire pédagogiques nécessaires à l'enseignement du français langue étrangère.
+
+## Contenu
+
+Gérer la classe et maîtriser les outils de la classe : 
+- Maîtriser les techniques d'animation et de gestion de classe - Favoriser les interactions authentiques en langue cible - Savoir utiliser des outils numériques simples pour la classe - Utiliser des supports et documents complémentaires (documents télévisuels, presse écrite, radio) - Connaître les ressources pédagogiques disponibles pour l'enseignant et l'apprenant. 
+Construire un cours : 
+- Planifier un cours et une progression - Cibler les activités sur des compétences visées - Enrichir le manuel avec des documents authentiques. Évaluer les apprenants par compétences : - Connaître les différentes formes d'évaluation - Utiliser l'évaluation comme outil de motivation - Identifier les erreurs et proposer des pistes de correction - Évaluer les apprenants sur la base du Cadre européen commun de référence (CECR).
+
+`,
         },
       },
     },
