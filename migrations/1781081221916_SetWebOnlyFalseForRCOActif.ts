0ad5f4650a79f40453caa2ce7c0293ae5b76ab2e
Jeremy PASTOURET <pastouret.jeremy@gmail.com>
2026-06-10 10:56:52 +0200

    fix: remove status Actif to make migration on all RCO

--- migrations/1781081221916_SetWebOnlyFalseForRCOActif.ts
+++ migrations/1781081221916_SetWebOnlyFalseForRCOActif.ts
@@ -5,13 +5,13 @@ export class SetWebOnlyFalseForRCOActif1781081221916 implements MigrationInterfa
   public async up(db: Db): Promise<void | never> {
     const result = await db
       .collection("dispositifs")
-      .updateMany({ origin: "RCO", status: "Actif" }, { $set: { webOnly: false } });
+      .updateMany({ origin: "RCO" }, { $set: { webOnly: false } });
     console.log(`Set webOnly: false on ${result.modifiedCount} RCO Actif dispositifs`);
   }
 
   public async down(db: Db): Promise<void | never> {
     await db
       .collection("dispositifs")
-      .updateMany({ origin: "RCO", status: "Actif", webOnly: false }, { $unset: { webOnly: "" } });
+      .updateMany({ origin: "RCO", webOnly: false }, { $unset: { webOnly: "" } });
   }
 }
