f29f1ca056a22c47851503095e66b8db8d43f71b
Jeremy PASTOURET <pastouret.jeremy@gmail.com>
2026-06-25 11:29:47 +0200

    fix(mission): update text content

--- apps/client/src/components/Pages/staticPages/mission-et-impact/SectionMission.tsx
+++ apps/client/src/components/Pages/staticPages/mission-et-impact/SectionMission.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { useTranslation } from "next-i18next";
-import AlainRegnier from "~/assets/staticPages/mission-et-impact/photo-alain-regnier.png";
 import ScreenshotRI from "~/assets/staticPages/mission-et-impact/ri-screenshot.png";
 import { Section, Title2 } from "~/components/Pages/staticPages/common";
 
@@ -32,14 +31,11 @@ export const SectionMission = () => {
                 __html: t("MissionImpact.mission_p1"),
               }}
             />
+            <p className="!text-large">{t("MissionImpact.mission_p11")}</p>
             <p className="!text-large">{t("MissionImpact.mission_p2")}</p>
             <p className="!text-large !mb-0">{t("MissionImpact.mission_p3")}</p>
             <div className="mt-10 flex items-center gap-6">
-              <Image src={AlainRegnier} alt="Alain Regnier - Photo" width={120} height={120} />
-              <p className="!text-large !mb-0">
-                <strong className="block">Alain Régnier</strong>
-                <span>{t("MissionImpact.mission_alain_position")}</span>
-              </p>
+              <p className="!text-large !mb-0"></p>
             </div>
           </div>
         </div>
